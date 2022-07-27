62c4dc59918ea4b7a1f399c0aef1ea958d082f25
Liu DongMing <l.dongming@infracreate.com>
2022-07-27 16:55:37 +0800

    init k3d and deploy a database cluster

---
+++ pkg/provider/provider.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2022 The OpenCli Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provider
+
+import (
+	"strings"
+
+	"helm.sh/helm/v3/pkg/repo"
+
+	"github.com/infracreate/opencli/pkg/utils/helm"
+)
+
+type Provider interface {
+	GetRepos() []repo.Entry
+	GetBaseCharts(ns string) []helm.InstallOpts
+	GetDBCharts(ns string, dbname string) []helm.InstallOpts
+}
+
+func NewProvider(e string) Provider {
+	switch buildEngineType(e) {
+	case BitnamiMySQL:
+		return new(BitnamiMysql)
+	case MySQLOperator:
+		return new(MysqlOperator)
+	default:
+		return nil
+	}
+}
+
+func buildEngineType(e string) EngineType {
+	if strings.Contains(e, "bitnami") && strings.Contains(e, "mysql") {
+		return BitnamiMySQL
+	}
+
+	if strings.Contains(e, "mysql") {
+		return MySQLOperator
+	}
+
+	return UnknownEngine
+}

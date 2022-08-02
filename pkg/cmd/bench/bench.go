9f396d0116d0c9cbb59a31e7ad65f0ba18506dff
l.dongming <l.dongming@infracreate.com>
2022-08-02 18:43:16 +0800

    fix: #5 bench clean up first

--- pkg/cmd/bench/bench.go
+++ pkg/cmd/bench/bench.go
@@ -58,7 +58,7 @@ func NewBenchCmd(f cmdutil.Factory) *cobra.Command {
 	}
 
 	cmd.PersistentFlags().IntVar(&maxProcs, "max-procs", 0, "runtime.GOMAXPROCS")
-	cmd.PersistentFlags().StringVarP(&dbName, "db", "D", "test", "Database name")
+	cmd.PersistentFlags().StringVarP(&dbName, "db", "D", "opencli_test", "Database name")
 	cmd.PersistentFlags().StringVarP(&host, "host", "H", "127.0.0.1", "Database host")
 	cmd.PersistentFlags().StringVarP(&user, "user", "U", "root", "Database user")
 	cmd.PersistentFlags().StringVarP(&password, "password", "p", "sakila", "Database password")

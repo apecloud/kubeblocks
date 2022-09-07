0f491a5d7b0b073887ac2871371b893e08c38547
chantu <chantu@infracreate.com>
2022-09-07 21:39:07 +0800

    move termination policy to deleteExternalResources

--- controllers/dbaas/cluster_controller.go
+++ controllers/dbaas/cluster_controller.go
@@ -105,20 +105,7 @@ func (r *ClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	}
 
 	res, err := intctrlutil.HandleCRDeletion(reqCtx, r, cluster, dbClusterFinalizerName, func() (*ctrl.Result, error) {
-		if err := r.deleteExternalResources(reqCtx, cluster); err != nil {
-			res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
-			return &res, err
-		}
-		switch cluster.Spec.TerminationPolicy {
-		case dbaasv1alpha1.DoNotTerminate:
-			res, err := intctrlutil.Reconciled()
-			return &res, err
-		case dbaasv1alpha1.Delete, dbaasv1alpha1.WipeOut:
-			err := r.deletePVCs(reqCtx, cluster)
-			res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
-			return &res, err
-		}
-		return nil, nil
+		return r.deleteExternalResources(reqCtx, cluster)
 	})
 	if res != nil {
 		return *res, err
@@ -186,18 +173,30 @@ func (r *ClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func (r *ClusterReconciler) deleteExternalResources(reqCtx intctrlutil.RequestCtx, cluster *dbaasv1alpha1.Cluster) error {
+func (r *ClusterReconciler) deleteExternalResources(reqCtx intctrlutil.RequestCtx, cluster *dbaasv1alpha1.Cluster) (*ctrl.Result, error) {
 	//
 	// delete any external resources associated with the cronJob
 	//
 	// Ensure that delete implementation is idempotent and safe to invoke
 	// multiple times for same object.
 
+	switch cluster.Spec.TerminationPolicy {
+	case dbaasv1alpha1.DoNotTerminate:
+		res, err := intctrlutil.Reconciled()
+		return &res, err
+	case dbaasv1alpha1.Delete, dbaasv1alpha1.WipeOut:
+		if err := r.deletePVCs(reqCtx, cluster); err != nil {
+			res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+			return &res, err
+		}
+	}
+
 	clusterDef := &dbaasv1alpha1.ClusterDefinition{}
 	if err := r.Get(reqCtx.Ctx, client.ObjectKey{
 		Name: cluster.Spec.ClusterDefRef,
 	}, clusterDef); err != nil {
-		return err
+		res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+		return &res, err
 	}
 
 	ml := client.MatchingLabels{
@@ -207,7 +206,8 @@ func (r *ClusterReconciler) deleteExternalResources(reqCtx intctrlutil.RequestCt
 
 	stsList := &appsv1.StatefulSetList{}
 	if err := r.List(reqCtx.Ctx, stsList, ml); err != nil {
-		return err
+		res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+		return &res, err
 	}
 	for _, sts := range stsList.Items {
 		if !controllerutil.ContainsFinalizer(&sts, dbClusterFinalizerName) {
@@ -216,12 +216,14 @@ func (r *ClusterReconciler) deleteExternalResources(reqCtx intctrlutil.RequestCt
 		patch := client.MergeFrom(sts.DeepCopy())
 		controllerutil.RemoveFinalizer(&sts, dbClusterFinalizerName)
 		if err := r.Patch(reqCtx.Ctx, &sts, patch); err != nil {
-			return err
+			res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+			return &res, err
 		}
 	}
 	svcList := &corev1.ServiceList{}
 	if err := r.List(reqCtx.Ctx, svcList, ml); err != nil {
-		return err
+		res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+		return &res, err
 	}
 	for _, svc := range svcList.Items {
 		if !controllerutil.ContainsFinalizer(&svc, dbClusterFinalizerName) {
@@ -230,12 +232,14 @@ func (r *ClusterReconciler) deleteExternalResources(reqCtx intctrlutil.RequestCt
 		patch := client.MergeFrom(svc.DeepCopy())
 		controllerutil.RemoveFinalizer(&svc, dbClusterFinalizerName)
 		if err := r.Patch(reqCtx.Ctx, &svc, patch); err != nil {
-			return err
+			res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+			return &res, err
 		}
 	}
 	secretList := &corev1.SecretList{}
 	if err := r.List(reqCtx.Ctx, secretList, ml); err != nil {
-		return err
+		res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+		return &res, err
 	}
 	for _, secret := range secretList.Items {
 		if !controllerutil.ContainsFinalizer(&secret, dbClusterFinalizerName) {
@@ -244,10 +248,11 @@ func (r *ClusterReconciler) deleteExternalResources(reqCtx intctrlutil.RequestCt
 		patch := client.MergeFrom(secret.DeepCopy())
 		controllerutil.RemoveFinalizer(&secret, dbClusterFinalizerName)
 		if err := r.Patch(reqCtx.Ctx, &secret, patch); err != nil {
-			return err
+			res, err := intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+			return &res, err
 		}
 	}
-	return nil
+	return nil, nil
 }
 
 func (r *ClusterReconciler) deletePVCs(reqCtx intctrlutil.RequestCtx, cluster *dbaasv1alpha1.Cluster) error {

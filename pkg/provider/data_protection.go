5ae5f13f1102b4822253f682e27dfd4a66f989a5
shaojiang <shaojiang.deng@infracreate.com>
2022-09-07 16:08:24 +0800

    chore: fix backup bug

--- pkg/provider/data_protection.go
+++ pkg/provider/data_protection.go
@@ -32,59 +32,40 @@ type DataProtection struct {
 }
 
 func (o *DataProtection) GetRepos() []repo.Entry {
-	return []repo.Entry{}
+	return []repo.Entry{
+		{
+			Name: "prometheus-community",
+			URL:  "https://prometheus-community.github.io/helm-charts",
+		},
+		{
+			Name: "mysql-operator",
+			URL:  "https://mysql.github.io/mysql-operator/",
+		},
+		{
+			Name: "piraeus-charts",
+			URL:  "https://piraeus.io/helm-charts/",
+		},
+		{
+			Name: "cloudve",
+			URL:  "https://github.com/CloudVE/helm-charts/raw/master",
+		},
+	}
 }
 
 func (o *DataProtection) GetBaseCharts(ns string) []helm.InstallOpts {
-	return []helm.InstallOpts{
-		{
-			Name:      "prometheus",
-			Chart:     "oci://yimeisun.azurecr.io/helm-chart/kube-prometheus-stack",
-			Wait:      false,
-			Version:   "38.0.2",
-			Namespace: ns,
-			Sets: []string{
-				"prometheusOperator.admissionWebhooks.patch.image.repository=weidixian/ingress-nginx-kube-webhook-certgen",
-				"kube-state-metrics.image.repository=jiamiao442/kube-state-metrics",
-				"kubeStateMetrics.enabled=false",
-				"grafana.sidecar.dashboards.searchNamespace=ALL",
-				"prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false",
-				"prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false",
-				"alertmanager.alertmanagerSpec.image.repository=infracreate/alertmanager",
-				"prometheusOperator.image.repository=infracreate/prometheus-operator",
-				"prometheusOperator.prometheusConfigReloader.image.repository=infracreate/prometheus-config-reloader",
-				"prometheusOperator.thanosImage.repository=infracreate/thanos",
-				"prometheusOperator.prometheusSpec.image.repository=infracreate/prometheus",
-				"prometheus.prometheusSpec.image.repository=infracreate/prometheus",
-				"thanosRuler.thanosRulerSpec.image.repository=infracreate/thanos",
-				"prometheus-node-exporter.image.repository=infracreate/node-exporter",
-				"grafana.sidecar.image.repository=infracreate/k8s-sidecar",
-			},
-			LoginOpts: &helm.LoginOpts{
-				User:   helmUser,
-				Passwd: helmPasswd,
-				URL:    helmURL,
-			},
-			TryTimes: 2,
-		},
-	}
+	return []helm.InstallOpts{}
 }
 
 func (o *DataProtection) GetDBCharts(ns string, dbname string) []helm.InstallOpts {
 	return []helm.InstallOpts{
 		{
 			Name:      "mysql-operator",
-			Chart:     "oci://yimeisun.azurecr.io/helm-chart/mysql-operator",
+			Chart:     "mysql-operator/mysql-operator",
 			Wait:      true,
 			Version:   "2.0.6",
 			Namespace: ns,
 			Sets:      []string{},
-			LoginOpts: &helm.LoginOpts{
-				User:   helmUser,
-				Passwd: helmPasswd,
-				URL:    helmURL,
-			},
-			TryTimes: 2,
+			TryTimes:  2,
 		},
 		{
 			Name:      dbname,
@@ -96,31 +77,26 @@ func (o *DataProtection) GetDBCharts(ns string, dbname string) []helm.InstallOpt
 				"serverVersion=" + o.ServerVersion,
 			},
 			LoginOpts: &helm.LoginOpts{
-				User:   helmUser,
-				Passwd: helmPasswd,
-				URL:    helmURL,
+				User:   "yimeisun",
+				Passwd: "8V+PmX1oSDv4pumDvZp6m7LS8iPgbY3A",
+				URL:    "yimeisun.azurecr.io",
 			},
 			TryTimes: 2,
 		},
 		{
 			Name:      "snapshot-controller",
-			Chart:     "oci://yimeisun.azurecr.io/helm-chart/snapshot-controller",
+			Chart:     "piraeus-charts/snapshot-controller",
 			Wait:      true,
 			Version:   "1.5.1",
 			Namespace: ns,
 			Sets: []string{
 				"image.repository=registry.aliyuncs.com/google_containers/snapshot-controller",
 			},
-			LoginOpts: &helm.LoginOpts{
-				User:   helmUser,
-				Passwd: helmPasswd,
-				URL:    helmURL,
-			},
 			TryTimes: 2,
 		},
 		{
 			Name:      "csi-s3",
-			Chart:     "oci://yimeisun.azurecr.io/helm-chart/csi-s3",
+			Chart:     "cloudve/csi-s3",
 			Wait:      true,
 			Version:   "0.31.3",
 			Namespace: ns,
@@ -130,11 +106,6 @@ func (o *DataProtection) GetDBCharts(ns string, dbname string) []helm.InstallOpt
 				"secret.endpoint=" + o.S3Endpoint,
 				"storageClass.singleBucket=" + o.S3Bucket,
 				"storageClass.mountOptions=--memory-limit 1000 --dir-mode 0777 --file-mode 0666 --region " + o.Region,
-			},
-			LoginOpts: &helm.LoginOpts{
-				User:   helmUser,
-				Passwd: helmPasswd,
-				URL:    helmURL,
 			},
 			TryTimes: 2,
 		},

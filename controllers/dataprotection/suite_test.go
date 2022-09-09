faf37aee0b9f23e30c90172b7d7aa655cbf00d37
shaojiang <shaojiang.deng@infracreate.com>
2022-09-09 15:39:05 +0800

    chore: added dataprotection controller unit test

--- controllers/dataprotection/suite_test.go
+++ controllers/dataprotection/suite_test.go
@@ -18,21 +18,20 @@ package dataprotection
 
 import (
 	"context"
+	dataprotectionv1alpha1 "github.com/apecloud/kubeblocks/apis/dataprotection/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"path/filepath"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	dataprotectionv1alpha1 "github.com/apecloud/kubeblocks/apis/dataprotection/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -49,7 +48,7 @@ func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
 	RunSpecsWithDefaultAndCustomReporters(t,
-		"Controller Suite",
+		"Data Protection Controller Suite",
 		[]Reporter{printer.NewlineReporter{}})
 }
 
@@ -59,9 +58,18 @@ var _ = BeforeSuite(func() {
 	ctx, cancel = context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
+	/*
+		customApiServerFlags := []string{
+			"--secure-port=6884",
+		}
+		apiServerFlags := append([]string(nil), envtest.DefaultKubeAPIServerFlags...)
+		apiServerFlags = append(apiServerFlags, customApiServerFlags...)
+
+	*/
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
+		//KubeAPIServerFlags:    apiServerFlags,
 	}
 
 	var err error
@@ -69,23 +77,28 @@ var _ = BeforeSuite(func() {
 	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
+	/*
+		err = dataprotectionv1alpha1.AddToScheme(scheme.Scheme)
+		Expect(err).NotTo(HaveOccurred())
+	*/
 
-	err = dataprotectionv1alpha1.AddToScheme(scheme.Scheme)
+	scheme := runtime.NewScheme()
+	err = dataprotectionv1alpha1.AddToScheme(scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
 	// run reconcile
 	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme.Scheme,
+		Scheme: scheme,
 	})
 	Expect(err).ToNot(HaveOccurred())
 

e895b7d93b6e60528dc3ccee56d7f56bd1581de2
Guo Ziang <ziang.gza@infracreate.com>
2022-10-11 19:33:24 +0800

    make test work on darwin

---
+++ internal/loadbalancer/network/client_darwin.go
@@ -0,0 +1,29 @@
+package network
+
+import (
+	"github.com/apecloud/kubeblocks/internal/loadbalancer/cloud"
+	iptableswrapper "github.com/apecloud/kubeblocks/internal/loadbalancer/iptables"
+	netlinkwrapper "github.com/apecloud/kubeblocks/internal/loadbalancer/netlink"
+	procfswrapper "github.com/apecloud/kubeblocks/internal/loadbalancer/procfs"
+	"github.com/go-logr/logr"
+)
+
+func NewClient(logger logr.Logger, nl netlinkwrapper.NetLink, ipt iptableswrapper.IPTables, procfs procfswrapper.ProcFS) (*networkClient, error) {
+	return &networkClient{ipt: ipt}, nil
+}
+
+func (n *networkClient) SetupNetworkForService(privateIP string, eni *cloud.ENIMetadata) error {
+	return nil
+}
+
+func (n *networkClient) CleanNetworkForService(privateIP string, eni *cloud.ENIMetadata) error {
+	return nil
+}
+
+func (n *networkClient) SetupNetworkForENI(eni *cloud.ENIMetadata) error {
+	return nil
+}
+
+func (n *networkClient) CleanNetworkForENI(eni *cloud.ENIMetadata) error {
+	return nil
+}

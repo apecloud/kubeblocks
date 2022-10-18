d876bf711e094f88cb5ab9d0e1e54c2975fcce2b
yangmei li <yangmeili@yangmeideMacBook-Pro.local>
2022-10-18 17:28:10 +0800

    Bugfix: copy(dst, src) to supersede original array by using a new array in function checkAndUpdatePodVolumes

--- controllers/dbaas/lifecycle_utils_test.go
+++ controllers/dbaas/lifecycle_utils_test.go
@@ -20,6 +20,8 @@ import (
 	"strings"
 	"testing"
 
+	appsv1 "k8s.io/api/apps/v1"
+
 	corev1 "k8s.io/api/core/v1"
 
 	dbaasv1alpha1 "github.com/apecloud/kubeblocks/apis/dbaas/v1alpha1"
@@ -174,3 +176,76 @@ var _ = Describe("create", func() {
 		})
 	})
 })
+
+func TestCheckAndUpdatePodVolumes(t *testing.T) {
+	var _ = Describe("lifecycle_utils", func() {
+		var sts appsv1.StatefulSet
+		var volumes map[string]dbaasv1alpha1.ConfigTemplate
+
+		Context("TestCheckAndUpdatePodVolumes", func() {
+			BeforeEach(func() {
+				sts = appsv1.StatefulSet{
+					Spec: appsv1.StatefulSetSpec{
+						Template: corev1.PodTemplateSpec{
+							Spec: corev1.PodSpec{
+								Volumes: []corev1.Volume{
+									{
+										Name: "data",
+										VolumeSource: corev1.VolumeSource{
+											EmptyDir: &corev1.EmptyDirVolumeSource{},
+										},
+									},
+								},
+								Containers: []corev1.Container{
+									{
+										Name:            "mysql",
+										Image:           "docker.io/infracreate/wesql-server-8.0:0.1-SNAPSHOT",
+										ImagePullPolicy: "IfNotPresent",
+										VolumeMounts: []corev1.VolumeMount{
+											{
+												Name:      "data",
+												MountPath: "/data",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+				volumes = make(map[string]dbaasv1alpha1.ConfigTemplate)
+
+			})
+
+			It("Corner case volume is nil, and add no volume", func() {
+				err := checkAndUpdatePodVolumes(&sts, volumes)
+				Expect(err).Should(BeNil())
+				Expect(len(sts.Spec.Template.Spec.Volumes)).To(Equal(1))
+			})
+
+			It("Normal test case, and add one volume", func() {
+				volumes["my_config"] = dbaasv1alpha1.ConfigTemplate{
+					Name:       "myConfig",
+					VolumeName: "myConfigVolume",
+				}
+				err := checkAndUpdatePodVolumes(&sts, volumes)
+				Expect(err).Should(BeNil())
+				Expect(len(sts.Spec.Template.Spec.Volumes)).To(Equal(2))
+			})
+
+			It("Normal test case, and add two volume", func() {
+				volumes["my_config"] = dbaasv1alpha1.ConfigTemplate{
+					Name:       "myConfig",
+					VolumeName: "myConfigVolume",
+				}
+				volumes["my_config1"] = dbaasv1alpha1.ConfigTemplate{
+					Name:       "myConfig",
+					VolumeName: "myConfigVolume",
+				}
+				err := checkAndUpdatePodVolumes(&sts, volumes)
+				Expect(err).Should(BeNil())
+				Expect(len(sts.Spec.Template.Spec.Volumes)).To(Equal(3))
+			})
+		})
+	})
+}

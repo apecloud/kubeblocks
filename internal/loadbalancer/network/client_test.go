cce322ce6d3813909d05564f933aac0387af3de9
l.dongming <l.dongming@infracreate.com>
2022-10-27 14:19:09 +0800

    rename copyright to ApeCloud Inc.

--- internal/loadbalancer/network/client_test.go
+++ internal/loadbalancer/network/client_test.go
@@ -1,7 +1,7 @@
 //go:build linux
 
 /*
-Copyright 2022 The KubeBlocks Authors
+Copyright ApeCloud Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

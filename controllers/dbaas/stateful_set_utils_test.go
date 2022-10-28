3280ad998e41cb1c97b85f07ff78f87b44aed86e
free6om <free6om.c@gmail.com>
2022-10-28 13:40:12 +0800

    stateful_set_utils test cases

--- controllers/dbaas/stateful_set_utils_test.go
+++ controllers/dbaas/stateful_set_utils_test.go
@@ -23,19 +23,12 @@ import (
 
 	apps "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/kubernetes/pkg/controller"
 )
 
 // ------- copy from stateful_set_utils_test.go ----
-
-// controllerKind contains the schema.GroupVersionKind for this controller type.
-var controllerKind = apps.SchemeGroupVersion.WithKind("StatefulSet")
-
 func TestGetParentNameAndOrdinal(t *testing.T) {
-	set := newStatefulSet(3)
+	set := newStatefulSet("foo", 3)
 	pod := newStatefulSetPod(set, 1)
 	if parent, ordinal := getParentNameAndOrdinal(pod); parent != set.Name {
 		t.Errorf("Extracted the wrong parent name expected %s found %s", set.Name, parent)
@@ -51,8 +44,8 @@ func TestGetParentNameAndOrdinal(t *testing.T) {
 }
 
 func TestIsMemberOf(t *testing.T) {
-	set := newStatefulSet(3)
-	set2 := newStatefulSet(3)
+	set := newStatefulSet("foo", 3)
+	set2 := newStatefulSet("bar", 3)
 	set2.Name = "foo2"
 	pod := newStatefulSetPod(set, 1)
 	if !isMemberOf(set, pod) {
@@ -63,51 +56,17 @@ func TestIsMemberOf(t *testing.T) {
 	}
 }
 
-func newPVC(name string) v1.PersistentVolumeClaim {
-	return v1.PersistentVolumeClaim{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: v1.NamespaceDefault,
-			Name:      name,
-		},
-		Spec: v1.PersistentVolumeClaimSpec{
-			Resources: v1.ResourceRequirements{
-				Requests: v1.ResourceList{
-					v1.ResourceStorage: *resource.NewQuantity(1, resource.BinarySI),
-				},
-			},
-		},
-	}
-}
+// ------- end copy from stateful_set_utils_test.go ----
 
-func newStatefulSetWithVolumes(replicas int, name string, petMounts []v1.VolumeMount, podMounts []v1.VolumeMount) *apps.StatefulSet {
-	mounts := append(petMounts, podMounts...)
-	claims := []v1.PersistentVolumeClaim{}
-	for _, m := range petMounts {
-		claims = append(claims, newPVC(m.Name))
-	}
-
-	vols := []v1.Volume{}
-	for _, m := range podMounts {
-		vols = append(vols, v1.Volume{
-			Name: m.Name,
-			VolumeSource: v1.VolumeSource{
-				HostPath: &v1.HostPathVolumeSource{
-					Path: fmt.Sprintf("/tmp/%v", m.Name),
-				},
-			},
-		})
-	}
-
+func newStatefulSet(name string, replicas int) *apps.StatefulSet {
 	template := v1.PodTemplateSpec{
 		Spec: v1.PodSpec{
 			Containers: []v1.Container{
 				{
-					Name:         "nginx",
-					Image:        "nginx",
-					VolumeMounts: mounts,
+					Name:  "nginx",
+					Image: "nginx",
 				},
 			},
-			Volumes: vols,
 		},
 	}
 
@@ -121,131 +80,24 @@ func newStatefulSetWithVolumes(replicas int, name string, petMounts []v1.VolumeM
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: v1.NamespaceDefault,
-			UID:       types.UID("test"),
 		},
 		Spec: apps.StatefulSetSpec{
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{"foo": "bar"},
 			},
-			Replicas:             func() *int32 { i := int32(replicas); return &i }(),
-			Template:             template,
-			VolumeClaimTemplates: claims,
-			ServiceName:          "governingsvc",
-			UpdateStrategy:       apps.StatefulSetUpdateStrategy{Type: apps.RollingUpdateStatefulSetStrategyType},
-			PersistentVolumeClaimRetentionPolicy: &apps.StatefulSetPersistentVolumeClaimRetentionPolicy{
-				WhenScaled:  apps.RetainPersistentVolumeClaimRetentionPolicyType,
-				WhenDeleted: apps.RetainPersistentVolumeClaimRetentionPolicyType,
-			},
-			RevisionHistoryLimit: func() *int32 {
-				limit := int32(2)
-				return &limit
-			}(),
+			Replicas:    func() *int32 { i := int32(replicas); return &i }(),
+			Template:    template,
+			ServiceName: "governingsvc",
 		},
 	}
 }
 
-func newStatefulSet(replicas int) *apps.StatefulSet {
-	petMounts := []v1.VolumeMount{
-		{Name: "datadir", MountPath: "/tmp/zookeeper"},
-	}
-	podMounts := []v1.VolumeMount{
-		{Name: "home", MountPath: "/home"},
-	}
-	return newStatefulSetWithVolumes(replicas, "foo", petMounts, podMounts)
-}
-
-// newStatefulSetPod returns a new Pod conforming to the set's Spec with an identity generated from ordinal.
 func newStatefulSetPod(set *apps.StatefulSet, ordinal int) *v1.Pod {
-	pod, _ := controller.GetPodFromTemplate(&set.Spec.Template, set, metav1.NewControllerRef(set, controllerKind))
+	pod := &v1.Pod{}
 	pod.Name = getPodName(set, ordinal)
-	initIdentity(set, pod)
-	updateStorage(set, pod)
 	return pod
 }
 
-// getPodName gets the name of set's child Pod with an ordinal index of ordinal
 func getPodName(set *apps.StatefulSet, ordinal int) string {
 	return fmt.Sprintf("%s-%d", set.Name, ordinal)
 }
-
-// getOrdinal gets pod's ordinal. If pod has no ordinal, -1 is returned.
-func getOrdinal(pod *v1.Pod) int {
-	_, ordinal := getParentNameAndOrdinal(pod)
-	return ordinal
-}
-
-// getPersistentVolumeClaimName gets the name of PersistentVolumeClaim for a Pod with an ordinal index of ordinal. claim
-// must be a PersistentVolumeClaim from set's VolumeClaims template.
-func getPersistentVolumeClaimName(set *apps.StatefulSet, claim *v1.PersistentVolumeClaim, ordinal int) string {
-	// NOTE: This name format is used by the heuristics for zone spreading in ChooseZoneForVolume
-	return fmt.Sprintf("%s-%s-%d", claim.Name, set.Name, ordinal)
-}
-
-// getPersistentVolumeClaims gets a map of PersistentVolumeClaims to their template names, as defined in set. The
-// returned PersistentVolumeClaims are each constructed with a the name specific to the Pod. This name is determined
-// by getPersistentVolumeClaimName.
-func getPersistentVolumeClaims(set *apps.StatefulSet, pod *v1.Pod) map[string]v1.PersistentVolumeClaim {
-	ordinal := getOrdinal(pod)
-	templates := set.Spec.VolumeClaimTemplates
-	claims := make(map[string]v1.PersistentVolumeClaim, len(templates))
-	for i := range templates {
-		claim := templates[i]
-		claim.Name = getPersistentVolumeClaimName(set, &claim, ordinal)
-		claim.Namespace = set.Namespace
-		if claim.Labels != nil {
-			for key, value := range set.Spec.Selector.MatchLabels {
-				claim.Labels[key] = value
-			}
-		} else {
-			claim.Labels = set.Spec.Selector.MatchLabels
-		}
-		claims[templates[i].Name] = claim
-	}
-	return claims
-}
-
-// updateStorage updates pod's Volumes to conform with the PersistentVolumeClaim of set's templates. If pod has
-// conflicting local Volumes these are replaced with Volumes that conform to the set's templates.
-func updateStorage(set *apps.StatefulSet, pod *v1.Pod) {
-	currentVolumes := pod.Spec.Volumes
-	claims := getPersistentVolumeClaims(set, pod)
-	newVolumes := make([]v1.Volume, 0, len(claims))
-	for name, claim := range claims {
-		newVolumes = append(newVolumes, v1.Volume{
-			Name: name,
-			VolumeSource: v1.VolumeSource{
-				PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
-					ClaimName: claim.Name,
-					// TODO: Use source definition to set this value when we have one.
-					ReadOnly: false,
-				},
-			},
-		})
-	}
-	for i := range currentVolumes {
-		if _, ok := claims[currentVolumes[i].Name]; !ok {
-			newVolumes = append(newVolumes, currentVolumes[i])
-		}
-	}
-	pod.Spec.Volumes = newVolumes
-}
-
-func initIdentity(set *apps.StatefulSet, pod *v1.Pod) {
-	updateIdentity(set, pod)
-	// Set these immutable fields only on initial Pod creation, not updates.
-	pod.Spec.Hostname = pod.Name
-	pod.Spec.Subdomain = set.Spec.ServiceName
-}
-
-// updateIdentity updates pod's name, hostname, and subdomain, and StatefulSetPodNameLabel to conform to set's name
-// and headless service.
-func updateIdentity(set *apps.StatefulSet, pod *v1.Pod) {
-	pod.Name = getPodName(set, getOrdinal(pod))
-	pod.Namespace = set.Namespace
-	if pod.Labels == nil {
-		pod.Labels = make(map[string]string)
-	}
-	pod.Labels[apps.StatefulSetPodNameLabel] = pod.Name
-}
-
-// ------- end copy from stateful_set_utils_test.go ----

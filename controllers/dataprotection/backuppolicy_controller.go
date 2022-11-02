f285a61be65d6cda650c98a8bfc1ddc03fa98dee
shaojiang <shaojiang.deng@infracreate.com>
2022-11-02 15:51:54 +0800

    chore: fix lint failed

--- controllers/dataprotection/backuppolicy_controller.go
+++ controllers/dataprotection/backuppolicy_controller.go
@@ -19,15 +19,12 @@ package dataprotection
 import (
 	"context"
 	"fmt"
-	corev1 "k8s.io/api/core/v1"
-
-	"k8s.io/apimachinery/pkg/types"
 
 	intctrlutil "github.com/apecloud/kubeblocks/internal/controllerutil"
-
-	"k8s.io/client-go/tools/record"
-
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"

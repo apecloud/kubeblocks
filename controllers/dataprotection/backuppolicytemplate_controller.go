5bce5b3c033bd0533930696825dfaf78090f5c16
shaojiang <shaojiang.deng@infracreate.com>
2022-11-04 10:36:01 +0800

    chore: reorder import.

--- controllers/dataprotection/backuppolicytemplate_controller.go
+++ controllers/dataprotection/backuppolicytemplate_controller.go
@@ -18,14 +18,14 @@ package dataprotection
 
 import (
 	"context"
-	"github.com/spf13/viper"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	intctrlutil "github.com/apecloud/kubeblocks/internal/controllerutil"
+	"github.com/spf13/viper"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	dataprotectionv1alpha1 "github.com/apecloud/kubeblocks/apis/dataprotection/v1alpha1"

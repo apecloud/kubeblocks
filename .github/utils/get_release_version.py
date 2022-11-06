37674c392e43da3aa73eae27bf699d1d29f599a3
L.DongMing <l.dongming@infracreate.com>
2022-11-06 23:52:36 +0800

    create release by tag

--- .github/utils/get_release_version.py
+++ .github/utils/get_release_version.py
@@ -1,6 +1,6 @@
 # Get release version from git tag and set the parsed version to
 # environment variable REL_VERSION, if found the release note, set
-# LATEST_RELEASE to true to add 'latest' tag.
+# WITH_RELEASE_NOTES to true.
 
 import os
 import sys
@@ -10,24 +10,23 @@ tagRefPrefix = "refs/tags/v"
 
 with open(os.getenv("GITHUB_ENV"), "a") as githubEnv:
     if gitRef is None or not gitRef.startswith(tagRefPrefix):
-        githubEnv.write("REL_VERSION=edge\n")
         print("This is not a release tag")
-        sys.exit(0)
+        sys.exit(1)
 
     releaseVersion = gitRef[len(tagRefPrefix):]
     releaseNotePath = "docs/release_notes/v{}.md".format(releaseVersion)
 
     if gitRef.find("-alpha.") > 0:
-        print("Alpha release build from {} ...").format(gitRef)
+        print("Alpha release build from {} ...".format(gitRef))
     elif gitRef.find("-beta.") > 0:
-        print("Beta release build from {} ...").format(gitRef)
+        print("Beta release build from {} ...".format(gitRef))
     elif gitRef.find("-rc.") > 0:
-        print("Release Candidate build from {} ...").format(gitRef)
+        print("Release Candidate build from {} ...".format(gitRef))
     else:
         print("Checking if {} exists".format(releaseNotePath))
         if os.path.exists(releaseNotePath):
             print("Found {}".format(releaseNotePath))
-            githubEnv.write("LATEST_RELEASE=true\n")
+            githubEnv.write("WITH_RELEASE_NOTES=true\n")
         else:
             print("{} is not found".format(releaseNotePath))
             sys.exit(1)

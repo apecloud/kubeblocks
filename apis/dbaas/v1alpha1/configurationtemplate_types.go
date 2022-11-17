8194468aeb84d77a0a42c19c6d2100203b9ee2cb
sophon <sophon@infracreate.com>
2022-11-17 15:28:59 +0800

    Adjust code as recommended by code review

--- apis/dbaas/v1alpha1/configurationtemplate_types.go
+++ apis/dbaas/v1alpha1/configurationtemplate_types.go
@@ -29,49 +29,50 @@ type ConfigurationTemplateSpec struct {
 	// +kubebuilder:validation:Required
 	TplRef string `json:"tplRef,omitempty"`
 
-	// CfgSchemaTopLevelName is cue type name, which generate openapi schema.
+	// CfgSchemaTopLevelName is cue type name, which generates openapi schema.
 	// +optional
 	CfgSchemaTopLevelName string `json:"cfgSchemaTopLevelName,omitempty"`
 
-	// ConfigurationSchema that impose restrictions on engine parameter's rule.
+	// ConfigurationSchema that imposes restrictions on engine parameter's rule.
 	// +optional
 	ConfigurationSchema *CustomParametersValidation `json:"configurationSchema,omitempty"`
 
-	// After any parameters in StaticParameters are modified, the controller need to restart the engine instance.
+	// StaticParameters, list of StaticParameter, modification of it triggers a process restart.
 	// +optional
 	StaticParameters []string `json:"staticParameters,omitempty"`
 
-	// DynamicParameters field and StaticParameters field are the same.
-	// for the convenience of the ISVs, if there are more static parameters, it is recommended to use StaticParameters field, otherwise it is recommended to use DynamicParameters field.
+	// DynamicParameters, list of DynamicParameter, modification of it triggers a config dynamic reload without process restart.
 	// +optional
 	DynamicParameters []string `json:"dynamicParameters,omitempty"`
 
-	// ImmutableParameters describe parameters that prohibit user from modification.
+	// ImmutableParameters describes parameters that prohibit user from modification.
 	// +optional
 	ImmutableParameters []string `json:"immutableParameters,omitempty"`
 
-	// UpgradeMode describe parameter update mode.
+	// Node: static parameters and unknown parameters are all in StaticParameters.
+	// UpgradeMode describes parameter update mode.
 	// For ISVs, it's impossible to enumerate all parameters, and when the user modify unknown parameters, or ISVs does not configure StaticParameters or DynamicParameters field,
 	// these parameters should be treated as dynamic parameter or static parameter?
 	// if it is treated as static parameter, the engine instance will be restarted, otherwise it will be reloaded.
 	// +kubebuilder:default:Enum=dynamic
 	// +kubebuilder:validation:Enum={dynamic,static}
 	// +optional
-	UpgradeMode UpdateMode `json:"upgradeMode,omitempty"`
+	// UpgradeMode UpdateMode `json:"upgradeMode,omitempty"`
 
-	// Formatter describe the format of the configuration file,
+	// Formatter describes the format of the configuration file,
 	// the controller parses configuration file based on formatter, and analyzes the modified parameters list,
-	// and then decides what policy to use to make those modified parameters take effect.
+	// and then applies corresponding policies.
 	// +kubebuilder:default:Enum=yaml
 	// +kubebuilder:validation:Enum={dotenv,ini,yaml,json,hcl}
 	Formatter ConfigurationFormatter `json:"formatter,omitempty"`
 
+	// Node: remove immutable
 	// The configuration template is common, if it is modified incorrectly, it will affect all clusters using this configuration,
 	// we do not recommend modifying the template that is already online, but modifications are inevitable during development or CICD,
 	// so this control parameter is provided.
 	// if set to true, Configmap object referenced by TplRef will also be modified to immutable. defaulted to true.
 	// +kubebuilder:default:true
-	Immutable bool `json:"immutable,omitempty"`
+	// Immutable bool `json:"immutable,omitempty"`
 }
 
 // ConfigurationTemplateStatus defines the observed state of ConfigurationTemplate.
@@ -90,7 +91,7 @@ type ConfigurationTemplateStatus struct {
 	Message string `json:"message,omitempty"`
 
 	// observedGeneration is the most recent generation observed for this
-	// ClusterDefinition. It corresponds to the ClusterDefinition's generation, which is
+	// ClusterDefinition. It corresponds to the ConfigurationTemplate's generation, which is
 	// updated on mutation by the API Server.
 	// +optional
 	ObservedGeneration int64 `json:"observedGeneration,omitempty"`

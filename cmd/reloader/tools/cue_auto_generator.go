0f4ec51b7f70230109cc05f5b1dc1755463f26a9
sophon <sophon@infracreate.com>
2022-11-21 21:47:52 +0800

    Refine auto generator code

--- cmd/reloader/tools/cue_auto_generator.go
+++ cmd/reloader/tools/cue_auto_generator.go
@@ -28,6 +28,11 @@ const (
 	DocField           = 7
 )
 
+var (
+	prefixString = ""
+	filePath     = ""
+)
+
 type ValueType string
 
 const (
@@ -44,16 +49,15 @@ func EmptyParser(s string) (interface{}, error) {
 	return s, nil
 }
 
-var dataValueParser = map[ValueType]ValueParser{
+var ValueTypeParserMap = map[ValueType]ValueParser{
 	BooleanTYpe: func(s string) (interface{}, error) {
 		return strconv.ParseBool(s)
 	},
 	IntegerType: func(s string) (interface{}, error) {
-		if v, err := strconv.ParseInt(s, 10, 64); err != nil {
-			return strconv.ParseUint(s, 10, 64)
-		} else {
+		if v, err := strconv.ParseInt(s, 10, 64); err == nil {
 			return v, nil
 		}
+		return strconv.ParseUint(s, 10, 64)
 	},
 	FloatType: func(s string) (interface{}, error) {
 		return strconv.ParseFloat(s, 64)
@@ -63,8 +67,13 @@ var dataValueParser = map[ValueType]ValueParser{
 }
 
 func main() {
-	var filePath string
+	// file context format
+	// parameter name | default value | value restrict | parameter type |  immutable | value type | static/dynamic parameter | doc
+	// e.g.
+	// default_authentication_plugin\tmysql_native_password\tmysql_native_password, sha256_password, caching_sha2_password\tfalse\tengine-default\tstatic\tstring\tThe default authentication plugin
+
 	flag.StringVar(&filePath, "file-path", "", "The generate cue scripts from file.")
+	flag.StringVar(&prefixString, "output-prefix", prefixString, "prefix, default: \"\"")
 	flag.Parse()
 
 	f, err := os.Open(filePath)
@@ -84,33 +93,30 @@ func main() {
 			continue
 		}
 
-		processParameter(fields)
+		wrapOutputCuelang(ConstructParameterType(fields), os.Stdout)
+	}
+}
+
+func wrapOutputCuelang(parameter *ParameterType, writer io.Writer) {
+	if !validateParameter(parameter) {
+		return
 	}
 
+	wrapper := CueWrapper{
+		writer:        writer,
+		ParameterType: parameter,
+	}
+
+	wrapper.output()
 }
 
-type ParameterRestrict struct {
-	isEnum bool
-
-	Min interface{}
-	Max interface{}
-
-	EnumList []interface{}
+func validateParameter(parameter *ParameterType) bool {
+	return parameter != nil &&
+		parameter.Name != "" &&
+		parameter.Type != ""
 }
 
-type ParameterType struct {
-	Name         string
-	Type         ValueType
-	DefaultValue string
-	IsStatic     bool
-
-	Immutable         bool
-	ParameterRestrict *ParameterRestrict
-	Document          string
-}
-
-func processParameter(fields []string) {
-
+func ConstructParameterType(fields []string) *ParameterType {
 	param := ParameterType{
 		Name:         fields[NameField],
 		Type:         ValueType(fields[DataTypeField]),
@@ -142,80 +148,47 @@ func processParameter(fields []string) {
 	}
 
 	param.ParameterRestrict = parseParameterRestrict(fields[ValueRestrictField], param.Type)
-	printParameterWithCue(param, os.Stdout)
+	return &param
 }
 
-func printParameterWithCue(param ParameterType, writer io.Writer) int {
-	var (
-		buffer        bytes.Buffer
-		PREFIX_STRING = "        "
-	)
+type ParameterRestrict struct {
+	isEnum bool
 
-	if len(param.Document) > 0 {
-		buffer.WriteString(PREFIX_STRING)
-		buffer.WriteString("// ")
-		buffer.WriteString(param.Document)
-		buffer.WriteByte('\n')
-	}
+	Min interface{}
+	Max interface{}
 
-	buffer.WriteString(PREFIX_STRING)
-	if strings.ContainsAny(param.Name, "-.") {
-		buffer.WriteByte('"')
-		buffer.WriteString(param.Name)
-		buffer.WriteByte('"')
-	} else {
-		buffer.WriteString(param.Name)
-	}
-
-	if param.DefaultValue == "" {
-		buffer.WriteByte('?')
-	}
-	buffer.WriteString(": ")
-	if param.Type == IntegerType {
-		buffer.WriteString("int")
-	} else {
-		buffer.WriteString(string(param.Type))
-	}
-	restrict := param.ParameterRestrict
-	if restrict != nil {
-		printRestrictParam(&buffer, restrict, param.Type)
-	}
-
-	if len(param.DefaultValue) > 0 {
-		printDefaultValue(&buffer, param.DefaultValue, param.Type)
-	}
-
-	buffer.WriteString("\n\n")
-
-	num, _ := writer.Write(buffer.Bytes())
-	return num
+	EnumList []interface{}
 }
 
-func printDefaultValue(buffer *bytes.Buffer, value string, valueType ValueType) {
-	buffer.WriteString(" | *")
-	printElem(buffer, value, valueType)
+type ParameterType struct {
+	Name         string
+	Type         ValueType
+	DefaultValue string
+	IsStatic     bool
+
+	Immutable         bool
+	ParameterRestrict *ParameterRestrict
+	Document          string
 }
 
-func printRestrictParam(buffer *bytes.Buffer, restrict *ParameterRestrict, valueType ValueType) {
+func generateRestrictParam(buffer *bytes.Buffer, restrict *ParameterRestrict, valueType ValueType) {
 	buffer.WriteString(" & ")
 	if restrict.isEnum {
 		for i := 0; i < len(restrict.EnumList); i++ {
 			if i > 0 {
 				buffer.WriteString(" | ")
 			}
-			printElem(buffer, restrict.EnumList[i], valueType)
+			generateElemValue(buffer, restrict.EnumList[i], valueType)
 		}
 	} else {
 		buffer.WriteString(">= ")
-		printElem(buffer, restrict.Min, valueType)
+		generateElemValue(buffer, restrict.Min, valueType)
 		buffer.WriteString(" & <= ")
-		printElem(buffer, restrict.Max, valueType)
-		// buffer.WriteString(fmt.Sprintf("%v", restrict.Min))
-		// buffer.WriteString(fmt.Sprintf("%v", restrict.Max))
+		generateElemValue(buffer, restrict.Max, valueType)
 	}
 }
 
-func printElem(buffer *bytes.Buffer, value interface{}, valueType ValueType) {
+func generateElemValue(buffer *bytes.Buffer, value interface{}, valueType ValueType) {
 	if valueType == StringType {
 		buffer.WriteString("\"")
 	}
@@ -226,7 +199,7 @@ func printElem(buffer *bytes.Buffer, value interface{}, valueType ValueType) {
 }
 
 func parseValue(s string, valueType ValueType) interface{} {
-	v, err := dataValueParser[valueType](s)
+	v, err := ValueTypeParserMap[valueType](s)
 	if err != nil {
 		fmt.Printf("parse type[%s] value[%s] failed!", valueType, s)
 		panic("")
@@ -285,7 +258,7 @@ func parseListParameter(s string, valueType ValueType) *ParameterRestrict {
 			continue
 		}
 
-		if typeValue, err := dataValueParser[valueType](v); err != nil {
+		if typeValue, err := ValueTypeParserMap[valueType](v); err != nil {
 			fmt.Printf("parse failed: [%s] [%s] [%s]\n", s, v, valueType)
 			// panic(fmt.Sprintf("parse faild: [%s] [%s]", s, v))
 		} else {
@@ -304,4 +277,66 @@ func AddRestrictValue(p *ParameterRestrict, value interface{}) {
 	}
 
 	p.EnumList = append(p.EnumList, value)
+}
+
+type CueWrapper struct {
+	writer io.Writer
+	*ParameterType
+}
+
+func (w *CueWrapper) output() int {
+	var buffer bytes.Buffer
+	w.generateCueDocument(&buffer)
+	w.generateCueTypeParameter(&buffer)
+	w.generateCueRestrict(&buffer)
+	w.generateCueDefaultValue(&buffer)
+
+	_ = buffer.WriteByte('\n')
+	_ = buffer.WriteByte('\n')
+	b, _ := w.writer.Write(buffer.Bytes())
+	return b
+}
+
+func (w *CueWrapper) generateCueDefaultValue(buffer *bytes.Buffer) {
+	if w.DefaultValue == "" {
+		return
+	}
+	buffer.WriteString(" | *")
+	generateElemValue(buffer, w.DefaultValue, w.Type)
+}
+
+func (w *CueWrapper) generateCueRestrict(buffer *bytes.Buffer) {
+	if w.ParameterRestrict != nil {
+		generateRestrictParam(buffer, w.ParameterRestrict, w.Type)
+	}
+}
+
+func (w *CueWrapper) generateCueTypeParameter(buffer *bytes.Buffer) {
+	buffer.WriteString(prefixString)
+	if strings.ContainsAny(w.Name, "-.") {
+		buffer.WriteByte('"')
+		buffer.WriteString(w.Name)
+		buffer.WriteByte('"')
+	} else {
+		buffer.WriteString(w.Name)
+	}
+
+	if w.DefaultValue == "" {
+		buffer.WriteByte('?')
+	}
+	buffer.WriteString(": ")
+	if w.Type == IntegerType {
+		buffer.WriteString("int")
+	} else {
+		buffer.WriteString(string(w.Type))
+	}
+}
+
+func (w *CueWrapper) generateCueDocument(buffer *bytes.Buffer) {
+	if w.Document != "" {
+		buffer.WriteString(prefixString)
+		buffer.WriteString("// ")
+		buffer.WriteString(w.Document)
+		buffer.WriteByte('\n')
+	}
 }

60266032c3b9b48cf6d9cdd5a2758b731fee7aec
free6om <free6om.c@gmail.com>
2022-12-13 14:18:50 +0800

    mongodb test cases

--- cmd/probe/internal/binding/mongodb/mongodb.go
+++ cmd/probe/internal/binding/mongodb/mongodb.go
@@ -1,12 +1,11 @@
 /*
 Copyright ApeCloud Inc.
+Copyright 2021 The Dapr Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,8 +53,8 @@ const (
 	// mongodb://<host>/<database><params>
 	connectionURIFormat = "mongodb://%s/%s%s"
 
-	// mongodb+srv://<server>/<database><params>
-	connectionURIFormatWithSrv = "mongodb+srv://%s/%s%s"
+	// mongodb+srv://<server>/<params>
+	connectionURIFormatWithSrv = "mongodb+srv://%s/%s"
 
 	// mongodb+srv://<username>:<password>@<server>/<database><params>
 	connectionURIFormatWithSrvAndCredentials = "mongodb+srv://%s:%s@%s/%s%s" //nolint:gosec
@@ -222,6 +221,7 @@ func (m *MongoDB) GetRole(ctx context.Context, cmd string) (string, error) {
 	status, err := getReplSetStatus(ctx, m.database)
 	if err != nil {
 		m.logger.Errorf("rs.status() error: %", err)
+		return "", err
 	}
 	for _, member := range status.Members {
 		if member.State == status.MyState {

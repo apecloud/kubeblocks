977830bb80575ca10115bc104ef9a72c86edbcf4
L.DongMing <l.dongming@infracreate.com>
2022-12-13 21:17:48 +0800

    chore: replace jihulab registry  with docker (#805)

--- controllers/dbaas/clusterdefinition_controller_test.go
+++ controllers/dbaas/clusterdefinition_controller_test.go
@@ -147,7 +147,7 @@ spec:
     podSpec: 
       containers:
       - name: mysql
-        image: registry.jihulab.com/apecloud/mysql-server/mysql/wesql-server-arm:latest
+        image: docker.io/apecloud/wesql-server:latest
       - name: mysql_exporter
         image: "prom/mysqld-exporter:v0.14.0"
 `

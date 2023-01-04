0fdc2c20a52d8fce0fd48ad66787b0896b0033b9
Weston <cyg.cao@gmail.com>
2023-01-04 12:08:21 +0800

    add header and fix naming in yaml

--- apis/dbaas/v1alpha1/clusterversion_types_test.go
+++ apis/dbaas/v1alpha1/clusterversion_types_test.go
@@ -1,10 +1,26 @@
+/*
+Copyright ApeCloud Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha1
 
 import (
 	"testing"
 
 	. "github.com/onsi/gomega"
-	
+
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
@@ -15,9 +31,9 @@ func TestGetInconsistentComponentsInfoWithoutResult(t *testing.T) {
 	clusterVersion := &ClusterVersion{}
 	clusterVersionYaml := `
 apiVersion: dbaas.kubeblocks.io/v1alpha1
-kind:       AppVersion
+kind:       ClusterVersion
 metadata:
-  name:     appversion-1
+  name:     clusterversion-1
 spec:
   clusterDefinitionRef: cluster-definition-1
   components:
@@ -71,11 +87,11 @@ func TestGetInconsistentComponentsInfoWithResults(t *testing.T) {
 	clusterVersion := &ClusterVersion{}
 	clusterVersionYaml := `
 apiVersion: dbaas.kubeblocks.io/v1alpha1
-kind:       AppVersion
+kind:       ClusterVersion
 metadata:
-  name:     appversion-1
+  name:     clusterversion-2
 spec:
-  clusterDefinitionRef: cluster-definition-1
+  clusterDefinitionRef: cluster-definition-2
   components:
   - type: component1
     podSpec: 
@@ -97,7 +113,7 @@ spec:
 apiVersion: dbaas.kubeblocks.io/v1alpha1
 kind:       ClusterDefinition
 metadata:
-  name:     cluster-definition-1
+  name:     cluster-definition-2
 spec:
   components:
   - typeName: component1

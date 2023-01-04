16da9a4ba3f2bd9c0947cff5b1e5dbea18947fe6
chantu <lynnleelhl@gmail.com>
2023-01-04 18:52:29 +0800

    update

--- controllers/dbaas/lifecycle_utils.go
+++ controllers/dbaas/lifecycle_utils.go
@@ -600,10 +600,7 @@ func existsPDBSpec(pdbSpec *policyv1.PodDisruptionBudgetSpec) bool {
 func needBuildPDB(params *createParams) bool {
 	if params.component.ComponentType == dbaasv1alpha1.Consensus {
 		// if MinReplicas is non-zero, build pdb
-		if params.component.MinReplicas > 0 {
-			return true
-		}
-		return false
+		return params.component.MinReplicas > 0
 	}
 	return existsPDBSpec(params.component.PodDisruptionBudgetSpec)
 }

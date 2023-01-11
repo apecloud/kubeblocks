409e57f1987d4f9896ad1408d73e0f23613689e6
xuanchi <xuriwuyun@gmail.com>
2023-01-11 23:47:52 +0800

    update probe test

--- cmd/probe/internal/base_binding_test.go
+++ cmd/probe/internal/base_binding_test.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/dapr/components-contrib/bindings"
 	"github.com/dapr/kit/logger"
+	"github.com/spf13/viper"
 )
 
 type fakeBinding struct{}
@@ -62,6 +63,7 @@ func TestInit(t *testing.T) {
 
 func TestInvoke(t *testing.T) {
 	p := mockProbeBase()
+	viper.SetDefault("KB_SERVICE_ROLES", "{\"follower\":\"Readonly\",\"leader\":\"ReadWrite\"}")
 	p.Init()
 
 	t.Run("runningCheck", func(t *testing.T) {

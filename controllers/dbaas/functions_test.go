68b081548582cf11d929c798c46ab9019ada76b8
Weston <cyg.cao@gmail.com>
2023-01-15 01:27:24 +0800

    chore: fix the infinite loop in getAvailableContainerPorts

---
+++ controllers/dbaas/functions_test.go
@@ -0,0 +1,38 @@
+package dbaas
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"testing"
+)
+
+func TestGetAvailableContainerPortsNormal(t *testing.T) {
+	var containers []corev1.Container
+
+	destPort := 3306
+	for p := 1024; p < destPort; p++ {
+		containers = append(containers, corev1.Container{Ports:[]corev1.ContainerPort{{ContainerPort: int32(p)}}})
+	}
+
+	containerPorts := []int32 {1025}
+	foundPorts, err := getAvailableContainerPorts(containers, containerPorts)
+	if err != nil {
+		t.Error("expect getAvailableContainerPorts success")
+	}
+	if len(foundPorts) != 1 || foundPorts[0] != int32(destPort) {
+		t.Error("expect getAvailableContainerPorts returns 3306")
+	}
+}
+
+func TestGetAvailableContainerPortsError(t *testing.T) {
+	var containers []corev1.Container
+
+	for p := 1024; p <= 100000; p++ {
+		containers = append(containers, corev1.Container{Ports:[]corev1.ContainerPort{{ContainerPort: int32(p)}}})
+	}
+
+	containerPorts := []int32 {1025}
+	_, err := getAvailableContainerPorts(containers, containerPorts)
+	if err == nil {
+		t.Error("expect getAvailableContainerPorts return error")
+	}
+}

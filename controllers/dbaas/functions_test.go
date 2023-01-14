04dbbea6d1f5060cb4fd3875a3195008f6e9ab33
Weston <cyg.cao@gmail.com>
2023-01-15 01:37:22 +0800

    fix lint

--- controllers/dbaas/functions_test.go
+++ controllers/dbaas/functions_test.go
@@ -1,8 +1,9 @@
 package dbaas
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	"testing"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 func TestGetAvailableContainerPortsNormal(t *testing.T) {
@@ -10,10 +11,10 @@ func TestGetAvailableContainerPortsNormal(t *testing.T) {
 
 	destPort := 3306
 	for p := 1024; p < destPort; p++ {
-		containers = append(containers, corev1.Container{Ports:[]corev1.ContainerPort{{ContainerPort: int32(p)}}})
+		containers = append(containers, corev1.Container{Ports: []corev1.ContainerPort{{ContainerPort: int32(p)}}})
 	}
 
-	containerPorts := []int32 {1025}
+	containerPorts := []int32{1025}
 	foundPorts, err := getAvailableContainerPorts(containers, containerPorts)
 	if err != nil {
 		t.Error("expect getAvailableContainerPorts success")
@@ -27,10 +28,10 @@ func TestGetAvailableContainerPortsError(t *testing.T) {
 	var containers []corev1.Container
 
 	for p := 1024; p <= 100000; p++ {
-		containers = append(containers, corev1.Container{Ports:[]corev1.ContainerPort{{ContainerPort: int32(p)}}})
+		containers = append(containers, corev1.Container{Ports: []corev1.ContainerPort{{ContainerPort: int32(p)}}})
 	}
 
-	containerPorts := []int32 {1025}
+	containerPorts := []int32{1025}
 	_, err := getAvailableContainerPorts(containers, containerPorts)
 	if err == nil {
 		t.Error("expect getAvailableContainerPorts return error")

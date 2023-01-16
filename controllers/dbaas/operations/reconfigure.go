11bcc09c283d9d342c13cf863c63f1a481d0d254
xingran <yong.huang@infracreate.com>
2023-01-16 20:49:59 +0800

    replicationset sync configspec

--- controllers/dbaas/operations/reconfigure.go
+++ controllers/dbaas/operations/reconfigure.go
@@ -22,8 +22,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	dbaasv1alpha1 "github.com/apecloud/kubeblocks/apis/dbaas/v1alpha1"
 	cfgcore "github.com/apecloud/kubeblocks/internal/configuration"
 )

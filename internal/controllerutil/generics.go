c46d7506aa98f66cecc0cd584c2638cc3f727611
Weston <cyg.cao@gmail.com>
2023-01-18 12:46:26 +0800

    fix: external resource leaks when delete cluster

--- internal/controllerutil/generics.go
+++ internal/controllerutil/generics.go
@@ -3,6 +3,7 @@ package controllerutil
 import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	policyv1 "k8s.io/api/policy/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -51,3 +52,26 @@ type StatefulSetListWrapper struct{}
 func (w StatefulSetListWrapper) GetItems(list *appsv1.StatefulSetList) []appsv1.StatefulSet {
 	return list.Items
 }
+
+// DeploymentListWrapper ObjListWrapper of appsv1.DeploymentList
+type DeploymentListWrapper struct{}
+
+func (w DeploymentListWrapper) GetItems(list *appsv1.DeploymentList) []appsv1.Deployment {
+	return list.Items
+}
+
+// ConfigMapListWrapper ObjListWrapper of corev1.ConfigMapList
+type ConfigMapListWrapper struct{}
+
+func (w ConfigMapListWrapper) GetItems(list *corev1.ConfigMapList) []corev1.ConfigMap {
+	return list.Items
+}
+
+// PodDisruptionBudgetListWrapper ObjListWrapper of policyv1.PodDisruptionBudgetList
+type PodDisruptionBudgetListWrapper struct{}
+
+func (w PodDisruptionBudgetListWrapper) GetItems(list *policyv1.PodDisruptionBudgetList) []policyv1.PodDisruptionBudget {
+	return list.Items
+}
+
+

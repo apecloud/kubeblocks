523e733547358aeccddf0181b3139e4386c4ba9c
Weston <cyg.cao@gmail.com>
2023-01-24 02:33:31 +0800

    fix a condition logic

--- internal/testutil/dbaas/common_util.go
+++ internal/testutil/dbaas/common_util.go
@@ -146,7 +146,8 @@ func ClearClusterResources(testCtx *testutil.TestContext) {
 		client.HasLabels{testCtx.TestObjLabelKey})
 
 	// mock behavior of garbage collection inside KCM
-	if testCtx.TestEnv.UseExistingCluster != nil && *testCtx.TestEnv.UseExistingCluster {
+	if !testCtx.UsingExistingCluster() || !(testCtx.TestEnv.UseExistingCluster != nil &&
+		*testCtx.TestEnv.UseExistingCluster) {
 		ClearResources(testCtx, intctrlutil.StatefulSetSignature, inNS)
 		ClearResources(testCtx, intctrlutil.DeploymentSignature, inNS)
 		ClearResources(testCtx, intctrlutil.ConfigMapSignature, inNS)

8f0347e4f3a751c11fa4144575cfb936d4f7c2cf
sophon <sophon@infracreate.com>
2023-02-02 17:08:03 +0800

    chore: Minor adjust (#1173)

--- internal/configuration/config_validate_test.go
+++ internal/configuration/config_validate_test.go
@@ -18,7 +18,6 @@ package configuration
 
 import (
 	"errors"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -27,68 +26,18 @@ import (
 	"github.com/apecloud/kubeblocks/test/testdata"
 )
 
-var toMap = func(str string) map[string]string {
-	return map[string]string{
-		"noKey": str,
+var fromTestData = func(fileName string) string {
+	content, err := testdata.GetTestDataFileContent(fileName)
+	if err != nil {
+		panic(err)
 	}
+	return string(content)
 }
 
-func TestSchemaValidatorWithCue(t *testing.T) {
-	// not config validate
-	{
-		validator := NewConfigValidator(&dbaasv1alpha1.ConfigConstraintSpec{})
-		require.NotNil(t, validator)
-		require.Nil(t, validator.Validate(nil))
-	}
-
-	{
-		validator := NewConfigValidator(fakeConfigurationTpl("./cue_testdata/wesql.cue", dbaasv1alpha1.INI))
-		require.Nil(t, validator.Validate(toMap(loadTestData("./cue_testdata/wesql.cnf"))))
-	}
-
-	{
-		validator := NewConfigValidator(fakeConfigurationTpl("./cue_testdata/pg14.cue", dbaasv1alpha1.DOTENV))
-		require.Nil(t, validator.Validate(toMap(loadTestData("./cue_testdata/pg14.conf"))))
-	}
-
-	// cue validate for ini
-	{
-		validator := NewConfigValidator(fakeConfigurationTpl("./cue_testdata/mysql.cue", dbaasv1alpha1.INI))
-		require.Nil(t, validator.Validate(toMap(loadTestData("./cue_testdata/mysql.cnf"))))
-		expectErr := errors.New(`failed to cue template render configure: [mysqld.innodb_autoinc_lock_mode: 3 errors in empty disjunction:
-mysqld.innodb_autoinc_lock_mode: conflicting values 0 and 100:
-    28:35
-mysqld.innodb_autoinc_lock_mode: conflicting values 1 and 100:
-    28:39
-mysqld.innodb_autoinc_lock_mode: conflicting values 2 and 100:
-    28:43
-]`)
-		require.Equal(t, expectErr, validator.Validate(toMap(loadTestData("./cue_testdata/mysql_err.cnf"))))
-	}
-
-	// cue validate for xml
-	{
-		validator := NewConfigValidator(fakeConfigurationTpl("./cue_testdata/clickhouse.cue", dbaasv1alpha1.XML))
-		require.Nil(t, validator.Validate(toMap(loadTestData("./cue_testdata/clickhouse.xml"))))
-	}
-
-}
-
-func TestSchemaValidatorWithOpenSchema(t *testing.T) {
-	tpl := fakeConfigurationTpl("./cue_testdata/mysql.cue", dbaasv1alpha1.INI)
-	validator := &schemaValidator{
-		typeName: tpl.CfgSchemaTopLevelName,
-		cfgType:  tpl.FormatterConfig.Formatter,
-		schema:   tpl.ConfigurationSchema.Schema,
-	}
-
-	require.Nil(t, validator.Validate(toMap(loadTestData("./cue_testdata/mysql.cnf"))))
-}
-
-func fakeConfigurationTpl(cuefile string, cfgFormatter dbaasv1alpha1.ConfigurationFormatter) *dbaasv1alpha1.ConfigConstraintSpec {
+var newFakeConfConstraint = func(cueFile string, cfgFormatter dbaasv1alpha1.ConfigurationFormatter) *dbaasv1alpha1.ConfigConstraintSpec {
 	return &dbaasv1alpha1.ConfigConstraintSpec{
 		ConfigurationSchema: &dbaasv1alpha1.CustomParametersValidation{
-			CUE: loadTestData(cuefile),
+			CUE: fromTestData(cueFile),
 		},
 		FormatterConfig: &dbaasv1alpha1.FormatterConfig{
 			Formatter: cfgFormatter,
@@ -96,7 +45,110 @@ func fakeConfigurationTpl(cuefile string, cfgFormatter dbaasv1alpha1.Configurati
 	}
 }
 
-func loadTestData(fileName string) string {
-	content, _ := os.ReadFile(testdata.SubTestDataPath(fileName))
-	return string(content)
+func TestSchemaValidatorWithCue(t *testing.T) {
+	type args struct {
+		cueFile    string
+		configFile string
+		formatter  dbaasv1alpha1.ConfigurationFormatter
+	}
+	tests := []struct {
+		name string
+		args args
+		err  error
+	}{{
+		name: "test_wesql",
+		args: args{
+			cueFile:    "cue_testdata/wesql.cue",
+			configFile: "cue_testdata/wesql.cnf",
+			formatter:  dbaasv1alpha1.INI,
+		},
+		err: nil,
+	}, {
+		name: "test_pg14",
+		args: args{
+			cueFile:    "cue_testdata/pg14.cue",
+			configFile: "cue_testdata/pg14.conf",
+			formatter:  dbaasv1alpha1.DOTENV,
+		},
+		err: nil,
+	}, {
+		name: "test_ck",
+		args: args{
+			cueFile:    "cue_testdata/clickhouse.cue",
+			configFile: "cue_testdata/clickhouse.xml",
+			formatter:  dbaasv1alpha1.XML,
+		},
+		err: nil,
+	}, {
+		name: "test_mysql",
+		args: args{
+			cueFile:    "cue_testdata/mysql.cue",
+			configFile: "cue_testdata/mysql.cnf",
+			formatter:  dbaasv1alpha1.INI,
+		},
+		err: nil,
+	}, {
+		name: "test_failed",
+		args: args{
+			cueFile:    "cue_testdata/mysql.cue",
+			configFile: "cue_testdata/mysql_err.cnf",
+			formatter:  dbaasv1alpha1.INI,
+		},
+		err: errors.New(`failed to cue template render configure: [mysqld.innodb_autoinc_lock_mode: 3 errors in empty disjunction:
+mysqld.innodb_autoinc_lock_mode: conflicting values 0 and 100:
+    28:35
+mysqld.innodb_autoinc_lock_mode: conflicting values 1 and 100:
+    28:39
+mysqld.innodb_autoinc_lock_mode: conflicting values 2 and 100:
+    28:43
+]`),
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			validator := NewConfigValidator(newFakeConfConstraint(tt.args.cueFile, tt.args.formatter))
+			require.NotNil(t, validator)
+			require.Equal(t, tt.err, validator.Validate(
+				map[string]string{
+					"key": fromTestData(tt.args.configFile),
+				}))
+		})
+	}
+}
+
+func TestSchemaValidatorWithOpenSchema(t *testing.T) {
+	type args struct {
+		cueFile        string
+		configFile     string
+		formatter      dbaasv1alpha1.ConfigurationFormatter
+		SchemaTypeName string
+	}
+	tests := []struct {
+		name string
+		args args
+		err  error
+	}{{
+		name: "test_wesql",
+		args: args{
+			cueFile:    "cue_testdata/mysql.cue",
+			configFile: "cue_testdata/mysql.cnf",
+			formatter:  dbaasv1alpha1.INI,
+		},
+		err: nil,
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tplConstraint := newFakeConfConstraint(tt.args.cueFile, tt.args.formatter)
+			validator := &schemaValidator{
+				typeName: tt.args.SchemaTypeName,
+				cfgType:  tplConstraint.FormatterConfig.Formatter,
+				schema:   tplConstraint.ConfigurationSchema.Schema,
+			}
+			require.Equal(t, tt.err, validator.Validate(
+				map[string]string{
+					"key": fromTestData(tt.args.configFile),
+				}))
+		})
+	}
 }

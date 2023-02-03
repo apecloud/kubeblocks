a69bc658821afa853640669a4438739b23e69196
free6om <free6om.c@gmail.com>
2023-02-03 17:29:28 +0800

    Revert "comment out uncached objects, temporarily"
    This reverts commit caa34450838372b7bbbd8b6a07a39b2f66a5f596.

--- controllers/dbaas/config_template_test.go
+++ controllers/dbaas/config_template_test.go
@@ -24,11 +24,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	dbaasv1alpha1 "github.com/apecloud/kubeblocks/apis/dbaas/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	dbaasv1alpha1 "github.com/apecloud/kubeblocks/apis/dbaas/v1alpha1"
 )
 
 type insClassType struct {

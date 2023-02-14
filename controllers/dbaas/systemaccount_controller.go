16e50bcb7a954cc7a92086cd202f1f5830f0649d
shanshanying <111829739+shanshanying@users.noreply.github.com>
2023-02-14 10:12:58 +0800

    chore: sysaccount refactored testcases using factory (#1414)

--- controllers/dbaas/systemaccount_controller.go
+++ controllers/dbaas/systemaccount_controller.go
@@ -230,7 +230,7 @@ func (r *SystemAccountReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 			isReady, svcEP, headlessEP, err := r.isComponentReady(reqCtx, cluster.Name, compName)
 			if err != nil {
-				return intctrlutil.RequeueWithErrorAndRecordEvent(cluster, r.Recorder, err, reqCtx.Log)
+				return intctrlutil.RequeueAfter(ControllerErrorRequeueTime, reqCtx.Log, "failed to get service")
 			}
 
 			// either service or endpoint is not ready, increase counter and continue to process next component

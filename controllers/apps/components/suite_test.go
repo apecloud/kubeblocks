c962c2c228d611bd4fdcb22314600d4a3b1d9c0a
Wei Cao <cyg.cao@gmail.com>
2023-02-22 12:05:07 +0800

    rm debug code

--- controllers/apps/components/suite_test.go
+++ controllers/apps/components/suite_test.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"path/filepath"
 	"testing"
-	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -119,7 +118,6 @@ var _ = BeforeSuite(func() {
 	}).SetupWithManager(k8sManager)
 	Expect(err).ToNot(HaveOccurred())
 
-	viper.Set("EventuallyTimeout", time.Second*1000000)
 	testCtx = testutil.NewDefaultTestContext(ctx, k8sClient, testEnv)
 
 	go func() {

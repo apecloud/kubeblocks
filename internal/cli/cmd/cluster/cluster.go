238ba8d053edd04043e563d7bf9ccff8f66d8f47
Ziang Guo <ziang.gza@infracreate.com>
2023-02-28 13:24:03 +0800

    feat: support expose cluster to vpc or internet on EKS (#1659)

--- internal/cli/cmd/cluster/cluster.go
+++ internal/cli/cmd/cluster/cluster.go
@@ -52,6 +52,7 @@ func NewClusterCmd(f cmdutil.Factory, streams genericclioptions.IOStreams) *cobr
 				NewListEventsCmd(f, streams),
 				NewListAccountsCmd(f, streams),
 				NewDeleteCmd(f, streams),
+				NewExposeCmd(f, streams),
 			},
 		},
 		{
@@ -97,10 +98,6 @@ func NewClusterCmd(f cmdutil.Factory, streams genericclioptions.IOStreams) *cobr
 	// add subcommands
 	groups.Add(cmd)
 	templates.ActsAsRootCommand(cmd, nil, groups...)
-
-	if viper.GetString(EnvExperimentalExpose) == "1" {
-		cmd.AddCommand(NewExposeCmd(f, streams))
-	}
 
 	return cmd
 }

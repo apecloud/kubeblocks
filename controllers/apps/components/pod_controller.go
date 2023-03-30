6708c3353b4a8dc6cfbacfdaae2922cfca86a9fa
chantu <lynnleelhl@gmail.com>
2023-03-30 16:35:18 +0800

    remove todo

--- controllers/apps/components/pod_controller.go
+++ controllers/apps/components/pod_controller.go
@@ -46,10 +46,6 @@ type PodReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the ClusterVersion object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile

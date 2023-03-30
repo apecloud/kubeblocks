f8efe8b0fea8aa774451f403ff89c109b250e6e5
free6om <free6om.c@gmail.com>
2023-03-30 13:17:48 +0800

    API Spec done

--- apis/workloads/v1alpha1/consensusset_types.go
+++ apis/workloads/v1alpha1/consensusset_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -25,17 +26,58 @@ import (
 
 // ConsensusSetSpec defines the desired state of ConsensusSet
 type ConsensusSetSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Replicas defines number of Pods
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=0
+	// +optional
+	Replicas int32 `json:"replicas,omitempty"`
 
-	// Foo is an example field of ConsensusSet. Edit consensusset_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	ServiceName string `json:"serviceName"`
+
+	Template corev1.PodTemplateSpec `json:"template"`
+
+	// Leader, one single leader.
+	// +kubebuilder:validation:Required
+	Leader ConsensusMember `json:"leader"`
+
+	// Followers, has voting right but not Leader.
+	// +optional
+	Followers []ConsensusMember `json:"followers,omitempty"`
+
+	// Learner, no voting right.
+	// +optional
+	Learner *ConsensusMember `json:"learner,omitempty"`
+
+	// RoleObservation provides method to observe role.
+	RoleObservation RoleObservation `json:"roleObservation"`
+
+	// UpdateStrategy, Pods update strategy.
+	// serial: update Pods one by one that guarantee minimum component unavailable time.
+	// 		Learner -> Follower(with AccessMode=none) -> Follower(with AccessMode=readonly) -> Follower(with AccessMode=readWrite) -> Leader
+	// bestEffortParallel: update Pods in parallel that guarantee minimum component un-writable time.
+	//		Learner, Follower(minority) in parallel -> Follower(majority) -> Leader, keep majority online all the time.
+	// parallel: force parallel
+	// +kubebuilder:default=Serial
+	// +kubebuilder:validation:Enum={Serial,BestEffortParallel,Parallel}
+	// +optional
+	UpdateStrategy UpdateStrategy `json:"updateStrategy,omitempty"`
 }
 
 // ConsensusSetStatus defines the observed state of ConsensusSet
 type ConsensusSetStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// leader status.
+	// +kubebuilder:validation:Required
+	Leader ConsensusMemberStatus `json:"leader"`
+
+	// followers status.
+	// +optional
+	Followers []ConsensusMemberStatus `json:"followers,omitempty"`
+
+	// learner status.
+	// +optional
+	Learner *ConsensusMemberStatus `json:"learner,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -57,6 +99,84 @@ type ConsensusSetList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []ConsensusSet `json:"items"`
+}
+
+type ConsensusMember struct {
+	// Name, role name.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default=leader
+	Name string `json:"name"`
+
+	// AccessMode, what service this member capable.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default=ReadWrite
+	// +kubebuilder:validation:Enum={None, Readonly, ReadWrite}
+	AccessMode AccessMode `json:"accessMode"`
+
+	// Replicas, number of Pods of this role.
+	// default 1 for Leader
+	// default 0 for Learner
+	// default Components[*].Replicas - Leader.Replicas - Learner.Replicas for Followers
+	// +kubebuilder:default=0
+	// +kubebuilder:validation:Minimum=0
+	// +optional
+	Replicas *int32 `json:"replicas,omitempty"`
+}
+
+// AccessMode define SVC access mode enums.
+// +enum
+type AccessMode string
+
+const (
+	ReadWriteMode AccessMode = "ReadWrite"
+	ReadonlyMode  AccessMode = "Readonly"
+	NoneMode      AccessMode = "None"
+)
+
+// UpdateStrategy define Cluster Component update strategy.
+// +enum
+type UpdateStrategy string
+
+const (
+	SerialUpdateStrategy             UpdateStrategy = "Serial"
+	BestEffortParallelUpdateStrategy UpdateStrategy = "BestEffortParallel"
+	ParallelUpdateStrategy           UpdateStrategy = "Parallel"
+)
+
+// RoleObservation defines method to observe role
+type RoleObservation struct {
+	Kind string `json:"kind"`
+
+	// Container will be run as a sidecar container
+	// +optional
+	Container *corev1.Container `json:"container,omitempty"`
+
+	// RoleProbe will be executed against Container to retrieve role info
+	RoleProbe corev1.Probe `json:"roleProbe,omitempty"`
+
+	// PostProcessCommand, processes role info returned from RoleProbe,
+	// to get a single string of the role name.
+	// all commands are from Container, or [BusyBox](https://busybox.net/) if Container not configured
+	// +optional
+	PostProcessCommand []string `json:"postProcessCommand,omitempty"`
+}
+
+type ConsensusMemberStatus struct {
+	// RoleName role name.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default=Unknown
+	RoleName string `json:"roleName"`
+
+	// accessMode, what service this pod provides.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Enum={None, Readonly, ReadWrite}
+	// +kubebuilder:default=None
+	AccessMode AccessMode `json:"accessMode"`
+
+	// PodName pod name.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default=Unknown
+	PodName string `json:"podName"`
 }
 
 func init() {

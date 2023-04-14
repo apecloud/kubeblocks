9c51d6b9eb0e29908f7e2af39ac063a040e9eab6
free6om <free6om.c@gmail.com>
2023-04-14 10:30:35 +0800

    handle ops request processing condition

--- internal/controller/lifecycle/transformer_init.go
+++ internal/controller/lifecycle/transformer_init.go
@@ -17,7 +17,11 @@ limitations under the License.
 package lifecycle
 
 import (
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	appsv1alpha1 "github.com/apecloud/kubeblocks/apis/apps/v1alpha1"
+	opsutil "github.com/apecloud/kubeblocks/controllers/apps/operations/util"
 	"github.com/apecloud/kubeblocks/internal/controller/graph"
 )
 
@@ -26,9 +30,37 @@ type initTransformer struct {
 	originCluster *appsv1alpha1.Cluster
 }
 
-func (i *initTransformer) Transform(ctx graph.TransformContext, dag *graph.DAG) error {
+func (t *initTransformer) Transform(ctx graph.TransformContext, dag *graph.DAG) error {
 	// put the cluster object first, it will be root vertex of DAG
-	rootVertex := &lifecycleVertex{obj: i.cluster, oriObj: i.originCluster}
+	rootVertex := &lifecycleVertex{obj: t.cluster, oriObj: t.originCluster}
 	dag.AddVertex(rootVertex)
+
+	if !isClusterDeleting(*t.cluster) {
+		t.handleLatestOpsRequestProcessingCondition()
+	}
 	return nil
 }
+
+// updateLatestOpsRequestProcessingCondition handles the latest opsRequest processing condition.
+func (t *initTransformer) handleLatestOpsRequestProcessingCondition() {
+	opsRecords, _ := opsutil.GetOpsRequestSliceFromCluster(t.cluster)
+	if len(opsRecords) == 0 {
+		return
+	}
+	ops := opsRecords[0]
+	opsBehaviour, ok := appsv1alpha1.OpsRequestBehaviourMapper[ops.Type]
+	if !ok {
+		return
+	}
+	opsCondition := newOpsRequestProcessingCondition(ops.Name, string(ops.Type), opsBehaviour.ProcessingReasonInClusterCondition)
+	oldCondition := meta.FindStatusCondition(t.cluster.Status.Conditions, opsCondition.Type)
+	if oldCondition == nil {
+		// if this condition not exists, insert it to the first position.
+		opsCondition.LastTransitionTime = metav1.Now()
+		t.cluster.Status.Conditions = append([]metav1.Condition{opsCondition}, t.cluster.Status.Conditions...)
+	} else {
+		meta.SetStatusCondition(&t.cluster.Status.Conditions, opsCondition)
+	}
+}
+
+var _ graph.Transformer = &initTransformer{}

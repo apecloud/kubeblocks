d13523bb2f8bff5e89b5af058099abdfa448ca16
wangyelei <yelei.wang@infracreate.com>
2023-04-19 15:29:43 +0800

    fix: vscale command is incorrect in kbcli describe-ops (#2728)

--- internal/cli/cmd/cluster/describe_ops.go
+++ internal/cli/cmd/cluster/describe_ops.go
@@ -270,10 +270,8 @@ func (o *describeOpsOptions) getVerticalScalingCommand(spec appsv1alpha1.OpsRequ
 		resource := resourceSlice[i].(corev1.ResourceRequirements)
 		commands[i] = fmt.Sprintf("kbcli cluster vscale %s --components=%s",
 			spec.ClusterRef, strings.Join(componentNameSlice[i], ","))
-		commands[i] += o.addResourceFlag("requests.cpu", resource.Requests.Cpu())
-		commands[i] += o.addResourceFlag("requests.memory", resource.Requests.Memory())
-		commands[i] += o.addResourceFlag("limits.cpu", resource.Limits.Cpu())
-		commands[i] += o.addResourceFlag("limits.memory", resource.Limits.Memory())
+		commands[i] += o.addResourceFlag("cpu", resource.Limits.Cpu())
+		commands[i] += o.addResourceFlag("memory", resource.Limits.Memory())
 	}
 	return commands
 }

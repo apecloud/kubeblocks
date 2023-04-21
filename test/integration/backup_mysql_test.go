0a1c4c5162b5843dbd4046c7bde4923dd0a1023f
ahjing99 <107018199+ahjing99@users.noreply.github.com>
2023-04-21 18:33:05 +0800

    Update Copyright and license

--- test/integration/backup_mysql_test.go
+++ test/integration/backup_mysql_test.go
@@ -1,7 +1,7 @@
 /*
 Copyright (C) 2022 ApeCloud Co., Ltd
 
-# This file is part of KubeBlocks project
+This file is part of KubeBlocks project
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -16,6 +16,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 package appstest
 
 import (

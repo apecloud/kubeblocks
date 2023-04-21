265d5bd317ed9a7566688cdb0c919932b8d4f7fd
Nash Tsai <nash.tsai@gmail.com>
2023-04-21 22:41:39 +0800

    chore: fixed scripts typo

--- .github/utils/merge_releasing_pr.sh
+++ .github/utils/merge_releasing_pr.sh
@@ -6,9 +6,9 @@ set -o pipefail
 
 # requires `git` and `gh` commands, ref. https://cli.github.com/manual/installation for installation guides.
 
-worddir=$(dirname $0)
-. ${worddir}/gh_env
-. ${worddir}/functions.bash
+workdir=$(dirname $0)
+. ${workdir}/gh_env
+. ${workdir}/functions.bash
 
 echo "Merging ${PR_TITLE}"
 
@@ -24,11 +24,4 @@ echo "pr_mergeable=${pr_mergeable}"
 
 if [ "${pr_merge_status}" == "CLEAN" ] && [ "${pr_mergeable}" == "MERGEABLE" ]; then
     gh pr --repo apecloud/kubeblocks merge ${pr_number} --merge
-# # gh API ref: https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#merge-a-pull-request
-# gh api \
-#   --method PUT \
-#   --header "Accept: application/vnd.github+json" \
-#   --header "X-GitHub-Api-Version: 2022-11-28" \
-#   /repos/${OWNER}/${REPO}/pulls/${pr_number}/merge \
-#   -f merge_method=merge
 fi

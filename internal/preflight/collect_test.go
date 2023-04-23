ab54adbece7dfed85d9f4e61d21b95e04db2b5b9
kubeJocker <102039539+kubeJocker@users.noreply.github.com>
2023-04-23 15:37:48 +0800

    refactor: redefine preflight api (#2775)

--- internal/preflight/collect_test.go
+++ internal/preflight/collect_test.go
@@ -97,7 +97,7 @@ var _ = Describe("collect_test", func() {
 					g.Expect(<-progressCh).NotTo(BeNil())
 				}
 			}()
-			results, err := CollectPreflight(context.TODO(), preflight, hostPreflight, progressCh)
+			results, err := CollectPreflight(tf, context.TODO(), preflight, hostPreflight, progressCh)
 			g.Expect(err).NotTo(HaveOccurred())
 			g.Expect(len(results)).Should(BeNumerically(">=", 3))
 		}).WithTimeout(timeOut).Should(Succeed())
@@ -126,7 +126,7 @@ var _ = Describe("collect_test", func() {
 					g.Expect(<-progressCh).NotTo(BeNil())
 				}
 			}()
-			collectResult, err := CollectRemoteData(context.TODO(), &preflightv1beta2.HostPreflight{}, progressCh)
+			collectResult, err := CollectRemoteData(context.TODO(), &preflightv1beta2.HostPreflight{}, tf, progressCh)
 			g.Expect(err).NotTo(HaveOccurred())
 			g.Expect(collectResult).NotTo(BeNil())
 		}).WithTimeout(timeOut).Should(Succeed())

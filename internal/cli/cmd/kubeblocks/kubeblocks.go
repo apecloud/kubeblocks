41a00e0b730b60349d8a26340d3ea5537958f8f0
kubeJocker <102039539+kubeJocker@users.noreply.github.com>
2023-04-23 15:37:48 +0800

    refactor: redefine preflight api (#2775)
    (cherry picked from commit ab54adbece7dfed85d9f4e61d21b95e04db2b5b9)

--- internal/cli/cmd/kubeblocks/kubeblocks.go
+++ internal/cli/cmd/kubeblocks/kubeblocks.go
@@ -40,8 +40,7 @@ func NewKubeBlocksCmd(f cmdutil.Factory, streams genericclioptions.IOStreams) *c
 		newStatusCmd(f, streams),
 		NewConfigCmd(f, streams),
 		NewDescribeConfigCmd(f, streams),
+		NewPreflightCmd(f, streams),
 	)
-	// add preflight cmd
-	cmd.AddCommand(NewPreflightCmd(f, streams))
 	return cmd
 }

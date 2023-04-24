a7e0017864b0b2b2eb6244d313c9f43c3a83e0f4
Ziang Guo <ziang.gza@infracreate.com>
2023-04-24 10:21:55 +0800

    chore: rename aws-loadbalancer-controller to aws-load-balancer-controller and add affinity (#2870)

--- internal/cli/cmd/playground/init.go
+++ internal/cli/cmd/playground/init.go
@@ -427,9 +427,9 @@ func (o *initOptions) installKubeBlocks(k8sClusterName string) error {
 			"prometheus.alertmanager.statefulSet.enabled=false")
 	} else if o.cloudProvider == cp.AWS {
 		insOpts.ValueOpts.Values = append(insOpts.ValueOpts.Values,
-			// enable aws loadbalancer controller addon automatically on playground
-			"aws-loadbalancer-controller.enabled=true",
-			fmt.Sprintf("aws-loadbalancer-controller.clusterName=%s", k8sClusterName),
+			// enable aws-load-balancer-controller addon automatically on playground
+			"aws-load-balancer-controller.enabled=true",
+			fmt.Sprintf("aws-load-balancer-controller.clusterName=%s", k8sClusterName),
 		)
 	}
 

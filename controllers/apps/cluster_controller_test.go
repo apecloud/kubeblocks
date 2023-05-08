867e57e6cdb4e13484631a17347cd253af4795c1
chantu <lynnleelhl@gmail.com>
2023-05-08 22:19:53 +0800

    fix: drop followers only when scaling in (#3130)
    (cherry picked from commit 8b24b6662bbd65e99e12ed0aac3e62f1831c8d8e)

--- controllers/apps/cluster_controller_test.go
+++ controllers/apps/cluster_controller_test.go
@@ -28,6 +28,8 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	snapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
 	"github.com/spf13/viper"
@@ -411,6 +413,7 @@ var _ = Describe("Cluster Controller", func() {
 					Name:      stsName + "-" + strconv.Itoa(i),
 					Namespace: testCtx.DefaultNamespace,
 					Labels: map[string]string{
+						constant.AppManagedByLabelKey:         constant.AppName,
 						constant.AppInstanceLabelKey:          clusterName,
 						constant.KBAppComponentLabelKey:       componentName,
 						appsv1.ControllerRevisionHashLabelKey: "mock-version",
@@ -921,9 +924,10 @@ var _ = Describe("Cluster Controller", func() {
 			sts = &stsList.Items[0]
 		}).Should(Succeed())
 
-		By("Creating mock pods in StatefulSet")
+		By("Creating mock pods in StatefulSet, and set controller reference")
 		pods := mockPodsForConsensusTest(clusterObj, replicas)
 		for _, pod := range pods {
+			Expect(controllerutil.SetControllerReference(sts, &pod, scheme.Scheme)).Should(Succeed())
 			Expect(testCtx.CreateObj(testCtx.Ctx, &pod)).Should(Succeed())
 			// mock the status to pass the isReady(pod) check in consensus_set
 			pod.Status.Conditions = []corev1.PodCondition{{
@@ -960,6 +964,17 @@ var _ = Describe("Cluster Controller", func() {
 			g.Expect(leaderCount).Should(Equal(1))
 			g.Expect(followerCount).Should(Equal(2))
 		}).Should(Succeed())
+
+		By("Checking pods' annotations")
+		Eventually(func(g Gomega) {
+			pods, err := util.GetPodListByStatefulSet(ctx, k8sClient, sts)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(len(pods)).Should(Equal(int(*sts.Spec.Replicas)))
+			for _, pod := range pods {
+				g.Expect(pod.Annotations).ShouldNot(BeNil())
+				g.Expect(pod.Annotations[constant.ComponentReplicasAnnotationKey]).Should(Equal(strconv.Itoa(int(*sts.Spec.Replicas))))
+			}
+		}, time.Second*100).Should(Succeed())
 
 		By("Updating StatefulSet's status")
 		sts.Status.UpdateRevision = "mock-version"

d2d6aca33932e51d675ff6a720ca78ead1498617
xingran <warnroot007@gmail.com>
2023-05-12 17:18:08 +0800

    chore: rename patroni scope and remove dependency resource deleted check (#3215)

--- internal/controller/plan/builtin_env_test.go
+++ internal/controller/plan/builtin_env_test.go
@@ -201,6 +201,7 @@ bootstrap:
 		cluster = &appsv1alpha1.Cluster{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "my",
+				UID:  "b006a20c-fb03-441c-bffa-2605cad7e297",
 			},
 		}
 		cfgTemplate = []appsv1alpha1.ComponentConfigSpec{{

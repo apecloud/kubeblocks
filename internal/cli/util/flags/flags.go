52f2ecd05f6d7a58fc8ca2931d5d64bf8524dab1
1aal <alal@zju.edu.cn>
2023-05-13 11:17:37 +0800

    fix the interface and spelling mistakes

--- internal/cli/util/flags/flags.go
+++ internal/cli/util/flags/flags.go
@@ -9,12 +9,12 @@ import (
 	"github.com/apecloud/kubeblocks/internal/cli/util"
 )
 
-// AddAddClusterDefinitionFlag add a flag "cluster-definition" for the cmd and store the value of the flag
+// AddClusterDefinitionFlag add a flag "cluster-definition" for the cmd and store the value of the flag
 // in string p
-func AddAddClusterDefinitionFlag(f *cmdutil.Factory, cmd *cobra.Command, p *string) {
+func AddClusterDefinitionFlag(f cmdutil.Factory, cmd *cobra.Command, p *string) {
 	cmd.Flags().StringVar(p, "cluster-definition", "", "list the clusterVersion belonging to the specified cluster definition")
 	util.CheckErr(cmd.RegisterFlagCompletionFunc("cluster-definition",
 		func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-			return utilcomp.CompGetResource(*f, cmd, util.GVRToString(types.ClusterDefGVR()), toComplete), cobra.ShellCompDirectiveNoFileComp
+			return utilcomp.CompGetResource(f, cmd, util.GVRToString(types.ClusterDefGVR()), toComplete), cobra.ShellCompDirectiveNoFileComp
 		}))
 }

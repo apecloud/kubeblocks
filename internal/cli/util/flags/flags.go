4d727cb0106ee82bfefdc15e46f4649d21b357d6
1aal <alal@zju.edu.cn>
2023-05-15 20:15:38 +0800

    use a more suitable description

--- internal/cli/util/flags/flags.go
+++ internal/cli/util/flags/flags.go
@@ -12,7 +12,7 @@ import (
 // AddClusterDefinitionFlag add a flag "cluster-definition" for the cmd and store the value of the flag
 // in string p
 func AddClusterDefinitionFlag(f cmdutil.Factory, cmd *cobra.Command, p *string) {
-	cmd.Flags().StringVar(p, "cluster-definition", *p, "list the resources related to the specified cluster definition")
+	cmd.Flags().StringVar(p, "cluster-definition", *p, "Specify cluster definition, run \"kbcli clusterdefinition list\" to show all available cluster definition")
 	util.CheckErr(cmd.RegisterFlagCompletionFunc("cluster-definition",
 		func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			return utilcomp.CompGetResource(f, cmd, util.GVRToString(types.ClusterDefGVR()), toComplete), cobra.ShellCompDirectiveNoFileComp

a8e727e21fdbc2f0332f04d3752a014011e15b23
sophon <sophon@infracreate.com>
2023-05-16 16:21:40 +0800

    chore: update appendonly when failover

--- deploy/redis-for-dynamic-switchover/scripts/redis-process-switchover.sh
+++ deploy/redis-for-dynamic-switchover/scripts/redis-process-switchover.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -ex
 
+REDISCLI="/kb/tools/redis-cli"
 
 current_role=$(cat $1)
-
 echo "current pod changed to $current_role"
+
+#appendonly yes
+value="no"
+
+if [ "x$current_role" == "xprimary" ];then
+value="yes"
+fi
+
+$REDISCLI -h 127.0.0.1 -p 6379 CONFIG SET appendonly "$value"

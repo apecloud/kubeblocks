c89cc834839e4e400239947669ce0a93d1f5af10
huyongqii <yongqi_hu@163.com>
2023-05-28 18:25:29 +0800

    support cli create --edit

--- internal/cli/create/create.go
+++ internal/cli/create/create.go
@@ -20,26 +20,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package create
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"embed"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/ast"
 	"cuelang.org/go/cue/cuecontext"
 	cuejson "cuelang.org/go/encoding/json"
 	"github.com/leaanthony/debme"
-	"github.com/spf13/cobra"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/printers"
-	res "k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/cli-runtime/pkg/resource"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
@@ -47,6 +52,7 @@ import (
 	"k8s.io/kubectl/pkg/scheme"
 
 	"github.com/apecloud/kubeblocks/internal/cli/printer"
+	"github.com/apecloud/kubeblocks/internal/cli/util/prompt"
 )
 
 var (
@@ -82,7 +88,8 @@ type CreateOptions struct {
 
 	// CueTemplateName cue template file name to render the resource
 	CueTemplateName string
-	Cmd             *cobra.Command
+
+	AutoApprove bool
 
 	// Options a command options object which extends CreateOptions that will be used
 	// to render the cue template
@@ -163,10 +170,9 @@ func (o *CreateOptions) Run() error {
 	}
 
 	if o.EditBeforeCreate {
-		if err = o.editResourceBeforeCreate(resObj); err != nil {
+		if err = o.RunEditOnCreate(resObj); err != nil {
 			return err
 		}
-		return nil
 	}
 
 	dryRunStrategy, err := o.GetDryRunStrategy()
@@ -222,37 +228,110 @@ func (o *CreateOptions) Run() error {
 	return printer.PrintObj(resObj, o.Out)
 }
 
-func (o *CreateOptions) editResourceBeforeCreate(obj *unstructured.Unstructured) error {
+func (o *CreateOptions) RunEditOnCreate(unstructuredObj *unstructured.Unstructured) error {
+	edit := editor.NewDefaultEditor([]string{
+		"KUBE_EDITOR",
+		"EDITOR",
+	})
 
-	editOptions := editor.NewEditOptions(editor.EditBeforeCreateMode, o.IOStreams)
-	editOptions.ValidateOptions = cmdutil.ValidateOptions{
-		ValidationDirective: metav1.FieldValidationStrict,
-	}
-	editOptions.FilenameOptions = res.FilenameOptions{Filenames: []string{"-"}}
+	var (
+		original []byte
+		edited   []byte
+		tmpFile  string
+	)
 
-	if err := o.EditComplete(editOptions, obj); err != nil {
-		return err
-	}
-
-	return editOptions.Run()
-}
-
-func (o *CreateOptions) EditComplete(oe *editor.EditOptions, obj *unstructured.Unstructured) error {
-	contentByte, err := json.Marshal(obj)
+	editPrinter, err := o.ToPrinter(nil, false)
 	if err != nil {
 		return err
 	}
-	if err := oe.Complete(o.Factory, []string{}, o.Cmd); err != nil {
+
+	buf := &bytes.Buffer{}
+	var w io.Writer = buf
+
+	// add header
+	if err := addHeader(w); err != nil {
 		return err
 	}
-	r := o.Factory.NewBuilder().
-		Unstructured().
-		Stream(bytes.NewReader(contentByte), "edit").
-		ContinueOnError().
-		Flatten().
-		Do()
 
-	oe.OriginalResult = r
+	originalObj := unstructuredObj.DeepCopyObject()
+	if err := editPrinter.PrintObj(originalObj, w); err != nil {
+		return err
+	}
+	original = buf.Bytes()
+
+	// launch the editor
+	edited, tmpFile, err = edit.LaunchTempFile(fmt.Sprintf("%s-edit-", filepath.Base(os.Args[0])), ".yaml", buf)
+	if err != nil {
+		return fmt.Errorf("error executing editor: %v", err)
+	}
+
+	// apply validation
+	fieldValidationVerifier := resource.NewQueryParamVerifier(o.Dynamic, o.Factory.OpenAPIGetter(), resource.QueryParamFieldValidation)
+	schema, err := o.Factory.Validator(metav1.FieldValidationStrict, fieldValidationVerifier)
+	if err != nil {
+		return err
+	}
+	err = schema.ValidateBytes(cmdutil.StripComments(edited))
+	if err != nil {
+		return fmt.Errorf("the edited file failed validation: %v", err)
+	}
+
+	// Compare content without comments
+	if bytes.Equal(cmdutil.StripComments(original), cmdutil.StripComments(edited)) {
+		err := os.Remove(tmpFile)
+		if err != nil {
+			return fmt.Errorf("error removing file: %v", err)
+		}
+		_, err = fmt.Fprintln(o.ErrOut, "Edit cancelled, no changes made.")
+		if err != nil {
+			return fmt.Errorf("error writing to stderr: %v", err)
+		}
+		return nil
+	}
+
+	// Returns an error if comments are included.
+	lines, err := hasComment(bytes.NewBuffer(edited))
+	if err != nil {
+		return fmt.Errorf("error checking for comment: %v", err)
+	}
+	if !lines {
+		err := os.Remove(tmpFile)
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprintln(o.ErrOut, "Edit cancelled, saved file was empty.")
+		if err != nil {
+			return err
+		}
+	}
+
+	decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(edited), len(edited))
+	if err := decoder.Decode(unstructuredObj); err != nil {
+		return err
+	}
+
+	if err := editPrinter.PrintObj(unstructuredObj, o.Out); err != nil {
+		return err
+	}
+	return o.confirmToContinue()
+}
+
+func (o *CreateOptions) confirmToContinue() error {
+	if !o.AutoApprove {
+		printer.Warning(o.Out, "Above resource will be created, do you want to continue to create this resource: %s  ?\n  Only 'yes' will be accepted to confirm.\n\n", o.Name)
+		entered, _ := prompt.NewPrompt("Enter a value:", nil, o.In).Run()
+		if entered != "yes" {
+			_, err := fmt.Fprintf(o.Out, "\nCancel resource creation.\n")
+			if err != nil {
+				return err
+			}
+			return cmdutil.ErrExit
+		}
+	}
+	_, err := fmt.Fprintf(o.Out, "Continue to create resource: %s\n", o.Name)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -355,4 +434,26 @@ func convertContentToUnstructured(cueValue cue.Value) (*unstructured.Unstructure
 		return nil, err
 	}
 	return unstructuredObj, nil
+}
+
+func hasComment(r io.Reader) (bool, error) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		if line := strings.TrimSpace(s.Text()); len(line) > 0 && line[0] != '#' {
+			return true, nil
+		}
+	}
+	if err := s.Err(); err != nil && err != io.EOF {
+		return false, err
+	}
+	return false, nil
+}
+
+func addHeader(w io.Writer) error {
+	_, err := fmt.Fprint(w, `# Please edit the object below. Lines beginning with a '#' will be ignored,
+# and an empty file will abort the edit. If an error occurs while saving this file will be
+# reopened with the relevant failures.
+#
+`)
+	return err
 }

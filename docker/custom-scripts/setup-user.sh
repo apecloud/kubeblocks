4f524754f73674e992c53851a34766ff590a79a6
Nash Tsai <nash.tsai@gmail.com>
2023-06-01 11:16:20 +0800

    chore: update license header

--- docker/custom-scripts/setup-user.sh
+++ docker/custom-scripts/setup-user.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #
-# Copyright 2022-2023 ApeCloud Co., Ltd
 # Copyright 2021 The Dapr Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

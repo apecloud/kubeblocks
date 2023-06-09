46f7ec8b5398c0ae020bf56168bde10a838be735
1aal <alal@zju.edu.cn>
2023-06-09 13:54:53 +0800

    fix test

--- internal/cli/cmd/dashboard/grafana_direct_test.go
+++ internal/cli/cmd/dashboard/grafana_direct_test.go
@@ -26,23 +26,23 @@ import (
 
 var _ = Describe("grafana open flag", func() {
 
-	var testCharType string
+	var testClusterType string
 	expectURL := map[string]string{
-		"apecloud-mysql": "http://127.0.0.1:8080/d/apecloud-mysql",
-		"cadvisor":       "http://127.0.0.1:8080/d/cadvisor",
-		"jmx":            "http://127.0.0.1:8080/d/jmx",
-		"kafka":          "http://127.0.0.1:8080/d/kafka",
-		"mongodb":        "http://127.0.0.1:8080/d/mongodb",
-		"node":           "http://127.0.0.1:8080/d/node",
-		"postgresql":     "http://127.0.0.1:8080/d/postgresql",
-		"redis":          "http://127.0.0.1:8080/d/redis",
-		"weaviate":       "http://127.0.0.1:8080/d/weaviate",
+		"mysql":      "http://127.0.0.1:8080/d/mysql",
+		"cadvisor":   "http://127.0.0.1:8080/d/cadvisor",
+		"jmx":        "http://127.0.0.1:8080/d/jmx",
+		"kafka":      "http://127.0.0.1:8080/d/kafka",
+		"mongodb":    "http://127.0.0.1:8080/d/mongodb",
+		"node":       "http://127.0.0.1:8080/d/node",
+		"postgresql": "http://127.0.0.1:8080/d/postgresql",
+		"redis":      "http://127.0.0.1:8080/d/redis",
+		"weaviate":   "http://127.0.0.1:8080/d/weaviate",
 	}
 
 	It("build grafana direct url", func() {
 		testURL := "http://127.0.0.1:8080"
-		testCharType = "invalid"
-		Expect(buildGrafanaDirectURL(&testURL, testCharType)).Should(HaveOccurred())
+		testClusterType = "invalid"
+		Expect(buildGrafanaDirectURL(&testURL, testClusterType)).Should(HaveOccurred())
 		for charType, targetURL := range expectURL {
 			testURL = "http://127.0.0.1:8080"
 			Expect(buildGrafanaDirectURL(&testURL, charType)).Should(Succeed())

bfe6d7def2f78f550a860e17faf514775cd39b52
chantu <lynnleelhl@gmail.com>
2023-06-12 17:13:08 +0800

    test: ut for types (#3725)

--- apis/apps/v1alpha1/cluster_types_test.go
+++ apis/apps/v1alpha1/cluster_types_test.go
@@ -17,10 +17,176 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"fmt"
 	"testing"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
+
+var _ = Describe("", func() {
+
+	It("test GetMinAvailable", func() {
+		prefer := intstr.IntOrString{}
+		clusterCompSpec := &ClusterComponentSpec{}
+		Expect(clusterCompSpec.GetMinAvailable(nil)).Should(BeNil())
+		clusterCompSpec.NoCreatePDB = false
+		clusterCompSpec.Replicas = 1
+		Expect(clusterCompSpec.GetMinAvailable(&prefer).IntVal).Should(BeEquivalentTo(0))
+		clusterCompSpec.Replicas = 2
+		Expect(clusterCompSpec.GetMinAvailable(&prefer).IntVal).Should(BeEquivalentTo(1))
+		clusterCompSpec.Replicas = 3
+		Expect(clusterCompSpec.GetMinAvailable(&prefer)).Should(BeEquivalentTo(&prefer))
+	})
+
+	It("test toVolumeClaimTemplate", func() {
+		r := ClusterComponentVolumeClaimTemplate{}
+		r.Name = "test-name"
+		Expect(r.toVolumeClaimTemplate().ObjectMeta.Name).Should(BeEquivalentTo(r.Name))
+	})
+
+	It("test ToV1PersistentVolumeClaimSpec", func() {
+		r := PersistentVolumeClaimSpec{}
+		pvcSpec := r.ToV1PersistentVolumeClaimSpec()
+		Expect(pvcSpec.AccessModes).Should(BeEquivalentTo(r.AccessModes))
+		Expect(pvcSpec.Resources).Should(BeEquivalentTo(r.Resources))
+		Expect(pvcSpec.StorageClassName).Should(BeEquivalentTo(r.StorageClassName))
+	})
+
+	It("test GetStorageClassName", func() {
+		preferSC := "prefer-sc"
+		r := PersistentVolumeClaimSpec{}
+		r.StorageClassName = nil
+		Expect(r.GetStorageClassName(preferSC)).Should(BeEquivalentTo(&preferSC))
+		scName := "test-sc"
+		r.StorageClassName = &scName
+		Expect(r.GetStorageClassName(preferSC)).Should(BeEquivalentTo(&scName))
+	})
+
+	It("test IsDeleting", func() {
+		r := Cluster{}
+		Expect(r.IsDeleting()).Should(Equal(false))
+	})
+
+	It("test IsUpdating", func() {
+		r := Cluster{}
+		r.Status = ClusterStatus{
+			ObservedGeneration: int64(0),
+		}
+		r.Generation = 1
+		Expect(r.IsUpdating()).Should(Equal(true))
+	})
+
+	It("test IsStatusUpdating", func() {
+		r := Cluster{}
+		r.Status = ClusterStatus{
+			ObservedGeneration: int64(1),
+		}
+		r.Generation = 1
+		Expect(r.IsStatusUpdating()).Should(Equal(true))
+	})
+
+	It("test GetVolumeClaimNames", func() {
+		r := Cluster{}
+		clusterName := "test-cluster"
+		r.Name = clusterName
+		Expect(r.GetVolumeClaimNames("")).Should(BeNil())
+		compName := "test-comp"
+		comp := ClusterComponentSpec{}
+		comp.Name = compName
+		r.Spec.ComponentSpecs = []ClusterComponentSpec{
+			comp,
+		}
+		Expect(r.GetVolumeClaimNames("")).Should(BeNil())
+		comp.VolumeClaimTemplates = []ClusterComponentVolumeClaimTemplate{
+			{
+				Name: compName,
+				Spec: PersistentVolumeClaimSpec{},
+			},
+		}
+		comp.Replicas = 1
+		r.Spec.ComponentSpecs = []ClusterComponentSpec{
+			comp,
+		}
+		Expect(r.GetVolumeClaimNames(compName)).ShouldNot(BeNil())
+		Expect(r.GetVolumeClaimNames(compName)).Should(ContainElement(fmt.Sprintf("%s-%s-%s-%d", compName, r.Name, compName, 0)))
+	})
+
+	It("test GetDefNameMappingComponents", func() {
+		r := ClusterSpec{}
+		key := "comp-def-ref"
+		comp := ClusterComponentSpec{}
+		comp.ComponentDefRef = key
+		r.ComponentSpecs = []ClusterComponentSpec{comp}
+		Expect(r.GetDefNameMappingComponents()[key]).Should(ContainElement(comp))
+	})
+
+	It("test SetComponentStatus", func() {
+		r := ClusterStatus{}
+		status := ClusterComponentStatus{}
+		compName := "test-comp"
+		r.Components = map[string]ClusterComponentStatus{
+			compName: {
+				Phase:                "",
+				Message:              nil,
+				PodsReady:            nil,
+				PodsReadyTime:        nil,
+				ConsensusSetStatus:   nil,
+				ReplicationSetStatus: nil,
+			},
+		}
+		r.SetComponentStatus(compName, status)
+		Expect(r.Components[compName]).Should(Equal(status))
+	})
+
+	It("test checkedInitComponentsMap", func() {
+		r := ClusterStatus{}
+		r.Components = nil
+		r.checkedInitComponentsMap()
+		Expect(r.Components).ShouldNot(BeNil())
+	})
+
+	It("test ToVolumeClaimTemplates", func() {
+		r := ClusterComponentSpec{}
+		r.VolumeClaimTemplates = []ClusterComponentVolumeClaimTemplate{{
+			Name: "",
+			Spec: PersistentVolumeClaimSpec{},
+		}}
+		Expect(r.ToVolumeClaimTemplates()).Should(HaveLen(1))
+	})
+
+	It("test GetClusterUpRunningPhases", func() {
+		Expect(GetClusterUpRunningPhases()).Should(ContainElements([]ClusterPhase{
+			RunningClusterPhase,
+			AbnormalClusterPhase,
+			FailedClusterPhase,
+		}))
+	})
+
+	It("GetComponentTerminalPhases", func() {
+		Expect(GetComponentTerminalPhases()).Should(ContainElements([]ClusterComponentPhase{
+			RunningClusterCompPhase,
+			StoppedClusterCompPhase,
+			FailedClusterCompPhase,
+			AbnormalClusterCompPhase,
+		}))
+	})
+
+	It("GetComponentUpRunningPhase", func() {
+		Expect(GetComponentUpRunningPhase()).Should(ContainElements([]ClusterComponentPhase{
+			RunningClusterCompPhase,
+			AbnormalClusterCompPhase,
+			FailedClusterCompPhase,
+		}))
+	})
+
+	It("ComponentPodsAreReady", func() {
+		ready := true
+		Expect(ComponentPodsAreReady(&ready)).Should(BeTrue())
+	})
+})
 
 func TestValidateEnabledLogs(t *testing.T) {
 	cluster := &Cluster{}

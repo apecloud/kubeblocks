d02d88bf8b1b49ffa2fb3e38caf6cbd0be21f5ce
chantu <lynnleelhl@gmail.com>
2023-06-12 15:24:27 +0800

    ut for types

--- apis/apps/v1alpha1/clusterdefinition_types_test.go
+++ apis/apps/v1alpha1/clusterdefinition_types_test.go
@@ -19,6 +19,10 @@ package v1alpha1
 import (
 	"testing"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
@@ -79,3 +83,134 @@ func TestGetComponentDefByName(t *testing.T) {
 		t.Error("function GetComponentDefByName should return nil")
 	}
 }
+
+var _ = Describe("", func() {
+
+	It("test GetTerminalPhases", func() {
+		r := ClusterDefinitionStatus{}
+		Expect(r.GetTerminalPhases()).Should(ContainElement(AvailablePhase))
+	})
+
+	It("test GetStatefulSetWorkload", func() {
+		r := &ClusterComponentDefinition{}
+		r.WorkloadType = Stateless
+		Expect(r.GetStatefulSetWorkload()).Should(BeNil())
+		r.WorkloadType = Stateful
+		Expect(r.GetStatefulSetWorkload()).Should(BeEquivalentTo(r.StatefulSpec))
+		r.WorkloadType = Consensus
+		Expect(r.GetStatefulSetWorkload()).Should(BeEquivalentTo(r.ConsensusSpec))
+		r.WorkloadType = Replication
+		Expect(r.GetStatefulSetWorkload()).Should(BeEquivalentTo(r.ReplicationSpec))
+	})
+
+	It("test GetMinAvailable", func() {
+		r := &ClusterComponentDefinition{}
+		r.WorkloadType = Consensus
+		Expect(r.GetMinAvailable().String()).Should(Equal("51%"))
+		r.WorkloadType = Stateful
+		Expect(r.GetMinAvailable().IntVal).Should(BeEquivalentTo(1))
+	})
+
+	It("test GetMaxUnavailable", func() {
+		r := &ClusterComponentDefinition{}
+		r.WorkloadType = Stateless
+		Expect(r.GetMaxUnavailable()).Should(BeNil())
+		maxUnavailable := intstr.IntOrString{StrVal: "49%"}
+		r.StatelessSpec = &StatelessSetSpec{
+			UpdateStrategy: appsv1.DeploymentStrategy{
+				RollingUpdate: &appsv1.RollingUpdateDeployment{
+					MaxUnavailable: &maxUnavailable,
+				},
+			},
+		}
+		Expect(r.GetMaxUnavailable()).Should(BeEquivalentTo(&maxUnavailable))
+		r.WorkloadType = Stateful
+		r.StatefulSpec = &StatefulSetSpec{
+			UpdateStrategy: BestEffortParallelStrategy,
+		}
+		Expect(r.GetMaxUnavailable().String()).Should(BeEquivalentTo("49%"))
+	})
+
+	It("test GetCommonStatefulSpec", func() {
+		r := &ClusterComponentDefinition{}
+		r.WorkloadType = Stateful
+		r.StatefulSpec = &StatefulSetSpec{}
+		spec, err := r.GetCommonStatefulSpec()
+		Expect(err).Should(BeNil())
+		Expect(spec).Should(BeEquivalentTo(r.StatefulSpec))
+		r.WorkloadType = Consensus
+		r.ConsensusSpec = &ConsensusSetSpec{
+			StatefulSetSpec: StatefulSetSpec{},
+		}
+		spec, err = r.GetCommonStatefulSpec()
+		Expect(err).Should(BeNil())
+		Expect(spec).Should(BeEquivalentTo(&r.ConsensusSpec.StatefulSetSpec))
+		r.WorkloadType = Replication
+		r.ReplicationSpec = &ReplicationSetSpec{
+			StatefulSetSpec: StatefulSetSpec{},
+		}
+		spec, err = r.GetCommonStatefulSpec()
+		Expect(err).Should(BeNil())
+		Expect(spec).Should(BeEquivalentTo(&r.ReplicationSpec.StatefulSetSpec))
+	})
+
+	It("test ToSVCSpec", func() {
+		r := ServiceSpec{
+			Ports: []ServicePort{
+				{
+					Name: "test-name",
+				},
+			},
+		}
+		Expect(r.ToSVCSpec().Ports[0].Name).Should(Equal(r.Ports[0].Name))
+	})
+
+	It("test GetUpdateStrategy", func() {
+		r := &StatefulSetSpec{}
+		r.UpdateStrategy = BestEffortParallelStrategy
+		Expect(r.GetUpdateStrategy()).Should(Equal(r.UpdateStrategy))
+	})
+
+	It("test finalStsUpdateStrategy", func() {
+		r := &StatefulSetSpec{}
+		r.UpdateStrategy = ParallelStrategy
+		policyType, strategy := r.finalStsUpdateStrategy()
+		Expect(policyType).Should(BeEquivalentTo(appsv1.ParallelPodManagement))
+		Expect(strategy.Type).Should(BeEquivalentTo(appsv1.RollingUpdateStatefulSetStrategyType))
+		r.UpdateStrategy = SerialStrategy
+		policyType, strategy = r.finalStsUpdateStrategy()
+		Expect(policyType).Should(BeEquivalentTo(appsv1.OrderedReadyPodManagement))
+		Expect(strategy.Type).Should(BeEquivalentTo(appsv1.RollingUpdateStatefulSetStrategyType))
+		Expect(strategy.RollingUpdate.MaxUnavailable.IntValue()).Should(BeEquivalentTo(1))
+	})
+
+	It("test consensus GetUpdateStrategy", func() {
+		r := &ConsensusSetSpec{}
+		r.UpdateStrategy = BestEffortParallelStrategy
+		Expect(r.GetUpdateStrategy()).Should(Equal(r.UpdateStrategy))
+	})
+
+	It("test consensus FinalStsUpdateStrategy", func() {
+		r := ConsensusSetSpec{}
+		policyType, strategy := r.FinalStsUpdateStrategy()
+		Expect(policyType).Should(BeEquivalentTo(appsv1.ParallelPodManagement))
+		Expect(strategy.Type).Should(BeEquivalentTo(appsv1.OnDeleteStatefulSetStrategyType))
+	})
+
+	It("test NewConsensusSetSpec", func() {
+		Expect(NewConsensusSetSpec()).ShouldNot(BeNil())
+	})
+
+	It("test replication GetUpdateStrategy", func() {
+		r := &ReplicationSetSpec{}
+		r.UpdateStrategy = BestEffortParallelStrategy
+		Expect(r.GetUpdateStrategy()).Should(Equal(r.UpdateStrategy))
+	})
+
+	It("test replication FinalStsUpdateStrategy", func() {
+		r := ReplicationSetSpec{}
+		policyType, strategy := r.FinalStsUpdateStrategy()
+		Expect(policyType).Should(BeEquivalentTo(appsv1.ParallelPodManagement))
+		Expect(strategy.Type).Should(BeEquivalentTo(appsv1.OnDeleteStatefulSetStrategyType))
+	})
+})

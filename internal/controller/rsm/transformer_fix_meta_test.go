c21267a7b148a0577cd3dad01e6767a0f7617c01
free6om <free6om.c@gmail.com>
2023-06-21 11:09:39 +0800

    fix meta ut done

---
+++ internal/controller/rsm/transformer_fix_meta_test.go
@@ -0,0 +1,91 @@
+/*
+Copyright (C) 2022-2023 ApeCloud Co., Ltd
+
+This file is part of KubeBlocks project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package rsm
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/utils/strings/slices"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	workloads "github.com/apecloud/kubeblocks/apis/workloads/v1alpha1"
+	"github.com/apecloud/kubeblocks/internal/controller/builder"
+	"github.com/apecloud/kubeblocks/internal/controller/graph"
+	"github.com/apecloud/kubeblocks/internal/controller/model"
+)
+
+var _ = Describe("fix meta transformer test.", func() {
+	const (
+		namespace = "foo"
+		name      = "bar"
+	)
+
+	var (
+		rsm         *workloads.ReplicatedStateMachine
+		transCtx    *rsmTransformContext
+		dag         *graph.DAG
+		transformer FixMetaTransformer
+	)
+
+	BeforeEach(func() {
+		rsm = builder.NewReplicatedStateMachineBuilder(namespace, name).
+			SetUID("foo-bar-uid").
+			SetReplicas(3).
+			GetObject()
+
+		ctx := context.Background()
+		logger := logf.FromContext(ctx).WithValues("rsm-test", namespace)
+		transCtx = &rsmTransformContext{
+			Context:       ctx,
+			Client:        k8sMock,
+			EventRecorder: nil,
+			Logger:        logger,
+			rsmOrig:       rsm.DeepCopy(),
+			rsm:           rsm,
+		}
+
+		dag = graph.NewDAG()
+		model.PrepareStatus(dag, transCtx.rsmOrig, transCtx.rsm)
+		transformer = FixMetaTransformer{}
+	})
+
+	Context("rsm deletion", func() {
+		It("should work well", func() {
+			Expect(transformer.Transform(transCtx, dag)).Should(Equal(graph.ErrPrematureStop))
+			dagExpected := graph.NewDAG()
+			root := &model.ObjectVertex{
+				Obj: transCtx.rsm,
+				OriObj: transCtx.rsmOrig,
+				Action: model.ActionPtr(model.UPDATE),
+			}
+			dagExpected.AddVertex(root)
+			Expect(dag.Equals(dagExpected, less)).Should(BeTrue())
+			root, err := model.FindRootVertex(dag)
+			Expect(err).Should(BeNil())
+			rsmNew, ok := root.Obj.(*workloads.ReplicatedStateMachine)
+			Expect(ok).Should(BeTrue())
+			Expect(rsmNew.Finalizers).ShouldNot(BeNil())
+			Expect(slices.Contains(rsmNew.Finalizers, rsmFinalizerName)).Should(BeTrue())
+		})
+	})
+})

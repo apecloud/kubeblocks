2a0d192c3843e91980a88c82a551b25a7b4769e0
sophon <sophon@apecloud.com>
2023-06-25 10:50:18 +0800

    chore: refine the code for cluster Validate

--- internal/cli/cmd/infrastructure/cluster.go
+++ internal/cli/cmd/infrastructure/cluster.go
@@ -27,9 +27,6 @@ import (
 	"strings"
 
 	"github.com/StudioSol/set"
-	"github.com/apecloud/kubeblocks/internal/cli/cmd/infrastructure/builder"
-	"github.com/apecloud/kubeblocks/internal/cli/printer"
-	"github.com/apecloud/kubeblocks/internal/cli/util/prompt"
 	kubekeyapiv1alpha2 "github.com/kubesphere/kubekey/v3/cmd/kk/apis/kubekey/v1alpha2"
 	"github.com/kubesphere/kubekey/v3/cmd/kk/pkg/common"
 	"github.com/spf13/cobra"
@@ -37,8 +34,12 @@ import (
 	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 
+	"github.com/apecloud/kubeblocks/internal/cli/cmd/infrastructure/builder"
 	"github.com/apecloud/kubeblocks/internal/cli/cmd/infrastructure/types"
+	"github.com/apecloud/kubeblocks/internal/cli/printer"
+	"github.com/apecloud/kubeblocks/internal/cli/util/prompt"
 	cfgcore "github.com/apecloud/kubeblocks/internal/configuration"
+	cfgutil "github.com/apecloud/kubeblocks/internal/configuration/util"
 )
 
 type clusterOptions struct {
@@ -75,7 +76,7 @@ func (o *clusterOptions) Complete() error {
 	}
 
 	if o.clusterConfig != "" {
-		return o.validateClusterConfig(o.clusterConfig)
+		return o.fillClusterConfig(o.clusterConfig)
 	}
 
 	if o.User.Name == "" {
@@ -86,17 +87,12 @@ func (o *clusterOptions) Complete() error {
 		o.User.Name = currentUser.Username
 		fmt.Printf("The user is not set, use current user %s\n", o.User.Name)
 	}
-	if o.User.PrivateKey == "" {
+	if o.User.Password == "" && o.User.PrivateKey == "" && o.User.PrivateKeyPath == "" {
 		home, err := os.UserHomeDir()
 		if err != nil {
 			return err
 		}
-		if o.User.PrivateKeyPath == "" && o.User.Password == "" {
-			o.User.PrivateKeyPath = filepath.Join(home, ".ssh", "id_rsa")
-		}
-		if strings.HasPrefix(o.User.PrivateKeyPath, "~/") {
-			o.User.PrivateKeyPath = filepath.Join(home, o.User.PrivateKeyPath[2:])
-		}
+		o.User.PrivateKeyPath = filepath.Join(home, ".ssh", "id_rsa")
 	}
 	if len(o.nodes) == 0 {
 		return cfgcore.MakeError("The list of machines where kubernetes is installed must be specified.")
@@ -121,69 +117,48 @@ func (o *clusterOptions) Complete() error {
 }
 
 func (o *clusterOptions) Validate() error {
-	checkFn := func(n string) bool {
-		for _, node := range o.Nodes {
-			if node.Name == n {
-				return true
-			}
-		}
-		return false
-	}
-	validateNodes := func(nodes []string) error {
-		sets := set.NewLinkedHashSetString()
-		for _, node := range nodes {
-			if !checkFn(node) {
-				return cfgcore.MakeError("node %s is not exist!", node)
-			}
-			if sets.InArray(node) {
-				return cfgcore.MakeError("node %s is repeat!", node)
-			}
-			sets.Add(node)
-		}
-		return nil
-	}
 	if o.User.Name == "" {
 		return cfgcore.MakeError("user name is empty")
 	}
-	if o.User.PrivateKey == "" && o.User.PrivateKeyPath != "" {
-		if _, err := os.Stat(o.User.PrivateKeyPath); err != nil {
-			return err
-		}
-		b, err := os.ReadFile(o.User.PrivateKeyPath)
-		if err != nil {
-			return err
-		}
-		o.User.PrivateKey = string(b)
+	if err := validateUser(o); err != nil {
+		return err
 	}
-	if o.RoleGroup.IsValidate() {
+	if !o.RoleGroup.IsValidate() {
 		return cfgcore.MakeError("etcd, master or worker is empty")
 	}
-	if err := validateNodes(o.RoleGroup.ETCD); err != nil {
+	if err := o.checkReplicaNode(o.RoleGroup.ETCD); err != nil {
 		return err
 	}
-	if err := validateNodes(o.RoleGroup.Master); err != nil {
+	if err := o.checkReplicaNode(o.RoleGroup.Master); err != nil {
 		return err
 	}
-	if err := validateNodes(o.RoleGroup.Worker); err != nil {
+	if err := o.checkReplicaNode(o.RoleGroup.Worker); err != nil {
 		return err
 	}
 	return nil
 }
 
-func syncClusterNodeRole(cluster *kubekeyapiv1alpha2.ClusterSpec, runtime *common.KubeRuntime) {
-	hostSet := set.NewLinkedHashSetString()
-	for _, role := range cluster.GroupHosts() {
-		for _, host := range role {
-			if host.IsRole(common.Master) || host.IsRole(common.Worker) {
-				host.SetRole(common.K8s)
-			}
-			if !hostSet.InArray(host.GetName()) {
-				hostSet.Add(host.GetName())
-				runtime.BaseRuntime.AppendHost(host)
-				runtime.BaseRuntime.AppendRoleMap(host)
-			}
+func (o *clusterOptions) checkReplicaNode(nodes []string) error {
+	sets := cfgutil.NewSet()
+	for _, node := range nodes {
+		if !o.hasNode(node) {
+			return cfgcore.MakeError("node %s is not exist!", node)
+		}
+		if sets.InArray(node) {
+			return cfgcore.MakeError("node %s is repeat!", node)
+		}
+		sets.Add(node)
+	}
+	return nil
+}
+
+func (o *clusterOptions) hasNode(n string) bool {
+	for _, node := range o.Nodes {
+		if node.Name == n {
+			return true
 		}
 	}
+	return false
 }
 
 func (o *clusterOptions) confirm(promptStr string) (bool, error) {
@@ -205,7 +180,7 @@ func (o *clusterOptions) confirm(promptStr string) (bool, error) {
 	return strings.ToLower(input) == yesStr, nil
 }
 
-func (o *clusterOptions) validateClusterConfig(configFile string) error {
+func (o *clusterOptions) fillClusterConfig(configFile string) error {
 	_, err := os.Stat(configFile)
 	if err != nil {
 		return err
@@ -221,4 +196,52 @@ func (o *clusterOptions) validateClusterConfig(configFile string) error {
 	}
 	o.Cluster = *c
 	return nil
+}
+
+func checkAndUpdateHomeDir(user *types.ClusterUser) error {
+	if !strings.HasPrefix(user.PrivateKeyPath, "~/") {
+		return nil
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	user.PrivateKeyPath = filepath.Join(home, user.PrivateKeyPath[2:])
+	return nil
+}
+
+func validateUser(o *clusterOptions) error {
+	if o.User.Password != "" || o.User.PrivateKey != "" {
+		return nil
+	}
+	if o.User.PrivateKey == "" && o.User.PrivateKeyPath != "" {
+		if err := checkAndUpdateHomeDir(&o.User); err != nil {
+			return err
+		}
+		if _, err := os.Stat(o.User.PrivateKeyPath); err != nil {
+			return err
+		}
+		b, err := os.ReadFile(o.User.PrivateKeyPath)
+		if err != nil {
+			return err
+		}
+		o.User.PrivateKey = string(b)
+	}
+	return nil
+}
+
+func syncClusterNodeRole(cluster *kubekeyapiv1alpha2.ClusterSpec, runtime *common.KubeRuntime) {
+	hostSet := set.NewLinkedHashSetString()
+	for _, role := range cluster.GroupHosts() {
+		for _, host := range role {
+			if host.IsRole(common.Master) || host.IsRole(common.Worker) {
+				host.SetRole(common.K8s)
+			}
+			if !hostSet.InArray(host.GetName()) {
+				hostSet.Add(host.GetName())
+				runtime.BaseRuntime.AppendHost(host)
+				runtime.BaseRuntime.AppendRoleMap(host)
+			}
+		}
+	}
 }

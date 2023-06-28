123013fd614baeac507e396b85c77d7642fd8c93
shaojiang <shaojiang.deng@apecloud.com>
2023-06-28 16:25:52 +0800

    improve other test cases for builder.

--- internal/controller/builder/builder_test.go
+++ internal/controller/builder/builder_test.go
@@ -26,6 +26,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/viper"
 
 	"github.com/leaanthony/debme"
 	appsv1 "k8s.io/api/apps/v1"
@@ -570,6 +571,34 @@ var _ = Describe("builder", func() {
 			Expect(obj).ShouldNot(BeNil())
 			Expect(obj.Name).Should(Equal(className))
 			Expect(obj.Driver).Should(Equal(driverName))
+		})
+
+		It("builds headless svc correctly", func() {
+			_, cluster, synthesizedComponent := newClusterObjs(nil)
+			expectSvcName := fmt.Sprintf("%s-%s-headless", cluster.Name, synthesizedComponent.Name)
+			obj, err := BuildHeadlessSvc(cluster, synthesizedComponent)
+			Expect(err).Should(BeNil())
+			Expect(obj).ShouldNot(BeNil())
+			Expect(obj.Name).Should(Equal(expectSvcName))
+		})
+
+		It("builds cfg manager tools  correctly", func() {
+			_, cluster, synthesizedComponent := newClusterObjs(nil)
+			cfgManagerParams := &cfgcm.CfgManagerBuildParams{
+				ManagerName:               constant.ConfigSidecarName,
+				SecreteName:               component.GenerateConnCredential(cluster.Name),
+				EnvConfigName:             component.GenerateComponentEnvName(cluster.Name, synthesizedComponent.Name),
+				Image:                     viper.GetString(constant.KBToolsImage),
+				Cluster:                   cluster,
+				ConfigLazyRenderedVolumes: make(map[string]corev1.VolumeMount),
+			}
+			toolContainers := []appsv1alpha1.ToolConfig{
+				{Name: "test-tool", Image: "test-image", Command: []string{"sh"}},
+			}
+
+			obj, err := BuildCfgManagerToolsContainer(cfgManagerParams, synthesizedComponent, toolContainers)
+			Expect(err).Should(BeNil())
+			Expect(obj).ShouldNot(BeEmpty())
 		})
 	})
 

058138612f53f67128dd2ec41ee6b70102cf44aa
fengluodb <woaibiancheng31@gmail.com>
2023-07-07 14:55:04 +0800

    feat: bench adapt kubebench operator

--- internal/cli/cmd/bench/bench.go
+++ internal/cli/cmd/bench/bench.go
@@ -21,10 +21,19 @@ package bench
 
 import (
 	"fmt"
+	"sort"
 
+	"github.com/docker/cli/cli"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/resource"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+
+	"github.com/apecloud/kubeblocks/internal/cli/list"
+	"github.com/apecloud/kubeblocks/internal/cli/printer"
+	"github.com/apecloud/kubeblocks/internal/cli/types"
 )
 
 const (
@@ -90,7 +99,101 @@ func NewBenchCmd(f cmdutil.Factory, streams genericclioptions.IOStreams) *cobra.
 	cmd.AddCommand(
 		NewSysBenchCmd(f, streams),
 		NewPgBenchCmd(f, streams),
+		newListCmd(f, streams),
 	)
 
 	return cmd
+}
+
+type benchListOptions struct {
+	Factory       cmdutil.Factory
+	LabelSelector string
+	Format        string
+
+	genericclioptions.IOStreams
+}
+
+func newListCmd(f cmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	o := &benchListOptions{
+		Factory:   f,
+		IOStreams: streams,
+	}
+	cmd := &cobra.Command{
+		Use:     "list",
+		Short:   "List all benchmarks.",
+		Aliases: []string{"ls"},
+		Args:    cli.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			cmdutil.CheckErr(benchListRun(o))
+		},
+	}
+
+	cmd.Flags().StringVarP(&o.LabelSelector, "selector", "l", o.LabelSelector, "Selector (label query) to filter on, supports '=', '==', and '!='.(e.g. -l key1=value1,key2=value2). Matching objects must satisfy all of the specified label constraints.")
+	return cmd
+}
+
+func benchListRun(o *benchListOptions) error {
+	benchGVRList := []schema.GroupVersionResource{
+		types.PgBenchGVR(),
+		types.SysbenchGVR(),
+	}
+
+	var infos []*resource.Info
+	for _, gvr := range benchGVRList {
+		bench := list.NewListOptions(o.Factory, o.IOStreams, gvr)
+
+		bench.Print = false
+		bench.LabelSelector = o.LabelSelector
+		result, err := bench.Run()
+		if err != nil {
+			return err
+		}
+
+		benchInfos, err := result.Infos()
+		if err != nil {
+			return err
+		}
+		infos = append(infos, benchInfos...)
+	}
+
+	if len(infos) == 0 {
+		fmt.Fprintf(o.Out, "No benchmarks found.\n")
+		return nil
+	}
+
+	printRows := func(tbl *printer.TablePrinter) error {
+		// sort bench with kind, then .status.phase, finally .metadata.name
+		sort.SliceStable(infos, func(i, j int) bool {
+			iKind := infos[i].Object.(*unstructured.Unstructured).GetKind()
+			jKind := infos[j].Object.(*unstructured.Unstructured).GetKind()
+			iPhase := infos[i].Object.(*unstructured.Unstructured).Object["status"].(map[string]interface{})["phase"]
+			jPhase := infos[j].Object.(*unstructured.Unstructured).Object["status"].(map[string]interface{})["phase"]
+			iName := infos[i].Object.(*unstructured.Unstructured).GetName()
+			jName := infos[j].Object.(*unstructured.Unstructured).GetName()
+
+			if iKind != jKind {
+				return iKind < jKind
+			}
+			if iPhase != jPhase {
+				return iPhase.(string) < jPhase.(string)
+			}
+			return iName < jName
+		})
+
+		for _, info := range infos {
+			obj := info.Object.(*unstructured.Unstructured)
+			tbl.AddRow(
+				obj.GetName(),
+				obj.GetKind(),
+				obj.Object["status"].(map[string]interface{})["phase"],
+				obj.Object["status"].(map[string]interface{})["completions"],
+			)
+		}
+		return nil
+	}
+
+	if err := printer.PrintTable(o.Out, nil, printRows, "NAME", "KIND", "STATUS", "COMPLETIONS"); err != nil {
+		return err
+	}
+	return nil
 }

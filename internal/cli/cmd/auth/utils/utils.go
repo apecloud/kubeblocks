0e055798c0470cc065843986b378c189648b7e90
huyongqii <yongqi_hu@163.com>
2023-07-17 12:02:14 +0800

    remove openBroeser func

--- internal/cli/cmd/auth/utils/utils.go
+++ internal/cli/cmd/auth/utils/utils.go
@@ -24,7 +24,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/mattn/go-isatty"
@@ -32,45 +31,6 @@ import (
 
 func IsTTY() bool {
 	return isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())
-}
-
-// OpenBrowser opens a web browser at the specified url.
-func OpenBrowser(goos, url string) *exec.Cmd {
-	if !IsTTY() {
-		panic("OpenBrowser called without a TTY")
-	}
-
-	exe := "open"
-	var args []string
-	switch goos {
-	case "darwin":
-		args = append(args, url)
-	case "windows":
-		exe, _ = exec.LookPath("cmd")
-		r := strings.NewReplacer("&", "^&")
-		args = append(args, "/c", "start", r.Replace(url))
-	default:
-		exe = linuxExe()
-		args = append(args, url)
-	}
-
-	cmd := exec.Command(exe, args...)
-	cmd.Stderr = os.Stderr
-	return cmd
-}
-
-func linuxExe() string {
-	exe := "xdg-open"
-
-	_, err := exec.LookPath(exe)
-	if err != nil {
-		_, err := exec.LookPath("wslview")
-		if err == nil {
-			exe = "wslview"
-		}
-	}
-
-	return exe
 }
 
 func NewRequest(ctx context.Context, url string, payload url.Values) (*http.Request, error) {

ed70f962ef32584d40f98935967c723dcc755bc2
dereckchen <2564938992@qq.com>
2023-07-17 16:04:04 +0800

    fix: rewrite main

--- cmd/probe/main.go
+++ cmd/probe/main.go
@@ -20,20 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 
-	"github.com/apecloud/kubeblocks/cmd/probe/internal/binding"
+	"github.com/apecloud/kubeblocks/cmd/probe/internal/middleware/http/probe"
 
-	"github.com/go-logr/logr"
-
-	"github.com/apecloud/kubeblocks/cmd/probe/internal/binding/mysql"
+	"github.com/apecloud/kubeblocks/cmd/probe/internal/component"
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -41,22 +37,23 @@ import (
 	"go.uber.org/automaxprocs/maxprocs"
 )
 
-var mysqlOps *mysql.MysqlOperations
+var port int
+var configDir string
+
+const (
+	DefaultPort       = 7373
+	DefaultConfigPath = "config-path"
+)
 
 func init() {
 	viper.AutomaticEnv()
-	mysqlOps = mysql.NewMysql(logr.Logger{})
-	err := mysqlOps.Init()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	flag.IntVar(&port, "port", DefaultPort, "probe default port")
+	flag.StringVar(&configDir, "config-path", DefaultConfigPath, "probe default config directory for builtin type")
 }
 
 func main() {
 	// set GOMAXPROCS
 	_, _ = maxprocs.Set()
-	mainLogger := logr.Logger{}
 
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
@@ -64,43 +61,18 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("fatal error viper bindPFlags: %v", err))
 	}
-	viper.SetConfigFile(viper.GetString("config")) // path to look for the config file in
-	err = viper.ReadInConfig()                     // Find and read the config file
-	if err != nil {                                // Handle errors reading the config file
+
+	err = component.GetAllComponent(configDir) // find all builtin config file and read
+	if err != nil {                            // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %v", err))
 	}
 
+	err = probe.RegisterBuiltin() // register all builtin component
 	if err != nil {
-		mainLogger.Error(err, "fatal error from runtime")
-		os.Exit(1)
+		panic(fmt.Errorf("fatal error register builtin: %v", err))
 	}
 
-	go func() {
-		http.HandleFunc("/probe", func(writer http.ResponseWriter, req *http.Request) {
-			// Readiness 是 Get请求, CmdChannel 是 Post请求
-			probeReq := &binding.ProbeRequest{Metadata: map[string]string{}}
-			if req.Method == "GET" {
-				probeReq.Operation = "getRole"
-			} else {
-				// 获取operation
-				err := json.NewDecoder(req.Body).Decode(probeReq)
-				if err != nil {
-					mysqlOps.Logger.Error(err, "json Decode failed")
-				}
-			}
-			dispatch, err := mysqlOps.Dispatch(req.Context(), probeReq)
-			if err != nil {
-				mysqlOps.Logger.Error(err, "dispatch failed")
-			}
-			if codeStr, ok := dispatch.Metadata[binding.StatusCode]; ok && (codeStr == binding.OperationNotFoundHTTPCode || codeStr == binding.OperationFailedHTTPCode) {
-				code, _ := strconv.Atoi(codeStr)
-				writer.WriteHeader(code)
-			}
-			// todo 修改状态码
-			writer.Write(dispatch.Data)
-		})
-		http.ListenAndServe("localhost:7979", nil)
-	}()
+	http.HandleFunc("/", probe.SetMiddleware(probe.GetRouter()))
 
 	// ha dependent on dbmanager which is initialized by rt.Run
 	// ha := highavailability.NewHa(logHa)

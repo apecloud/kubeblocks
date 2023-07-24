3afe5921327f8bfb7e3cbf568a136409bbb4da91
Guo Ziang <ziang.gza@apecloud.com>
2023-07-24 17:02:50 +0800

    chore: update api doc

--- apis/apps/v1alpha1/clusterdefinition_types.go
+++ apis/apps/v1alpha1/clusterdefinition_types.go
@@ -75,7 +75,8 @@ type ClusterService struct {
 	ServiceSpec `json:",inline"`
 
 	// frontendComponents defines the frontend components that can provide service for cluster, ordered by priority.
-	// This field is required if the ClusterDefinition have multiple ClusterComponentDefinition.
+	// If this field is not specified, the first component that provides the `ClusterDefinition.spec.componentDefs[].service`
+	// attribute will be used as the frontend component.
 	// +optional
 	FrontendComponents []string `json:"frontendComponents,omitempty"`
 }

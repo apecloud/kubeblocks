3a254b2ef248acdea0cc89ae985d51640cc9dbd3
1aal <alal@zju.edu.cn>
2023-07-25 15:12:59 +0800

    support more infos about the config

--- internal/cli/cmd/cluster/create.go
+++ internal/cli/cmd/cluster/create.go
@@ -1306,7 +1306,8 @@ func setKeys() []string {
 	}
 }
 
-// validateDefaultSCInConfig will verify if the ConfigMap of Kubeblocks is configured with the DEFAULT_STORAGE_CLASS
+// validateDefaultSCInConfig will verify if the ConfigMap of Kubeblocks is configured with the DEFAULT_STORAGE_CLASS.
+// When we install Kubeblocks, certain configurations will be rendered in a ConfigMap named kubeblocks-manager-config. You can find the details in deploy/helm/template/configmap.yaml.
 func validateDefaultSCInConfig(dynamic dynamic.Interface) (bool, error) {
 	// todo:  types.KubeBlocksManagerConfigMapName almost is hard code, add a unique label for kubeblocks-manager-config
 	namespace, err := util.GetKubeBlocksNamespaceByDynamic(dynamic)

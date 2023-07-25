78b7f13d5f1b056845692ed3e5a1a7f8f848cd3b
dereckchen <2564938992@qq.com>
2023-07-26 00:33:36 +0800

    chore: merfe utils.go into util.go

--- cmd/probe/internal/component/util.go
+++ cmd/probe/internal/component/util.go
@@ -1,7 +1,28 @@
+/*
+Copyright (C) 2022-2023 ApeCloud Co., Ltd
+
+This file is part of KubeBlocks project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package component
 
 import (
 	"os"
+
+	"github.com/spf13/viper"
 )
 
 func MaxInt64(x, y int64) int64 {
@@ -20,4 +41,60 @@ func GetSQLChannelProc() (*os.Process, error) {
 	}
 
 	return sqlChannelProc, nil
+}
+
+type Properties map[string]string
+
+type Component struct {
+	Name string
+	Spec ComponentSpec
+}
+
+type ComponentSpec struct {
+	Version  string
+	Metadata []kv
+}
+
+type kv struct {
+	Name  string
+	Value string
+}
+
+var Name2Property = map[string]Properties{}
+
+func readConfig(filename string) (string, Properties, error) {
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(filename)
+	if err := viper.ReadInConfig(); err != nil {
+		return "", nil, err
+	}
+	component := &Component{}
+	if err := viper.Unmarshal(component); err != nil {
+		return "", nil, err
+	}
+	properties := make(Properties)
+	properties["version"] = component.Spec.Version
+	for _, pair := range component.Spec.Metadata {
+		properties[pair.Name] = pair.Value
+	}
+	return component.Name, properties, nil
+}
+
+func GetAllComponent(dir string) error {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		name, properties, err := readConfig(dir + "/" + file.Name())
+		if err != nil {
+			return err
+		}
+		Name2Property[name] = properties
+	}
+	return nil
+}
+
+func GetProperties(name string) Properties {
+	return Name2Property[name]
 }

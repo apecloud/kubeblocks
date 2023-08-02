bf54684c6dcc522b3d070bf852dce33e9fb3a382
a le <101848970+1aal@users.noreply.github.com>
2023-08-02 14:56:07 +0800

    feat: kbcli `compare` command supports more clear KB upgrade version information. (#4539)
    Co-authored-by: 1aal <1aal@users.noreply.github.com>

--- internal/cli/util/helm/diff_test.go
+++ internal/cli/util/helm/diff_test.go
@@ -22,6 +22,7 @@ package helm
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"golang.org/x/exp/maps"
 
 	"go.uber.org/zap/buffer"
 	helm "helm.sh/helm/v3/pkg/release"
@@ -65,15 +66,41 @@ const (
    change: 1
 
 `
+
+	crdContent = `apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: componentclassdefinitions.apps.kubeblocks.io
+spec:
+  group: apps.kubeblocks.io
+  names:
+    categories:
+    - kubeblocks
+    kind: ComponentClassDefinition
+  scope: Cluster
+  versions:
+  - name: v1alpha1
+    schema:
+      openAPIV3Schema:
+        description: ComponentClassDefinition is the Schema for the componentclassdefinitions
+          API
+        properties:
+          apiVersion:
+            type: string
+    served: true
+    storage: true
+    subresources:
+      status: {}`
 )
 
 var _ = Describe("helm diff", func() {
-	var obj map[interface{}]interface{}
+	var obj map[any]any
+	var crdObj map[any]any
 	var content string
 	var release *helm.Release
 	var out buffer.Buffer
 
-	buildRelease := func(obj map[interface{}]interface{}) *helm.Release {
+	buildRelease := func(obj map[any]any) *helm.Release {
 		var res helm.Release
 		marshal, _ := yaml.Marshal(obj)
 		manifest := `---
@@ -83,26 +110,26 @@ var _ = Describe("helm diff", func() {
 		return &res
 	}
 
-	exceptToMap := func(obj map[interface{}]interface{}, key string) map[interface{}]interface{} {
+	exceptToMap := func(obj map[any]any, key string) map[any]any {
 		Expect(obj).ShouldNot(BeNil())
-		m, ok := obj[key].(map[interface{}]interface{})
+		m, ok := obj[key].(map[any]any)
 		Expect(ok).Should(BeTrue())
 		return m
 	}
-	exceptToSlice := func(obj map[interface{}]interface{}, key string) []interface{} {
+	exceptToSlice := func(obj map[any]any, key string) []any {
 		Expect(obj).ShouldNot(BeNil())
-		m, ok := obj[key].([]interface{})
+		m, ok := obj[key].([]any)
 		Expect(ok).Should(BeTrue())
 		return m
 	}
 
 	BeforeEach(func() {
-		obj = map[interface{}]interface{}{
+		obj = map[any]any{
 			"apiVersion": "rbac.authorization.k8s.io/v1",
 			"kind":       "ClusterRole",
-			"metadata": map[interface{}]interface{}{
+			"metadata": map[any]any{
 				"name": "kubeblocks-opsrequest-editor-role",
-				"labels": map[interface{}]interface{}{
+				"labels": map[any]any{
 					"helm.sh/chart":                "kubeblocks-0.5.1",
 					"app.kubernetes.io/name":       "kubeblocks",
 					"app.kubernetes.io/instance":   "kubeblocks",
@@ -110,11 +137,11 @@ var _ = Describe("helm diff", func() {
 					"app.kubernetes.io/managed-by": "Helm",
 				},
 			},
-			"rules": map[interface{}]interface{}{
+			"rules": map[any]any{
 				"description": "should be delete",
 				"change":      1,
-				"slice": []interface{}{
-					map[interface{}]interface{}{
+				"slice": []any{
+					map[any]any{
 						"description": "should be delete too",
 					},
 				},
@@ -122,10 +149,6 @@ var _ = Describe("helm diff", func() {
 		}
 		release = buildRelease(obj)
 		content = release.Manifest
-	})
-
-	AfterEach(func() {
-
 	})
 
 	It("test metadata String", func() {
@@ -188,25 +211,48 @@ var _ = Describe("helm diff", func() {
 		Expect(slice[0]).Should(BeEmpty())
 	})
 
-	It("test ParseContent", func() {
-		parseContent, err := ParseContent(content)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(parseContent.Name).Should(Equal(resourceName))
-		Expect(parseContent.Kind).Should(Equal("ClusterRole"))
-		unusefulContent := `---
+	Context("test ParseContent", func() {
+
+		It("test ParseContent", func() {
+			parseContent, err := ParseContent(content)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(parseContent.Name).Should(Equal(resourceName))
+			Expect(parseContent.Kind).Should(Equal("ClusterRole"))
+			unusefulContent := `---
 # Source: kubeblocks/templates/rbac/apps_backuppolicytemplate_editor_role.yaml
 # permissions for end users to edit backuppolicytemplates.
 `
-		parseContent, err = ParseContent(unusefulContent)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(parseContent).Should(BeNil())
-		errorContent := `---
-# Source: kubeblocks/tJ!@JDASD!bASD!@D!@
-# permissions for end users to edit backuppolicytemplates.
-ASDasdodh1*(!@#D!`
-		parseContent, err = ParseContent(errorContent)
-		Expect(err).Should(HaveOccurred())
-		Expect(parseContent).Should(BeNil())
+			parseContent, err = ParseContent(unusefulContent)
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(parseContent).Should(BeNil())
+			errorContent := `---
+			# Source: kubeblocks/tJ!@JDASD!bASD!@D!@
+			# permissions for end users to edit backuppolicytemplates.
+			ASDasdodh1*(!@#D!`
+			parseContent, err = ParseContent(errorContent)
+			Expect(err).Should(HaveOccurred())
+			Expect(parseContent).Should(BeNil())
+		})
+
+		It("test black list", func() {
+			blackListContent := `apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: grafana-chart-kubeblocks-values
+data:
+  values-kubeblocks-override.yaml: |-
+    adminPassword: kubeblocks
+    adminUser: admin`
+			parseContent, err := ParseContent(blackListContent)
+			Expect(err).Should(Succeed())
+			Expect(parseContent).Should(BeNil())
+		})
+
+		It("test Parse CRD", func() {
+			parseContent, err := ParseContent(crdContent)
+			Expect(err).Should(Succeed())
+			Expect(parseContent).ShouldNot(BeNil())
+		})
 	})
 
 	It("test buildManifestMapByRelease", func() {
@@ -215,24 +261,172 @@ ASDasdodh1*(!@#D!`
 		Expect(releaseMap).Should(HaveKey(resourceName))
 	})
 
-	It("test OutputDiff", func() {
-		releaseA := release
-		Expect(OutputDiff(releaseA, nil, "0.5.1-fake", "", &out)).ShouldNot(HaveOccurred())
-		Expect(out.String()).Should(Equal(exceptRemove))
-		out.Reset()
-		// add
-		otherObj := obj
-		rules := exceptToMap(otherObj, "rules")
-		rules["apiGroups"] = "apps"
-		releaseB := buildRelease(otherObj)
-		Expect(OutputDiff(releaseA, releaseB, "0.5.1-fake", "0.5.2-fake", &out)).ShouldNot(HaveOccurred())
-		Expect(out.String()).Should(Equal(exceptAdd))
-		// modify
-		out.Reset()
-		rules["apiGroups"] = "appsv2"
-		releaseA = buildRelease(otherObj)
-		Expect(OutputDiff(releaseA, releaseB, "0.5.1-fake", "0.5.2-fake", &out)).ShouldNot(HaveOccurred())
-		Expect(out.String()).Should(Equal(exceptModify))
+	Context("test OutputDiff", func() {
+		BeforeEach(func() {
+			crdObj = map[any]any{
+				"apiVersion": "apiextensions.k8s.io/v1",
+				"kind":       "CustomResourceDefinition",
+				"metadata": map[any]any{
+					"name": "kubeblocks-opsrequest-editor-role",
+					"labels": map[any]any{
+						"helm.sh/chart": "kubeblocks-0.5.1",
+					},
+				},
+				"spec": map[any]any{
+					"versions": []any{
+						map[any]any{
+							"schema": map[any]any{
+								"openAPIV3Schema": map[any]any{
+									"type": object,
+									"properties": map[any]any{
+										"apiVersion": map[any]any{"type": "string"},
+										"kind":       map[any]any{"type": "string"},
+										"metadata":   map[any]any{"type": object},
+										"spec": map[any]any{
+											"properties": map[any]any{
+												"fake": map[any]any{"type": "string"},
+											},
+											"type": object,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+		})
+		It("test OutputDiff CRD", func() {
+			crdRelease := buildRelease(crdObj)
+			Expect(OutputDiff(crdRelease, nil, "0.5.1-fake", "", &out, false)).ShouldNot(HaveOccurred())
+			Expect(out.String()).Should(Equal(`CUSTOMRESOURCEDEFINITION            MODE      
+kubeblocks-opsrequest-editor-role   Removed   
+
+`))
+			out.Reset()
+			Expect(OutputDiff(nil, crdRelease, "", "0.5.1-fake", &out, false)).Should(Succeed())
+			Expect(out.String()).Should(Equal(`CUSTOMRESOURCEDEFINITION            MODE    
+kubeblocks-opsrequest-editor-role   Added   
+
+`))
+		})
+
+		It("test OutputDiff detail", func() {
+			out.Reset()
+			releaseA := release
+			Expect(OutputDiff(releaseA, nil, "0.5.1-fake", "", &out, true)).Should(Succeed())
+			Expect(out.String()).Should(Equal(exceptRemove))
+			out.Reset()
+			// add
+			otherObj := obj
+			rules := exceptToMap(otherObj, "rules")
+			rules["apiGroups"] = "apps"
+			releaseB := buildRelease(otherObj)
+			Expect(OutputDiff(releaseA, releaseB, "0.5.1-fake", "0.5.2-fake", &out, true)).Should(Succeed())
+			Expect(out.String()).Should(Equal(exceptAdd))
+			// modify
+			out.Reset()
+			rules["apiGroups"] = "appsv2"
+			releaseA = buildRelease(otherObj)
+			Expect(OutputDiff(releaseA, releaseB, "0.5.1-fake", "0.5.2-fake", &out, true)).Should(Succeed())
+			Expect(out.String()).Should(Equal(exceptModify))
+		})
 	})
 
+	Context("test outputCRDDiff", func() {
+		var apiA map[string]any
+		var properties = map[any]any{}
+
+		removeAPIAndAddAPI := func() map[string]any {
+			var newProperties = map[any]any{}
+			maps.Copy(newProperties, properties)
+			delete(newProperties, "namespace")
+			newProperties["newApi"] = map[any]any{TYPE: "boolean"}
+			temp := map[string]any{
+				"spec": map[any]any{
+					REQUIRED:   []any{"name"},
+					TYPE:       object,
+					PROPERTIES: newProperties,
+				},
+			}
+			return temp
+		}
+
+		modifyTheRequired := func() map[string]any {
+			temp := map[string]any{
+				"spec": map[any]any{
+					TYPE:       object,
+					PROPERTIES: properties,
+				},
+			}
+			return temp
+		}
+
+		modifyTheField := func() map[string]any {
+			var newProperties = map[any]any{}
+			maps.Copy(newProperties, properties)
+			newProperties["name"] = map[any]any{TYPE: "string", "maxLength": 63}
+			temp := map[string]any{
+				"spec": map[any]any{
+					REQUIRED:   []any{"name"},
+					TYPE:       object,
+					PROPERTIES: newProperties,
+				},
+			}
+			return temp
+		}
+		BeforeEach(func() {
+			properties = map[any]any{
+				"name":      map[any]any{TYPE: "string"},
+				"namespace": map[any]any{TYPE: "string"},
+				"defaultInstallValues": map[any]any{
+					TYPE: array,
+					"items": map[any]any{
+						TYPE: object,
+						PROPERTIES: map[any]any{
+							"container1": map[any]any{TYPE: "string"},
+							"container2": map[any]any{TYPE: "string"},
+						},
+					},
+				},
+			}
+			apiA = map[string]any{
+				"spec": map[any]any{
+					REQUIRED:   []any{"name"},
+					TYPE:       object,
+					PROPERTIES: properties,
+				},
+			}
+		})
+		It("test Added and Removed", func() {
+			out.Reset()
+			apiB := removeAPIAndAddAPI()
+			outputCRDDiff(apiA, apiB, "Fake CRD", &out)
+			Expect(out.String()).Should(Equal(`Fake CRD
+API              IS-REQUIRED   MODE      DETAILS             
+spec.newApi      false         Added                         
+spec.namespace   false         Removed   {"type":"string"}   
+
+`))
+		})
+		It("test Modified", func() {
+			out.Reset()
+			apiB := modifyTheField()
+			outputCRDDiff(apiA, apiB, "Fake CRD", &out)
+			Expect(out.String()).Should(Equal(`Fake CRD
+API         IS-REQUIRED   MODE       DETAILS                                                 
+spec.name   true          Modified   {"type":"string"} -> {"maxLength":63,"type":"string"}   
+
+`))
+			out.Reset()
+			apiB = modifyTheRequired()
+			outputCRDDiff(apiA, apiB, "Fake CRD", &out)
+			Expect(out.String()).Should(Equal(`Fake CRD
+API         IS-REQUIRED     MODE       DETAILS   
+spec.name   true -> false   Modified             
+
+`))
+		})
+
+	})
 })

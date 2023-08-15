d5ae26689f3e3d183249e86b0e745fbfd7b1ce14
kizuna-lek <asd98041@qq.com>
2023-08-15 17:38:13 +0800

    style:modify name

--- cmd/probe/internal/highavailability/ha.go
+++ cmd/probe/internal/highavailability/ha.go
@@ -172,11 +172,11 @@ func (ha *Ha) Start() {
 		return
 	}
 
-	isDnsReady, err := ha.IsPodReady()
-	for err != nil || !isDnsReady {
+	isPodReady, err := ha.IsPodReady()
+	for err != nil || !isPodReady {
 		ha.logger.Infof("Waiting for dns resolution to be ready")
 		time.Sleep(3 * time.Second)
-		isDnsReady, err = ha.IsPodReady()
+		isPodReady, err = ha.IsPodReady()
 	}
 	ha.logger.Infof("dns resolution is ready")
 
@@ -303,7 +303,7 @@ func (ha *Ha) HasOtherHealthyMember(ctx context.Context, cluster *dcs.Cluster) b
 	return false
 }
 
-// IsPodReady check if pod is ready, it can successfully resolve domain
+// IsPodReady checks if pod is ready, it can successfully resolve domain
 func (ha *Ha) IsPodReady() (bool, error) {
 	domain := viper.GetString("KB_POD_FQDN")
 

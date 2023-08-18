5bedc5bf9e32587cc32fd7b5308f4e53d9a7256b
linghan-hub <56351212+linghan-hub@users.noreply.github.com>
2023-08-18 16:02:44 +0800

    chore: adjust e2e test report info (#4806)

--- test/e2e/testdata/smoketest/analyze_report.go
+++ test/e2e/testdata/smoketest/analyze_report.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	. "github.com/onsi/ginkgo/v2"
-	"github.com/pkg/errors"
 
 	. "github.com/apecloud/kubeblocks/test/e2e"
 	e2eutil "github.com/apecloud/kubeblocks/test/e2e/util"
@@ -25,13 +24,15 @@ func AnalyzeE2eReport() {
 		if err != nil {
 			log.Println(err)
 		}
-		It("e2e report", func() {
-			f, err := os.OpenFile(TestType+"-log.txt", os.O_CREATE|os.O_WRONLY, 0644)
+		It("create e2e report file", func() {
+			f, err := os.OpenFile(TestType+"-log.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 			if err != nil {
 				fmt.Println("Failed to open file:", err)
 				return
 			}
 			log.SetOutput(f)
+		})
+		It("e2e report", func() {
 			log.Println("\n====================" + TestType + " e2e test report====================")
 			if len(TestResults) > 0 && len(TestType) > 0 {
 				files, _ := e2eutil.GetFiles(dir + "/testdata/smoketest/" + TestType)
@@ -42,19 +43,30 @@ func AnalyzeE2eReport() {
 				} else {
 					log.Println("Total " + fmt.Sprint(len(TestResults)) + " | " + "Passed " + fmt.Sprint(len(TestResults)))
 				}
+				var CaseNames []string
 				for _, v := range TestResults {
+					CaseNames = append(CaseNames, v.CaseName)
 					if v.ExecuteResult {
 						log.Printf(" [PASS] [%s] %s %s ", v.CaseName, fmt.Sprint(v.ExecuteResult), v.TroubleShooting)
 					} else {
 						log.Printf(" [ERROR] [%s] %s %s ", v.CaseName, fmt.Sprint(v.ExecuteResult), v.TroubleShooting)
 					}
 				}
-
-			} else {
-				err := errors.New("[ERROR] create cluster failed")
-				log.Println(err)
+				if len(files) > len(TestResults) {
+					var filesNames []string
+					for _, file := range files {
+						name := e2eutil.GetPrefix(file, "/")
+						filesNames = append(filesNames, name)
+					}
+					diffs := e2eutil.RemoveElements(filesNames, CaseNames)
+					for _, diff := range diffs {
+						log.Printf(" [ERROR] [%s] %s %s ", diff, fmt.Sprint(false), "")
+					}
+				}
 			}
-			log.Println()
+			if len(TestResults) == 0 {
+				log.Println("[ERROR] create cluster failed")
+			}
 		})
 	})
 }

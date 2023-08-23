670713ffdadabdc7cdf1917594ea3399e65bb0e6
huyongqii <yongqi_hu@163.com>
2023-08-23 02:32:26 -0700

    integrate neon-autoscaler

--- deploy/neon/scripts/compute.sh
+++ deploy/neon/scripts/compute.sh
@@ -1,88 +1,90 @@
-#!/bin/sh
-  set -ex
+#!/bin/bash
+set -ex
+export PAGESERVER=neon-cluster-neon-pageserver
+export SAFEKEEPERS=neon-cluster-neon-safekeeper
+SPEC_FILE_ORG=/spec_prep_docker.json
+SPEC_FILE=/spec.json
+PG_VERSION=14
+echo "Waiting pageserver become ready."
+while ! nc -z $PAGESERVER 6400; do
+sleep 1;
+done
+echo "Page server is ready."
 
-  SPEC_FILE_ORG=spec.prep.DOCKER.json
-  SPEC_FILE=/tmp/spec.json
-  PG_VERSION=14
+echo "Create a tenant and timeline"
+if [ -z "$TENANT" ]; then
+PARAMS=(
+-sb
+-X POST
+-H "Content-Type: application/json"
+-d "{}"
+"http://${PAGESERVER}:9898/v1/tenant/"
+)
+tenant_id=$(curl "${PARAMS[@]}" | sed 's/"//g')
+else
+tenant_id=$TENANT
+fi
 
-  echo "Waiting pageserver become ready."
-  while ! nc -z $PAGESERVER 6400; do
-       sleep 1;
-  done
-  echo "Page server is ready."
+if [ -z "$TIMELINE" ]; then
+PARAMS=(
+-sb
+-X POST
+-H "Content-Type: application/json"
+-d "{\"tenant_id\":\"${tenant_id}\", \"pg_version\": ${PG_VERSION}}"
+"http://${PAGESERVER}:9898/v1/tenant/${tenant_id}/timeline/"
+)
+result=$(curl "${PARAMS[@]}")
+echo $result | jq .
 
-  echo "Create a tenant and timeline"
-  if [ -z "$TENANT" ]; then
-  PARAMS=(
-       -sb
-       -X POST
-       -H "Content-Type: application/json"
-       -d "{}"
-       http://${PAGESERVER}:9898/v1/tenant/
-  )
-  tenant_id=$(curl "${PARAMS[@]}" | sed 's/"//g')
-  else
-     tenant_id=$TENANT
-  fi
+echo "Overwrite tenant id and timeline id in spec file"
+tenant_id=$(echo ${result} | jq -r .tenant_id)
+timeline_id=$(echo ${result} | jq -r .timeline_id)
 
+else
 
-  if [ -z "$TIMELINE" ]; then
-  PARAMS=(
-       -sb
-       -X POST
-       -H "Content-Type: application/json"
-       -d "{\"tenant_id\":\"${tenant_id}\", \"pg_version\": ${PG_VERSION}}"
-       "http://${PAGESERVER}:9898/v1/tenant/${tenant_id}/timeline/"
-  )
-  result=$(curl "${PARAMS[@]}")
-  echo $result | jq .
+#If not empty CREATE_BRANCH
+#we create branch with given ancestor_timeline_id as TIMELINE
 
-  echo "Overwrite tenant id and timeline id in spec file"
-  tenant_id=$(echo ${result} | jq -r .tenant_id)
-  timeline_id=$(echo ${result} | jq -r .timeline_id)
+if [ ! -z "$CREATE_BRANCH" ]; then
 
-  else
+PARAMS=(
+-sb
+-X POST
+-H "Content-Type: application/json"
+-d "{\"tenant_id\":\"${tenant_id}\", \"pg_version\": ${PG_VERSION}, \"ancestor_timeline_id\":\"${TIMELINE}\"}"
+"http://${PAGESERVER}:9898/v1/tenant/${tenant_id}/timeline/"
+)
 
-  #If not empty CREATE_BRANCH
-  #we create branch with given ancestor_timeline_id as TIMELINE
+result=$(curl "${PARAMS[@]}")
+echo $result | jq .
 
-  if [ ! -z "$CREATE_BRANCH" ]; then
+echo "Overwrite tenant id and timeline id in spec file"
+tenant_id=$(echo ${result} | jq -r .tenant_id)
+timeline_id=$(echo ${result} | jq -r .timeline_id)
 
-  PARAMS=(
-       -sb
-       -X POST
-       -H "Content-Type: application/json"
-       -d "{\"tenant_id\":\"${tenant_id}\", \"pg_version\": ${PG_VERSION}, \"ancestor_timeline_id\":\"${TIMELINE}\"}"
-       "http://${PAGESERVER}:9898/v1/tenant/${tenant_id}/timeline/"
-  )
+else
+timeline_id=$TIMELINE
+fi #end if CREATE_BRANCH
 
-  result=$(curl "${PARAMS[@]}")
-  echo $result | jq .
+fi
 
-  echo "Overwrite tenant id and timeline id in spec file"
-  tenant_id=$(echo ${result} | jq -r .tenant_id)
-  timeline_id=$(echo ${result} | jq -r .timeline_id)
+sed "s/TENANT_ID/${tenant_id}/" ${SPEC_FILE_ORG} > ${SPEC_FILE}
+sed -i "s/TIMELINE_ID/${timeline_id}/" ${SPEC_FILE}
+sed -i "s/PAGESERVER_SPEC/${PAGESERVER}/" ${SPEC_FILE}
+sed -i "s/SAFEKEEPERS_SPEC/${SAFEKEEPERS}/" ${SPEC_FILE}
 
-  else
-      timeline_id=$TIMELINE
-  fi #end if CREATE_BRANCH
+cat ${SPEC_FILE}
 
-  fi
+echo "Start compute node"
+whoami
+echo $PWD
+if [ ! -d "data" ]; then
+mkdir data
+fi
+ls -lah /data
 
-  sed "s/TENANT_ID/${tenant_id}/" ${SPEC_FILE_ORG} > ${SPEC_FILE}
-  sed -i "s/TIMELINE_ID/${timeline_id}/" ${SPEC_FILE}
-  sed -i "s/PAGESERVER_SPEC/${PAGESERVER}/" ${SPEC_FILE}
-  sed -i "s/SAFEKEEPERS_SPEC/${SAFEKEEPERS}/" ${SPEC_FILE}
-
-  cat ${SPEC_FILE}
-
-  echo "Start compute node"
-  whoami
-  echo $PWD
-  ls -lah /data
-
-  mkdir /data/pgdata
-  /opt/neondatabase-neon/target/release/compute_ctl --pgdata /data/pgdata \
-       -C "postgresql://cloud_admin@localhost:55432/postgres"  \
-       -b /opt/neondatabase-neon/pg_install/v14/bin/postgres   \
-       -S ${SPEC_FILE}
+if [ ! -d "/data/pgdata" ]; then
+mkdir -p /data/pgdata
+fi
+chown -R postgres:postgres /data
+nohup su - postgres -c "/usr/local/bin/compute_ctl --pgdata /data/pgdata -C 'postgresql://cloud_admin@localhost:55432/postgres' -b /usr/local/bin/postgres -S ${SPEC_FILE}" &

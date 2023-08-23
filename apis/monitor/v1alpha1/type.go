fd70f5010fe8caaa6775a089387d0f14126ba295
sophon <sophon@apecloud.com>
2023-08-23 12:58:59 +0800

    add LokiConfig-type LogsExporterSink api

--- apis/monitor/v1alpha1/type.go
+++ apis/monitor/v1alpha1/type.go
@@ -19,5 +19,52 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package v1alpha1
 
+import "time"
+
+// LogsSinkType defines the type for LogsExporterSink
+// +enum
+// +kubebuilder:validation:Enum={S3SinkType,LokiSinkType}
+type LogsSinkType string
+
+const (
+	S3SinkType   LogsSinkType = "apeclouds3"
+	LokiSinkType LogsSinkType = "loki"
+)
+
+type RetryPolicyOnFailure struct {
+	// initialInterval the time to wait after the first failure before retrying.
+	// +optional
+	InitialInterval time.Duration `json:"initialInterval"`
+
+	// randomizationFactor is a random factor used to calculate next backoffs
+	// Randomized interval = RetryInterval * (1 ± RandomizationFactor)
+	// +optional
+	RandomizationFactor int64 `json:"randomizationFactor"`
+
+	// multiplier is the value multiplied by the backoff interval bounds
+	// +optional
+	Multiplier int64 `json:"multiplier"`
+
+	// maxInterval is the upper bound on backoff interval. Once this value is reached the delay between
+	// consecutive retries will always be `MaxInterval`.
+	// +optional
+	MaxInterval time.Duration `json:"maxInterval"`
+
+	// maxElapsedTime is the maximum amount of time (including retries) spent trying to send a request/batch.
+	// Once this value is reached, the data is discarded.
+	// +optional
+	MaxElapsedTime time.Duration `json:"maxElapsedTime"`
+}
+
+type SinkQueueConfig struct {
+	// numConsumers is the number of consumers from the queue.
+	// +optional
+	NumConsumers int `json:"numConsumers"`
+
+	// queueSize is the maximum number of batches allowed in queue at a given time.
+	// +optional
+	QueueSize int `json:"queueSize"`
+}
+
 type Config struct {
 }

4d5c52903fb0bd4c78a25201ff421d315e4406b1
sophon <sophon@apecloud.com>
2023-09-05 15:36:32 +0800

    chore: update crd config

--- apis/apps/v1alpha1/configuration_types.go
+++ apis/apps/v1alpha1/configuration_types.go
@@ -64,12 +64,12 @@ type ConfigurationSpec struct {
 	// clusterDefRef referencing ClusterDefinition name. This is an immutable attribute.
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Pattern:=`^[a-z0-9]([a-z0-9\.\-]*[a-z0-9])?$`
-	// ClusterDefRef string `json:"clusterDefRef"`
+	ClusterDefRef string `json:"clusterDefRef"`
 
 	// clusterVerRef referencing ClusterVersion name.
 	// +kubebuilder:validation:Pattern:=`^[a-z0-9]([a-z0-9\.\-]*[a-z0-9])?$`
 	// +optional
-	// ClusterVersionRef string `json:"clusterVerRef,omitempty"`
+	ClusterVersionRef string `json:"clusterVerRef,omitempty"`
 
 	// customConfigurationItems describes user-defined config template.
 	// +optional
@@ -77,7 +77,7 @@ type ConfigurationSpec struct {
 	// +patchStrategy=merge,retainKeys
 	// +listType=map
 	// +listMapKey=name
-	ConfigItemDetails []ConfigurationItemDetail `json:"ConfigItemDetails,omitempty"`
+	ConfigItemDetails []ConfigurationItemDetail `json:"configItemDetails,omitempty"`
 }
 
 type ConfigurationItemDetailStatus struct {

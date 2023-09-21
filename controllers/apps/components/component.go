096c2d3fcfec2f083155073a4aa3315229c9e7d0
wangyelei <yelei.wang@apecloud.com>
2023-09-21 17:52:49 +0800

    fix conflicts

--- controllers/apps/components/component.go
+++ controllers/apps/components/component.go
@@ -729,14 +729,13 @@ func (c *rsmComponent) isScaleOutFailed(reqCtx intctrlutil.RequestCtx, cli clien
 	} else if status == backupStatusFailed {
 		return true, nil
 	}
-	// TODO(wyl): to fix it
-	// for _, name := range d.pvcKeysToRestore() {
-	// 	if status, err := d.checkRestoreStatus(name); err != nil {
-	// 		return false, err
-	// 	} else if status == backupStatusFailed {
-	// 		return true, nil
-	// 	}
-	// }
+	for i := *c.runningWorkload.Spec.Replicas; i < c.component.Replicas; i++ {
+		if status, err := d.checkRestoreStatus(i); err != nil {
+			return false, err
+		} else if status == backupStatusFailed {
+			return true, nil
+		}
+	}
 	return false, nil
 }
 

dcc6faceb53769de3c15a53b61738a22c8fb777f
wangyelei <yelei.wang@apecloud.com>
2023-10-11 12:50:51 +0800

    chore: remove the failure check for volumeExpansion opsRequest (#5379)
    (cherry picked from commit 22d1387bb4b4c6666910ee3258f6f889bea880b5)

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -361,15 +361,6 @@ func main() {
 			os.Exit(1)
 		}
 
-		if err = (&k8scorecontrollers.PersistentVolumeClaimReconciler{
-			Client:   mgr.GetClient(),
-			Scheme:   mgr.GetScheme(),
-			Recorder: mgr.GetEventRecorderFor("pvc-controller"),
-		}).SetupWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create controller", "controller", "PersistentVolumeClaim")
-			os.Exit(1)
-		}
-
 		if err = (&appscontrollers.ComponentClassReconciler{
 			Client:   mgr.GetClient(),
 			Scheme:   mgr.GetScheme(),

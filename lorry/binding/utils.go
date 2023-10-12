ceb4c7fe5f064ff6160fe0f506b2e514fa94cd89
xuriwuyun <xuriwuyun@gmail.com>
2023-10-12 18:21:05 +0800

    fix: update mongodb checkrole (#5410)

--- lorry/binding/utils.go
+++ lorry/binding/utils.go
@@ -224,7 +224,7 @@ func String2RoleType(roleName string) RoleType {
 
 func SentProbeEvent(ctx context.Context, opsResult OpsResult, resp *ProbeResponse, log logr.Logger) {
 	log.Info(fmt.Sprintf("send event: %v", opsResult))
-	roleUpdateMechanism := workloads.NoneUpdate
+	roleUpdateMechanism := workloads.DirectAPIServerEventUpdate
 	if viper.IsSet(rsmRoleUpdateMechanismVarName) {
 		roleUpdateMechanism = workloads.RoleUpdateMechanism(viper.GetString(rsmRoleUpdateMechanismVarName))
 	}

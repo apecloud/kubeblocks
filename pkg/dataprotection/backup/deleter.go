8a629787715b466547528b6b1bc160036e526be3
wangyelei <yelei.wang@apecloud.com>
2023-10-25 17:31:50 +0800

    fix conflict

--- pkg/dataprotection/backup/deleter.go
+++ pkg/dataprotection/backup/deleter.go
@@ -236,7 +236,7 @@ func (d *Deleter) DeleteVolumeSnapshots(backup *dpv1alpha1.Backup) error {
 	snaps := &vsv1.VolumeSnapshotList{}
 	if err := vsCli.List(snaps, client.InNamespace(backup.Namespace),
 		client.MatchingLabels(map[string]string{
-			dptypes.DataProtectionLabelBackupNameKey: backup.Name,
+			dptypes.BackupNameLabelKey: backup.Name,
 		})); err != nil {
 		return client.IgnoreNotFound(err)
 	}

9be21e46ec090a60d57f4094c518d568ce7f1dad
free6om <free6om.c@gmail.com>
2023-11-01 17:04:17 +0800

    fix: cluster phase keeps in Updating after reconfiguration done (#5721)
    (cherry picked from commit b391f0dfa664a27fce37d9f40431e7553b23feda)

--- pkg/controller/configuration/pipeline.go
+++ pkg/controller/configuration/pipeline.go
@@ -112,7 +112,7 @@ func (p *pipeline) RenderScriptTemplate() *pipeline {
 func (p *pipeline) UpdateConfiguration() *pipeline {
 	buildConfiguration := func() (err error) {
 		expectedConfiguration := p.createConfiguration()
-		if intctrlutil.SetOwnerReference(p.ctx.Cluster, expectedConfiguration) != nil {
+		if intctrlutil.SetControllerReference(p.ctx.Cluster, expectedConfiguration) != nil {
 			return
 		}
 

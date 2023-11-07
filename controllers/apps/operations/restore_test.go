cea5654e7b1823c87f67f0a59aff2a86fe9a96bc
fengluodb <woaibiancheng31@gmail.com>
2023-11-07 17:27:48 +0800

    test: add testcase for restore

--- controllers/apps/operations/restore_test.go
+++ controllers/apps/operations/restore_test.go
@@ -23,21 +23,22 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	appsv1alpha1 "github.com/apecloud/kubeblocks/apis/apps/v1alpha1"
+	dpv1alpha1 "github.com/apecloud/kubeblocks/apis/dataprotection/v1alpha1"
+	"github.com/apecloud/kubeblocks/pkg/constant"
 	intctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
 	"github.com/apecloud/kubeblocks/pkg/generics"
 	testapps "github.com/apecloud/kubeblocks/pkg/testutil/apps"
+	testdp "github.com/apecloud/kubeblocks/pkg/testutil/dataprotection"
 )
 
-var _ = Describe("Backup OpsRequest", func() {
-
+var _ = Describe("Restore OpsRequest", func() {
 	var (
-		randomStr             = testCtx.GetRandomStr()
-		clusterDefinitionName = "cluster-definition-for-ops-" + randomStr
-		clusterVersionName    = "clusterversion-for-ops-" + randomStr
-		clusterName           = "cluster-for-ops-" + randomStr
+		randomStr   = testCtx.GetRandomStr()
+		clusterName = "cluster-for-ops-" + randomStr
 	)
 
 	cleanEnv := func() {
@@ -61,41 +62,57 @@ var _ = Describe("Backup OpsRequest", func() {
 
 	AfterEach(cleanEnv)
 
-	Context("Test OpsRequest for backup", func() {
+	Context("Test OpsRequest for Restore", func() {
 		var (
 			opsRes *OpsResource
 			reqCtx intctrlutil.RequestCtx
+			backup *dpv1alpha1.Backup
 		)
 		BeforeEach(func() {
 			By("init operations resources ")
-			opsRes, _, _ = initOperationsResources(clusterDefinitionName, clusterVersionName, clusterName)
+			backup = testdp.NewFakeBackup(&testCtx, nil)
 			reqCtx = intctrlutil.RequestCtx{Ctx: testCtx.Ctx}
 		})
 
-		It("should create a backup resource for cluster", func() {
-			By("create Backup OpsRequest")
-			opsRes.OpsRequest = createBackupOpsObj(clusterName, "backup-ops-"+randomStr)
+		It("", func() {
+			By("create Restore OpsRequest")
+			opsRes.OpsRequest = createRestoreOpsObj(clusterName, "restore-ops-"+randomStr, backup.Name)
 
-			By("mock backup OpsRequest is Running")
+			By("mock restore OpsRequest is Running")
 			_, err := GetOpsManager().Do(reqCtx, k8sClient, opsRes)
 			Expect(err).ShouldNot(HaveOccurred())
 			Eventually(testapps.GetOpsRequestPhase(&testCtx, client.ObjectKeyFromObject(opsRes.OpsRequest))).Should(Equal(appsv1alpha1.OpsCreatingPhase))
 
-			By("test backup action and reconcile function")
+			By("test restore action and reconcile function")
 			testapps.MockConsensusComponentStatefulSet(&testCtx, clusterName, consensusComp)
 			testapps.MockStatelessComponentDeploy(&testCtx, clusterName, statelessComp)
-			bHandler := BackupOpsHandler{}
-			_ = bHandler.Action(reqCtx, k8sClient, opsRes)
+			restoreHandler := RestoreOpsHandler{}
+			Expect(restoreHandler.Action(reqCtx, k8sClient, opsRes)).Should(Succeed())
 
-			By("test backup reconcile action")
+			By("test restore reconcile function")
 			_, err = GetOpsManager().Reconcile(reqCtx, k8sClient, opsRes)
 			Expect(err).ShouldNot(HaveOccurred())
 		})
 	})
 })
 
-func createBackupOpsObj(clusterName, backupOpsName string) *appsv1alpha1.OpsRequest {
-	ops := testapps.NewOpsRequestObj(backupOpsName, testCtx.DefaultNamespace,
-		clusterName, appsv1alpha1.BackupType)
+func createRestoreOpsObj(clusterName, restoreOpsName, backupName string) *appsv1alpha1.OpsRequest {
+	ops := &appsv1alpha1.OpsRequest{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      restoreOpsName,
+			Namespace: testCtx.DefaultNamespace,
+			Labels: map[string]string{
+				constant.AppInstanceLabelKey:    clusterName,
+				constant.OpsRequestTypeLabelKey: string(appsv1alpha1.RestoreType),
+			},
+		},
+		Spec: appsv1alpha1.OpsRequestSpec{
+			ClusterRef: clusterName,
+			Type:       appsv1alpha1.RestoreType,
+			RestoreSpec: &appsv1alpha1.RestoreSpec{
+				BackupName: backupName,
+			},
+		},
+	}
 	return testapps.CreateOpsRequest(ctx, testCtx, ops)
 }

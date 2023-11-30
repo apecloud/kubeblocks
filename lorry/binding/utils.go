399e1ba8b0f3ca9da43f22376d8fc7cc0ab95f15
xuriwuyun <xuriwuyun@gmail.com>
2023-11-30 21:29:05 +0800

    fix send event failed

--- lorry/binding/utils.go
+++ lorry/binding/utils.go
@@ -223,6 +223,7 @@ func String2RoleType(roleName string) RoleType {
 }
 
 func SentProbeEvent(ctx context.Context, opsResult OpsResult, resp *ProbeResponse, log logr.Logger) {
+	ctx = context.Background()
 	log.Info(fmt.Sprintf("send event: %v", opsResult))
 	roleUpdateMechanism := workloads.DirectAPIServerEventUpdate
 	if viper.IsSet(RSMRoleUpdateMechanismVarName) {

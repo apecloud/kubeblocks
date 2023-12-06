c9c7a2802e5c78956f1730541b0f1754c4a8daa9
sophon <sophon@apecloud.com>
2023-12-06 17:33:57 +0800

    fix: make lint fails

--- apis/apps/v1alpha1/opsrequest_webhook.go
+++ apis/apps/v1alpha1/opsrequest_webhook.go
@@ -291,20 +291,17 @@ func (r *OpsRequest) validateReconfigure(cluster *Cluster) error {
 		return notEmptyError("spec.reconfigure")
 	}
 	if reconfigure != nil {
-		return r.validateReconfigureParams(ctx, k8sClient, cluster, reconfigure)
+		return r.validateReconfigureParams(cluster, reconfigure)
 	}
 	for _, reconfigure := range r.Spec.Reconfigures {
-		if err := r.validateReconfigureParams(ctx, k8sClient, cluster, &reconfigure); err != nil {
+		if err := r.validateReconfigureParams(cluster, &reconfigure); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (r *OpsRequest) validateReconfigureParams(ctx context.Context,
-	k8sClient client.Client,
-	cluster *Cluster,
-	reconfigure *Reconfigure) error {
+func (r *OpsRequest) validateReconfigureParams(cluster *Cluster, reconfigure *Reconfigure) error {
 	if cluster.Spec.GetComponentByName(reconfigure.ComponentName) == nil {
 		return fmt.Errorf("component %s not found", reconfigure.ComponentName)
 	}

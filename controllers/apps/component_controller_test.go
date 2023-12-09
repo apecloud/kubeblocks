8b9e94956a27f61391ae4550efae4706f1a3027e
free6om <free6om.c@gmail.com>
2023-12-09 21:52:52 +0800

    fix lint error

--- controllers/apps/component_controller_test.go
+++ controllers/apps/component_controller_test.go
@@ -1393,7 +1393,7 @@ var _ = Describe("Component Controller", func() {
 						{Key: constant.CertName, Path: constant.CertName},
 						{Key: constant.KeyName, Path: constant.KeyName},
 					},
-					Optional: func() *bool { o := false; return &o }(),
+					Optional:    func() *bool { o := false; return &o }(),
 					DefaultMode: func() *int32 { m := int32(0600); return &m }(),
 				},
 			},

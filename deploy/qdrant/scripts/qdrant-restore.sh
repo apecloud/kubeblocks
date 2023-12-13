a456768b6f0aea4a59c12b1ac1a2ae5d88eb0db4
wangyelei <yelei.wang@apecloud.com>
2023-12-13 18:49:19 +0800

    fix: qdrant cluster restore failed

--- deploy/qdrant/scripts/qdrant-restore.sh
+++ deploy/qdrant/scripts/qdrant-restore.sh
@@ -4,34 +4,18 @@ set -e
 set -o pipefail
 export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
 export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
-mkdir -p ${DATA_DIR}
-res=`find ${DATA_DIR} -type f`
-if [ ! -z "${res}" ]; then
-  echo "${DATA_DIR} is not empty! Please make sure that the directory is empty before restoring the backup."
-  exit 1
-fi
 
-# download snapshot file
 SNAPSHOT_DIR="${DATA_DIR}/_dp_snapshots"
-SNAPSHOT_FILE="${DP_BACKUP_NAME}.snapshot"
 mkdir -p "${SNAPSHOT_DIR}"
-datasafed pull "${SNAPSHOT_FILE}" "${SNAPSHOT_DIR}/${SNAPSHOT_FILE}"
-
-# start qdrant restore process
-/qdrant/qdrant --storage-snapshot "${SNAPSHOT_DIR}/${SNAPSHOT_FILE}"  --config-path /qdrant/config/config.yaml  --force-snapshot --uri http://localhost:6333 2>&1 | tee "${DATA_DIR}/restore.log" &
-
-# wait until restore finished
-# note: if we have curl, we can detect it this way
-#   until curl http://localhost:6333/cluster; do sleep 1; done
-until grep -q "Qdrant HTTP listening on" "${DATA_DIR}/restore.log"; do
-  sleep 1
+for snapshot in $(datasafed list /) ; do
+  collection_name=${snapshot%.*}
+  echo "INFO: start to restore collection ${collection_name}..."
+  # download snapshot file
+  datasafed pull "${snapshot}" "${SNAPSHOT_DIR}/${snapshot}"
+  curl -X POST "http://${DP_DB_HOST}:6333/collections/${collection_name}/snapshots/upload?priority=snapshot" \
+    -H 'Content-Type:multipart/form-data' \
+    -F "snapshot=@${SNAPSHOT_DIR}/${snapshot}"
+  echo "upload collection ${collection_name} successfully"
 done
-rm "${DATA_DIR}/restore.log"
 
-# restore finished, we can kill the restore process now
-pid=`pidof qdrant`
-kill -s INT ${pid}
-wait ${pid}
 
-# delete snapshot file
-rm -rf "${SNAPSHOT_DIR}"
--- deploy/qdrant/scripts/qdrant-setup.sh
+++ deploy/qdrant/scripts/qdrant-setup.sh
@@ -7,5 +7,10 @@ BOOTSTRAP_HOSTNAME=$(eval echo \$KB_QDRANT_0_HOSTNAME)
 if [ "$IDX" == "0" ]; then
   ./qdrant --uri "http://${HOSTNAME}:6335"
 else
+  echo "BOOTSTRAP_HOSTNAME: ${BOOTSTRAP_HOSTNAME}"
+  until ./tools/curl http://${BOOTSTRAP_HOSTNAME}:6333/cluster; do
+    echo "INFO: wait for bootstrap node starting..."
+    sleep 1;
+  done
   ./qdrant --bootstrap "http://${BOOTSTRAP_HOSTNAME}:6335" --uri "http://${HOSTNAME}:6335"
 fi

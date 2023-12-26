284c0561e578dc0699b6e63ea20a6201745c9e9e
xuriwuyun <xuriwuyun@gmail.com>
2023-12-26 16:00:07 +0800

    fix oceanbase switchover

--- pkg/lorry/dcs/k8s.go
+++ pkg/lorry/dcs/k8s.go
@@ -231,7 +231,7 @@ func (store *KubernetesStore) GetMembers() ([]Member, error) {
 		member := &members[i]
 		member.Name = pod.Name
 		// member.Name = fmt.Sprintf("%s.%s-headless.%s.svc", pod.Name, store.clusterCompName, store.namespace)
-		member.Role = pod.Labels["app.kubernetes.io/role"]
+		member.Role = pod.Labels[constant.RoleLabelKey]
 		member.PodIP = pod.Status.PodIP
 		member.DBPort = getDBPort(&pod)
 		member.LorryPort = getLorryPort(&pod)

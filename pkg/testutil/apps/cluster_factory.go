462228caa681d091fcba00c4fb5720a00f6fd48c
Y-Rookie <yong.huang@apecloud.com>
2023-12-29 15:54:24 +0800

    fix ut

--- pkg/testutil/apps/cluster_factory.go
+++ pkg/testutil/apps/cluster_factory.go
@@ -81,10 +81,10 @@ func (factory *MockClusterFactory) AddComponentV2(compName string, compDefName s
 	return factory
 }
 
-func (factory *MockClusterFactory) AddService(service appsv1alpha1.Service) *MockClusterFactory {
+func (factory *MockClusterFactory) AddService(service appsv1alpha1.ClusterService) *MockClusterFactory {
 	services := factory.Get().Spec.Services
 	if len(services) == 0 {
-		services = []appsv1alpha1.Service{}
+		services = []appsv1alpha1.ClusterService{}
 	}
 	services = append(services, service)
 	factory.Get().Spec.Services = services

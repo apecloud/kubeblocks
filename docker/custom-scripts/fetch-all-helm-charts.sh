9e5df7dffaa59292aaeea4dfd82c5304994ed228
huangzhangshu <huangzhangshu@apecloud.com>
2024-01-09 11:04:31 +0800

    chore: change to github addon url

--- docker/custom-scripts/fetch-all-helm-charts.sh
+++ docker/custom-scripts/fetch-all-helm-charts.sh
@@ -14,6 +14,7 @@ fi
 KB_CHART_DIR=${1}
 TARGET_DIR=${2:-"charts"}
 MANIFESTS_DIR="/tmp/manifests/"
+GITHUB_HELM_CHARTS_URL=https://github.com/apecloud/helm-charts/releases/download
 
 ADDON_DIR="kubeblocks/templates/addons"
 APP_DIR="kubeblocks/templates/applications"
@@ -57,8 +58,14 @@ mkdir -p "${MANIFESTS_DIR}"
 # get all manifests
 helm version
 
-echo "fetch addons helm charts, addon CRs directory: ${ADDON_DIR}, helm chart url: ${ADDON_HELM_CHART_URL}"
-fetch_helm_charts "${ADDON_HELM_CHART_URL}" "${ADDON_DIR}"
+echo "fetch addons helm charts, addon CRs directory: ${ADDON_DIR}, helm chart url: ${GITHUB_HELM_CHARTS_URL}"
+fetch_helm_charts "${GITHUB_HELM_CHARTS_URL}" "${ADDON_DIR}"
 
-echo "fetch applications helm charts, applications CRs directory: ${APP_DIR}, helm chart url: ${APP_HELM_CHART_URL}"
-fetch_helm_charts "${APP_HELM_CHART_URL}" "${APP_DIR}"
+echo "fetch applications helm charts, applications CRs directory: ${APP_DIR}, helm chart url: ${GITHUB_HELM_CHARTS_URL}"
+fetch_helm_charts "${GITHUB_HELM_CHARTS_URL}" "${APP_DIR}"
+
+#echo "fetch addons helm charts, addon CRs directory: ${ADDON_DIR}, helm chart url: ${ADDON_HELM_CHART_URL}"
+#fetch_helm_charts "${ADDON_HELM_CHART_URL}" "${ADDON_DIR}"
+
+#echo "fetch applications helm charts, applications CRs directory: ${APP_DIR}, helm chart url: ${APP_HELM_CHART_URL}"
+#fetch_helm_charts "${APP_HELM_CHART_URL}" "${APP_DIR}"

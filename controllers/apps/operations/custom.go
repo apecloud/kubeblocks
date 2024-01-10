8504bc4479c0f3d44b612c9025909b5044231c5b
wangyelei <yelei.wang@apecloud.com>
2024-01-10 22:22:42 +0800

    fix: create job failed with no this sa for custom ops (#6415)
    (cherry picked from commit 4e671c83b126152fb831ad7c49e8d4d09af75dd0)

--- controllers/apps/operations/custom.go
+++ controllers/apps/operations/custom.go
@@ -233,13 +233,15 @@ func (c CustomOpsHandler) buildJob(reqCtx intctrlutil.RequestCtx,
 		if len(jobSpec.Template.Spec.Tolerations) == 0 {
 			jobSpec.Template.Spec.Tolerations = comp.Tolerations
 		}
-		getSAName := func() string {
-			if comp.ServiceAccountName != "" {
-				return comp.ServiceAccountName
+		if comp.ServiceAccountName != "" {
+			jobSpec.Template.Spec.ServiceAccountName = comp.ServiceAccountName
+		} else {
+			saKey := client.ObjectKey{Namespace: opsRes.Cluster.Namespace,
+				Name: constant.GenerateDefaultServiceAccountName(opsRes.Cluster.Name)}
+			if exists, _ := intctrlutil.CheckResourceExists(reqCtx.Ctx, cli, saKey, &corev1.ServiceAccount{}); exists {
+				jobSpec.Template.Spec.ServiceAccountName = comp.ServiceAccountName
 			}
-			return constant.GenerateDefaultServiceAccountName(opsRes.Cluster.Name)
 		}
-		jobSpec.Template.Spec.ServiceAccountName = getSAName()
 		return &jobSpec, nil
 	}
 

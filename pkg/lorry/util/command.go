42ef83aa467e0920942dd4cd8a14aa47bf386da3
xuriwuyun <xuriwuyun@gmail.com>
2024-01-15 18:11:57 +0800

    get role use command if specified

---
+++ pkg/lorry/util/command.go
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2022-2023 ApeCloud Co., Ltd
+
+This file is part of KubeBlocks project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package util
+
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
+func ExecCommand(command []string) (string, error) {
+	if len(command) == 0 {
+		return "", errors.New("command can not be empty")
+	}
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Env = os.Environ()
+	bytes, err := cmd.Output()
+	return string(bytes), err
+}

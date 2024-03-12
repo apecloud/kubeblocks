ccb1f21d7b479a65588b8a565a81c7c8365fa2b6
wusai80 <wusai80@gmail.com>
2024-03-12 13:31:32 +0800

    fix: convert rsmSpec roles

--- apis/apps/v1alpha1/zz_generated.deepcopy.go
+++ apis/apps/v1alpha1/zz_generated.deepcopy.go
@@ -26,7 +26,7 @@ package v1alpha1
 
 import (
 	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"

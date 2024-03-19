7314f425e5c952db3f4dede185f3e300b6fb2ee0
xuriwuyun <xuriwuyun@gmail.com>
2024-03-19 16:15:55 +0800

    update log

--- pkg/lorry/util/event.go
+++ pkg/lorry/util/event.go
@@ -58,7 +58,7 @@ func SentEventForProbe(ctx context.Context, data map[string]any) error {
 		}
 		event, err := CreateEvent(string(operation.(OperationKind)), data)
 		if err != nil {
-			logger.Error(err, "generate event failed")
+			logger.Info("generate event failed", "error", err.Error())
 			return err
 		}
 
@@ -116,13 +116,13 @@ func SendEvent(ctx context.Context, event *corev1.Event) error {
 	ctx1 := context.Background()
 	config, err := ctlruntime.GetConfig()
 	if err != nil {
-		logger.Error(err, "get k8s client config failed")
+		logger.Info("get k8s client config failed", "error", err.Error())
 		return err
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		logger.Error(err, "k8s client create failed")
+		logger.Info("k8s client create failed", "error", err.Error())
 		return err
 	}
 	namespace := os.Getenv(constant.KBEnvNamespace)
@@ -131,7 +131,7 @@ func SendEvent(ctx context.Context, event *corev1.Event) error {
 		if err == nil {
 			break
 		}
-		logger.Error(err, "send event failed")
+		logger.Info("send event failed", "error", err.Error())
 		time.Sleep(10 * time.Second)
 	}
 	return err

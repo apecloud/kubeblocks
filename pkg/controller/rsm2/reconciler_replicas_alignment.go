4773f5d43c0b0863ea85aa4dd07d2a2f1de827fe
free6om <free6om.c@gmail.com>
2024-03-24 23:45:32 +0800

    make replicas creation parallel for test

--- pkg/controller/rsm2/reconciler_replicas_alignment.go
+++ pkg/controller/rsm2/reconciler_replicas_alignment.go
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package rsm2
 
 import (
-	apps "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 
@@ -81,7 +80,7 @@ func (r *replicasAlignmentReconciler) Reconcile(tree *kubebuilderx.ObjectTree) (
 	// default OrderedReady policy
 	createCount, deleteCount := 1, 1
 	shouldReady := true
-	if rsm.Spec.PodManagementPolicy == apps.ParallelPodManagement {
+	if rsm.Spec.PodManagementPolicy == "" {
 		createCount = len(createNameSet)
 		deleteCount = len(deleteNameSet)
 		shouldReady = false

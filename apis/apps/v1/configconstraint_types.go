a3887212634433acab642451c896ddab7f05b936
sophon <sophon@apecloud.com>
2024-03-26 22:08:24 +0800

    chore: add apps/v1 sdk

---
+++ apis/apps/v1/configconstraint_types.go
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2022-2024 ApeCloud Co., Ltd
+
+This file is part of KubeBlocks project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ConfigConstraintSpec defines the desired state of ConfigConstraint
+type ConfigConstraintSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ConfigConstraint. Edit configconstraint_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ConfigConstraintStatus defines the observed state of ConfigConstraint
+type ConfigConstraintStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:storageversion
+
+// ConfigConstraint is the Schema for the configconstraints API
+type ConfigConstraint struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ConfigConstraintSpec   `json:"spec,omitempty"`
+	Status ConfigConstraintStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ConfigConstraintList contains a list of ConfigConstraint
+type ConfigConstraintList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ConfigConstraint `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ConfigConstraint{}, &ConfigConstraintList{})
+}

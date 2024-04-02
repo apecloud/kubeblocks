b664826031978b50ae0b4a19ec608ecae5d4c124
wusai80 <wusai80@gmail.com>
2024-04-02 14:47:14 +0800

    chore: update apis licence to Apache

--- apis/dataprotection/v1alpha1/actionset_types.go
+++ apis/dataprotection/v1alpha1/actionset_types.go
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2022-2023 ApeCloud Co., Ltd
+Copyright (C) 2022-2024 ApeCloud Co., Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

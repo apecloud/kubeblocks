f96f4649b5c7ca9bfeabae6394b0cde54bd94679
free6om <free6om.c@gmail.com>
2024-04-10 07:58:54 +0800

    feat: support cluster RuntimeClassName configuration (#7001)

--- apis/workloads/v1alpha1/zz_generated.deepcopy.go
+++ apis/workloads/v1alpha1/zz_generated.deepcopy.go
@@ -138,6 +138,11 @@ func (in *InstanceTemplate) DeepCopyInto(out *InstanceTemplate) {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
+	if in.RuntimeClassName != nil {
+		in, out := &in.RuntimeClassName, &out.RuntimeClassName
+		*out = new(string)
+		**out = **in
+	}
 	if in.Resources != nil {
 		in, out := &in.Resources, &out.Resources
 		*out = new(v1.ResourceRequirements)

dab912702f48aa96567adcb1b78a018caebd42cf
free6om <free6om.c@gmail.com>
2024-04-20 14:00:43 +0800

    watch pod by labels

--- controllers/workloads/instanceset_controller.go
+++ controllers/workloads/instanceset_controller.go
@@ -194,6 +194,9 @@ func (r *InstanceSetReconciler) SetupWithManager(mgr ctrl.Manager, multiClusterM
 }
 
 func (r *InstanceSetReconciler) setupWithManager(mgr ctrl.Manager, ctx *handler.FinderContext) error {
+	itsFinder := handler.NewLabelFinder(&workloads.InstanceSet{}, rsm.WorkloadsManagedByLabelKey, rsm.KindInstanceSet, rsm.WorkloadsInstanceLabelKey)
+	podHandler := handler.NewBuilder(ctx).AddFinder(itsFinder).Build()
+
 	if viper.GetBool(rsm.FeatureGateRSMCompatibilityMode) {
 		nameLabels := []string{constant.AppInstanceLabelKey, constant.KBAppComponentLabelKey}
 		delegatorFinder := handler.NewDelegatorFinder(&workloads.InstanceSet{}, nameLabels)
@@ -211,13 +214,11 @@ func (r *InstanceSetReconciler) setupWithManager(mgr ctrl.Manager, ctx *handler.
 			Watches(&appsv1.StatefulSet{}, stsHandler).
 			Watches(&batchv1.Job{}, jobHandler).
 			Watches(&corev1.Pod{}, stsPodHandler).
-			Owns(&corev1.Pod{}).
+			Watches(&corev1.Pod{}, podHandler).
 			Owns(&corev1.PersistentVolumeClaim{}).
 			Complete(r)
 	}
 
-	itsFinder := handler.NewLabelFinder(&workloads.InstanceSet{}, rsm.WorkloadsManagedByLabelKey, rsm.KindInstanceSet, rsm.WorkloadsInstanceLabelKey)
-	podHandler := handler.NewBuilder(ctx).AddFinder(itsFinder).Build()
 	return intctrlutil.NewNamespacedControllerManagedBy(mgr).
 		For(&workloads.InstanceSet{}).
 		WithOptions(controller.Options{

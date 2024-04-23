bb358772f9df3b43a6862df099eac568ed99dfac
wangyelei <yelei.wang@apecloud.com>
2024-04-23 10:02:46 +0800

    cleanup test code

--- clean_pv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pvs=$(kubectl get pv | grep Released | awk '{print $1}')
-
-for pv in $pvs; do
-  kubectl delete pv $pv
-done
--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -152,8 +152,8 @@ func (r flagName) viperName() string {
 }
 
 func setupFlags() {
-	flag.String(metricsAddrFlagKey.String(), ":8082", "The address the metric endpoint binds to.")
-	flag.String(probeAddrFlagKey.String(), ":8083", "The address the probe endpoint binds to.")
+	flag.String(metricsAddrFlagKey.String(), ":8080", "The address the metric endpoint binds to.")
+	flag.String(probeAddrFlagKey.String(), ":8081", "The address the probe endpoint binds to.")
 	flag.Bool(leaderElectFlagKey.String(), false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")

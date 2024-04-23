b32970b42dc74a40a3e0be288873f1d3fcfcd21a
Y-Rookie <yong.huang@apecloud.com>
2024-04-23 16:11:23 +0800

    remove update primary index

--- controllers/apps/transformer_component_status.go
+++ controllers/apps/transformer_component_status.go
@@ -24,7 +24,6 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/exp/slices"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -217,13 +216,6 @@ func (r *componentStatusHandler) reconcileComponentStatus() error {
 		r.setComponentStatusPhase(appsv1alpha1.FailedClusterCompPhase, messages, "component is Failed")
 	default:
 		r.setComponentStatusPhase(appsv1alpha1.AbnormalClusterCompPhase, nil, "component is Abnormal")
-	}
-
-	// set primary-pod annotation
-	// TODO(free6om): primary-pod is only used in redis to bootstrap the redis cluster correctly.
-	// it is too hacky to be replaced by a better design.
-	if err := r.updatePrimaryIndex(); err != nil {
-		return err
 	}
 
 	return nil
@@ -484,67 +476,6 @@ func (r *componentStatusHandler) updateComponentStatus(phaseTransitionMsg string
 	if phase != r.comp.Status.Phase {
 		if r.reqCtx.Recorder != nil && phaseTransitionMsg != "" {
 			r.reqCtx.Recorder.Eventf(r.comp, corev1.EventTypeNormal, componentPhaseTransition, phaseTransitionMsg)
-		}
-	}
-	return nil
-}
-
-// updatePrimaryIndex updates the primary pod index to the pod annotations.
-// TODO: the need to update primary info is because some database engines currently require external specification of the primary node.
-// Based on the specified primary node, the primary-secondary relationship can be established during startup. Examples of such engines include primary-secondary Redis.
-// In the future, there is a need for a better design to replace this kind of workaround.
-func (r *componentStatusHandler) updatePrimaryIndex() error {
-	// TODO(xingran): consider if there are alternative ways to determine whether it is necessary to specify primary info in the Controller
-	if r.synthesizeComp.RoleArbitrator == nil || *r.synthesizeComp.RoleArbitrator != appsv1alpha1.LorryRoleArbitrator {
-		return nil
-	}
-	podList, err := component.ListPodOwnedByComponent(r.reqCtx.Ctx, r.cli, r.cluster.Namespace,
-		constant.GetComponentWellKnownLabels(r.cluster.Name, r.synthesizeComp.Name), inDataContext4C())
-	if err != nil {
-		return err
-	}
-	if len(podList) == 0 {
-		return nil
-	}
-	slices.SortFunc(podList, func(a, b *corev1.Pod) bool {
-		return a.GetName() < b.GetName()
-	})
-	primaryPods := make([]string, 0)
-	emptyRolePods := make([]string, 0)
-	for _, pod := range podList {
-		role, ok := pod.Labels[constant.RoleLabelKey]
-		if !ok || role == "" {
-			emptyRolePods = append(emptyRolePods, pod.Name)
-			continue
-		}
-		if role == constant.Primary {
-			primaryPods = append(primaryPods, pod.Name)
-		}
-	}
-	primaryPodName, err := func() (string, error) {
-		switch {
-		// if the workload is newly created, and the role label is not set, we set the pod with index=0 as the primary by default.
-		case len(emptyRolePods) == len(podList):
-			return podList[0].Name, nil
-		case len(primaryPods) != 1:
-			return "", fmt.Errorf("the number of primary pod is not equal to 1, primary pods: %v, emptyRole pods: %v", primaryPods, emptyRolePods)
-		default:
-			return primaryPods[0], nil
-		}
-	}()
-	if err != nil {
-		return err
-	}
-	graphCli := model.NewGraphClient(r.cli)
-	for _, pod := range podList {
-		if pod.Annotations == nil {
-			pod.Annotations = map[string]string{}
-		}
-		pi, ok := pod.Annotations[constant.PrimaryAnnotationKey]
-		if !ok || pi != primaryPodName {
-			origPod := pod.DeepCopy()
-			pod.Annotations[constant.PrimaryAnnotationKey] = primaryPodName
-			graphCli.Do(r.dag, origPod, pod, model.ActionUpdatePtr(), nil, inDataContext4G())
 		}
 	}
 	return nil

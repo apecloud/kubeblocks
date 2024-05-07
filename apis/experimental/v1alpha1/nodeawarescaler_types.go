715c58bac8ba07caba2dc21e1b0fd83cf0ced3e9
free6om <free6om.c@gmail.com>
2024-05-07 15:17:25 +0800

    fix printColumn

--- apis/experimental/v1alpha1/nodeawarescaler_types.go
+++ apis/experimental/v1alpha1/nodeawarescaler_types.go
@@ -100,9 +100,9 @@ const (
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:categories={kubeblocks,all},shortName=nas
 // +kubebuilder:printcolumn:name="TARGET-CLUSTER-NAME",type="string",JSONPath=".spec.targetClusterName",description="target cluster name."
-// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type==ScaleReady)].status",description="scale ready."
-// +kubebuilder:printcolumn:name="REASON",type="string",JSONPath=".status.conditions[?(@.type==ScaleReady)].reason",description="reason."
-// +kubebuilder:printcolumn:name="MESSAGE",type="string",JSONPath=".status.conditions[?(@.type==ScaleReady)].message",description="message."
+// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type==\"ScaleReady\")].status",description="scale ready."
+// +kubebuilder:printcolumn:name="REASON",type="string",JSONPath=".status.conditions[?(@.type==\"ScaleReady\")].reason",description="reason."
+// +kubebuilder:printcolumn:name="MESSAGE",type="string",JSONPath=".status.conditions[?(@.type==\"ScaleReady\")].message",description="message."
 // +kubebuilder:printcolumn:name="LAST-SCALE-TIME",type="date",JSONPath=".status.lastScaleTime"
 
 // NodeAwareScaler is the Schema for the nodeawarescalers API

0a604128e884d9480b80014198d9f5866e7eaabf
xuriwuyun <xuriwuyun@gmail.com>
2024-05-13 17:17:16 +0800

    update

--- pkg/lorry/plugin/sanitizer.go
+++ pkg/lorry/plugin/sanitizer.go
@@ -1,21 +1,22 @@
 /*
-Copyright 2018 The Kubernetes Authors.
+Copyright (C) 2022-2024 ApeCloud Co., Ltd
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+This file is part of KubeBlocks project
 
-    http://www.apache.org/licenses/LICENSE-2.0
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Package protosanitizer supports logging of gRPC messages without
-// accidentally revealing sensitive fields.
 package plugin
 
 import (
@@ -116,9 +117,7 @@ func (s *stripSecrets) strip(parsed interface{}, msg interface{}) {
 			// to CapacityRange. We need a pointer to such
 			// a value for recursive stripping.
 			typeName := field.GetTypeName()
-			if strings.HasPrefix(typeName, ".") {
-				typeName = typeName[1:]
-			}
+			typeName = strings.TrimPrefix(typeName, ".")
 			t := proto.MessageType(typeName)
 			if t == nil || t.Kind() != reflect.Ptr {
 				// Shouldn't happen, but

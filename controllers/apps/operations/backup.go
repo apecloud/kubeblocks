811eff6e1a57c6fa03209e05cc73e393b4e40312
wangyelei <yelei.wang@apecloud.com>
2024-05-28 13:33:44 +0800

    fix: create backup failed when cluster phase is Updating

--- controllers/apps/operations/backup.go
+++ controllers/apps/operations/backup.go
@@ -44,8 +44,9 @@ var _ OpsHandler = BackupOpsHandler{}
 func init() {
 	// ToClusterPhase is not defined, because 'backup' does not affect the cluster phase.
 	backupBehaviour := OpsBehaviour{
-		FromClusterPhases: appsv1alpha1.GetClusterUpRunningPhases(),
-		OpsHandler:        BackupOpsHandler{},
+		FromClusterPhases: []appsv1alpha1.ClusterPhase{appsv1alpha1.RunningClusterPhase,
+			appsv1alpha1.UpdatingClusterPhase, appsv1alpha1.AbnormalClusterPhase},
+		OpsHandler: BackupOpsHandler{},
 	}
 
 	opsMgr := GetOpsManager()

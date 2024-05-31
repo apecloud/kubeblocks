142d37e0934e83ab8bb82d64cce48d2a6ff0b1ec
wangyelei <yelei.wang@apecloud.com>
2024-05-31 10:34:55 +0800

    chore: existing multi continuous backup when the backupSchedule has been re-created (#7461)

--- pkg/dataprotection/backup/scheduler_test.go
+++ pkg/dataprotection/backup/scheduler_test.go
@@ -23,11 +23,15 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	appsv1alpha1 "github.com/apecloud/kubeblocks/apis/apps/v1alpha1"
 	dpv1alpha1 "github.com/apecloud/kubeblocks/apis/dataprotection/v1alpha1"
 	"github.com/apecloud/kubeblocks/pkg/constant"
 	ctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
+	dptypes "github.com/apecloud/kubeblocks/pkg/dataprotection/types"
 	"github.com/apecloud/kubeblocks/pkg/generics"
 	testapps "github.com/apecloud/kubeblocks/pkg/testutil/apps"
 	testdp "github.com/apecloud/kubeblocks/pkg/testutil/dataprotection"
@@ -55,6 +59,11 @@ var _ = Describe("Scheduler Test", func() {
 		// delete rest mocked objects
 		inNS := client.InNamespace(testCtx.DefaultNamespace)
 		ml := client.HasLabels{testCtx.TestObjLabelKey}
+		testapps.ClearResources(&testCtx, generics.ClusterSignature, inNS, ml)
+		testapps.ClearResources(&testCtx, generics.PodSignature, inNS, ml)
+		testapps.ClearResources(&testCtx, generics.StorageClassSignature, ml)
+		testapps.ClearResourcesWithRemoveFinalizerOption(&testCtx, generics.PersistentVolumeSignature, true, ml)
+		testapps.ClearResourcesWithRemoveFinalizerOption(&testCtx, generics.PersistentVolumeClaimSignature, true, ml, inNS)
 		testapps.ClearResourcesWithRemoveFinalizerOption(&testCtx, generics.BackupPolicySignature, true, inNS)
 		testapps.ClearResourcesWithRemoveFinalizerOption(&testCtx, generics.BackupScheduleSignature, true, inNS)
 		testapps.ClearResourcesWithRemoveFinalizerOption(&testCtx, generics.ActionSetSignature, true, ml)
@@ -72,15 +81,17 @@ var _ = Describe("Scheduler Test", func() {
 		var (
 			backupPolicy   *dpv1alpha1.BackupPolicy
 			backupSchedule *dpv1alpha1.BackupSchedule
-
-			scheduler *Scheduler
+			actionSet      *dpv1alpha1.ActionSet
+			scheduler      *Scheduler
+			cluster        *appsv1alpha1.Cluster
 		)
 
 		BeforeEach(func() {
 			By("creating an actionSet")
-			actionSet := testapps.CreateCustomizedObj(&testCtx, "backup/actionset.yaml",
+			actionSet = testapps.CreateCustomizedObj(&testCtx, "backup/actionset.yaml",
 				&dpv1alpha1.ActionSet{}, testapps.WithName(testdp.ActionSetName))
-
+			clusterInfo := testdp.NewFakeCluster(&testCtx)
+			cluster = clusterInfo.Cluster
 			backupPolicy = testdp.NewBackupPolicyFactory(testCtx.DefaultNamespace, testdp.BackupPolicyName).
 				SetBackupRepoName(testdp.BackupRepoName).
 				SetPathPrefix(testdp.BackupPathPrefix).
@@ -90,7 +101,16 @@ var _ = Describe("Scheduler Test", func() {
 				AddBackupMethod(testdp.BackupMethodName, false, actionSet.Name).
 				AddBackupMethod(testdp.VSBackupMethodName, true, "").
 				Create(&testCtx).GetObject()
-			backupSchedule = testdp.NewFakeBackupSchedule(&testCtx, nil)
+			backupSchedule = testdp.NewFakeBackupSchedule(&testCtx, func(schedule *dpv1alpha1.BackupSchedule) {
+				schedule.OwnerReferences = []v1.OwnerReference{
+					{
+						APIVersion: appsv1alpha1.APIVersion,
+						Kind:       "Cluster",
+						Name:       cluster.Name,
+						UID:        cluster.UID,
+					},
+				}
+			})
 
 			scheduler = buildScheduler()
 		})
@@ -109,6 +129,58 @@ var _ = Describe("Scheduler Test", func() {
 					scheduler.BackupPolicy.Spec.BackupMethods[i].Name = "not-exist"
 				}
 				Expect(scheduler.Schedule()).ShouldNot(Succeed())
+			})
+
+			It("test schedule for continuous backup", func() {
+				By("set actionSet type to Continuous")
+				Expect(testapps.ChangeObj(&testCtx, actionSet, func(as *dpv1alpha1.ActionSet) {
+					actionSet.Spec.BackupType = dpv1alpha1.BackupTypeContinuous
+				})).Should(Succeed())
+
+				By("enable backupMethod and do scheduling")
+				Expect(testapps.ChangeObj(&testCtx, backupSchedule, func(schedule *dpv1alpha1.BackupSchedule) {
+					for i, v := range backupSchedule.Spec.Schedules {
+						if v.BackupMethod == testdp.BackupMethodName {
+							backupSchedule.Spec.Schedules[i].Enabled = pointer.Bool(true)
+							break
+						}
+					}
+				})).Should(Succeed())
+
+				scheduler.BackupPolicy = backupPolicy
+				scheduler.BackupSchedule = backupSchedule
+				Expect(scheduler.Schedule()).Should(Succeed())
+
+				By("check the continuous backup created")
+				backupName := GenerateCRNameByBackupSchedule(backupSchedule, testdp.BackupMethodName)
+				Eventually(testapps.CheckObjExists(&testCtx, client.ObjectKey{Name: backupName, Namespace: testCtx.DefaultNamespace},
+					&dpv1alpha1.Backup{}, true)).Should(Succeed())
+
+				By("re-create backupSchedule and enable the method")
+				Expect(k8sClient.Delete(ctx, backupSchedule)).Should(Succeed())
+				backupSchedule = testdp.NewFakeBackupSchedule(&testCtx, func(schedule *dpv1alpha1.BackupSchedule) {
+					schedule.OwnerReferences = []v1.OwnerReference{
+						{
+							APIVersion: appsv1alpha1.APIVersion,
+							Kind:       "Cluster",
+							Name:       cluster.Name,
+							UID:        cluster.UID,
+						},
+					}
+					for i, v := range backupSchedule.Spec.Schedules {
+						if v.BackupMethod == testdp.BackupMethodName {
+							backupSchedule.Spec.Schedules[i].Enabled = pointer.Bool(true)
+							break
+						}
+					}
+				})
+				By("Expect only one continuous backup to exist")
+				scheduler.BackupSchedule = backupSchedule
+				Expect(scheduler.Schedule()).Should(Succeed())
+				Eventually(testapps.List(&testCtx, generics.BackupSignature, client.MatchingLabels{
+					dptypes.BackupTypeLabelKey:     string(dpv1alpha1.BackupTypeContinuous),
+					dptypes.BackupScheduleLabelKey: backupSchedule.Name,
+				}, client.InNamespace(testCtx.DefaultNamespace))).Should(HaveLen(1))
 			})
 		})
 	})

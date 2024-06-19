86566da63be4c57b7121683b66c970f0d35483e1
xuriwuyun <xuriwuyun@gmail.com>
2024-06-19 14:29:58 +0800

    update

--- pkg/kb_agent/util/k8s_client.go
+++ pkg/kb_agent/util/k8s_client.go
@@ -17,16 +17,12 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package kubernetes
+package util
 
 import (
 	"github.com/pkg/errors"
 	"k8s.io/client-go/kubernetes"
-	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	ctlruntime "sigs.k8s.io/controller-runtime"
-
-	appsv1alpha1 "github.com/apecloud/kubeblocks/apis/apps/v1alpha1"
 )
 
 // GetClientSet returns a kubernetes clientSet.
@@ -41,22 +37,4 @@ func GetClientSet() (*kubernetes.Clientset, error) {
 	}
 
 	return clientSet, nil
-}
-
-// GetRESTClientForKB returns a kubernetes restClient for KubeBlocks types.
-func GetRESTClientForKB() (*rest.RESTClient, error) {
-	restConfig, err := ctlruntime.GetConfig()
-	if err != nil {
-		return nil, errors.Wrap(err, "get kubeConfig failed")
-	}
-	_ = appsv1alpha1.AddToScheme(clientsetscheme.Scheme)
-	restConfig.GroupVersion = &appsv1alpha1.GroupVersion
-	restConfig.APIPath = "/apis"
-	restConfig.NegotiatedSerializer = clientsetscheme.Codecs.WithoutConversion()
-	client, err := rest.RESTClientFor(restConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
 }

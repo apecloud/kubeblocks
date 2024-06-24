3f5de0350a665f8ca69a99db9dde19fd8c31da2e
huangzhangshu <huangzhangshu@apecloud.com>
2024-06-24 12:57:52 +0800

    chore: fix ut

--- pkg/controller/instanceset/revision_util_test.go
+++ pkg/controller/instanceset/revision_util_test.go
@@ -962,7 +962,7 @@ var _ = Describe("revision util test", func() {
 			Expect(err).Should(Succeed())
 			cr, err := NewRevision(its)
 			Expect(err).Should(Succeed())
-			Expect(cr.Name).Should(Equal("redis-test-redis-694cf8dbf8"))
+			Expect(cr.Name).Should(Equal("redis-test-redis-59996f5569"))
 		})
 	})
 

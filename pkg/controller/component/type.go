7e409262b7bf7e1738d6e63a498acf7410b64aa0
zhangtao <111836083+sophon-zt@users.noreply.github.com>
2024-07-17 20:37:46 +0800

    fix: mysql/wesql has no slow log (#7824)

--- pkg/controller/component/type.go
+++ pkg/controller/component/type.go
@@ -80,4 +80,5 @@ type SynthesizedComponent struct {
 	CharacterType         string                          `json:"characterType,omitempty"`
 	HorizontalScalePolicy *v1alpha1.HorizontalScalePolicy `json:"horizontalScalePolicy,omitempty"`
 	VolumeTypes           []v1alpha1.VolumeTypeSpec       `json:"volumeTypes,omitempty"` // The VolumeTypes will be replaced with Volumes in the future.
+	EnabledLogs           []string                        `json:"enabledLogs,omitempty"`
 }

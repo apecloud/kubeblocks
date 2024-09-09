136c5989782fed83bfbb208c3026085501674dc5
Harold Cheng <niuchangcun@gmail.com>
2024-09-09 12:55:57 +0800

    fix

--- pkg/controller/scheduling/scheduling_utils.go
+++ pkg/controller/scheduling/scheduling_utils.go
@@ -49,7 +49,7 @@ func buildSchedulingPolicy(cluster *appsv1alpha1.Cluster, compSpec *appsv1alpha1
 	// make a copy, or it will affect cluster's spec
 	schedulingPolicy := cluster.Spec.SchedulingPolicy.DeepCopy()
 	if compSpec != nil && compSpec.SchedulingPolicy != nil {
-		schedulingPolicy = compSpec.SchedulingPolicy
+		schedulingPolicy = compSpec.SchedulingPolicy.DeepCopy()
 	}
 
 	mergeGlobalAffinity := func() error {

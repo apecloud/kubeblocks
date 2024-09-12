3e98dadafed6a6b0a38231dad6ea0b6625bbb5a5
Harold Cheng <niuchangcun@gmail.com>
2024-09-12 11:17:49 +0800

    revert skipping kb-agent

--- controllers/apps/transformer_component_workload.go
+++ controllers/apps/transformer_component_workload.go
@@ -406,6 +406,10 @@ func checkNRollbackProtoImages(itsObj, itsProto *workloads.InstanceSet) {
 	for i, cc := range [][]corev1.Container{itsObj.Spec.Template.Spec.InitContainers, itsObj.Spec.Template.Spec.Containers} {
 		images[i] = make(map[string]string)
 		for _, c := range cc {
+			// skip the kb-agent container
+			if component.IsKBAgentContainer(&c) {
+				continue
+			}
 			images[i][c.Name] = c.Image
 		}
 	}

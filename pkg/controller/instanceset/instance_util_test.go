e8c5cb55661bf4bca6cf5c86f6a9840056dd7c68
cjc7373 <niuchangcun@gmail.com>
2024-09-18 17:50:41 +0800

    feat: specify node for rebuild instance ops (#8151)

--- pkg/controller/instanceset/instance_util_test.go
+++ pkg/controller/instanceset/instance_util_test.go
@@ -213,6 +213,34 @@ var _ = Describe("instance util test", func() {
 			Expect(instance.pvcs[0].Labels[constant.VolumeClaimTemplateNameLabelKey]).Should(Equal(volumeClaimTemplates[0].Name))
 			Expect(instance.pvcs[0].Spec.Resources).Should(Equal(volumeClaimTemplates[0].Spec.Resources))
 		})
+
+		It("adds nodeSelector according to annotation", func() {
+			itsExt, err := buildInstanceSetExt(its, nil)
+			Expect(err).Should(BeNil())
+			nameTemplate, err := buildInstanceName2TemplateMap(itsExt)
+			Expect(err).Should(BeNil())
+			name := name + "-0"
+			Expect(nameTemplate).Should(HaveKey(name))
+			template := nameTemplate[name]
+
+			node := "test-node-1"
+			Expect(MergeNodeSelectorOnceAnnotation(its, map[string]string{name: node})).To(Succeed())
+			instance, err := buildInstanceByTemplate(name, template, its, "")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(instance.pod.Spec.NodeSelector[corev1.LabelHostname]).To(Equal(node))
+
+			By("test with an already existing annotation")
+			delete(its.Annotations, constant.NodeSelectorOnceAnnotationKey)
+			Expect(MergeNodeSelectorOnceAnnotation(its, map[string]string{"other-pod": "other-node"})).To(Succeed())
+			Expect(MergeNodeSelectorOnceAnnotation(its, map[string]string{name: node})).To(Succeed())
+			mapping, err := ParseNodeSelectorOnceAnnotation(its)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(mapping).To(HaveKeyWithValue("other-pod", "other-node"))
+			Expect(mapping).To(HaveKeyWithValue(name, node))
+			instance, err = buildInstanceByTemplate(name, template, its, "")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(instance.pod.Spec.NodeSelector[corev1.LabelHostname]).To(Equal(node))
+		})
 	})
 
 	Context("buildInstancePVCByTemplate", func() {

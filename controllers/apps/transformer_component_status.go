96d3fddda9a82f2f952f6411422f63f64eb3d06c
Leon <leon@apecloud.com>
2024-09-26 15:03:16 +0800

    fix lint

--- controllers/apps/transformer_component_status.go
+++ controllers/apps/transformer_component_status.go
@@ -21,12 +21,12 @@ package apps
 
 import (
 	"fmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"strconv"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"

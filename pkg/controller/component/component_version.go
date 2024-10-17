9fddd903f0988b73d62d184461c54a552f9acebc
Y-Rookie <yong.huang@apecloud.com>
2024-10-17 16:47:43 +0800

    fix comment

--- pkg/controller/component/component_version.go
+++ pkg/controller/component/component_version.go
@@ -275,7 +275,7 @@ func checkNMergeImages(serviceVersion string, appsInDef, appsInVer map[string]ap
 		if len(ver.name) == 0 {
 			match, err := CompareServiceVersion(serviceVersion, def.version)
 			if err != nil {
-				def.err = fmt.Errorf("failed to compare service version: %v", err)
+				def.err = fmt.Errorf("failed to compare service version (service version: %s, def version: %s): %w", serviceVersion, def.version, err)
 			}
 			if !match && def.required {
 				def.err = fmt.Errorf("no matched image found for container %s with required version %s", name, serviceVersion)

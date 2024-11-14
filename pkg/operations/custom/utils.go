e664af96e71123070a56fda6b6d5c68a893a471e
wangyelei <yelei.wang@apecloud.com>
2024-11-14 17:06:56 +0800

    add test case

--- pkg/operations/custom/utils.go
+++ pkg/operations/custom/utils.go
@@ -410,10 +410,10 @@ func getNameFromObjectKey(objectKey string) string {
 }
 
 func getTolerations(cluster *appsv1.Cluster, compSpec *appsv1.ClusterComponentSpec) []corev1.Toleration {
-	if compSpec.SchedulingPolicy != nil {
+	if compSpec != nil && compSpec.SchedulingPolicy != nil {
 		return compSpec.SchedulingPolicy.Tolerations
 	}
-	if cluster.Spec.SchedulingPolicy != nil {
+	if cluster != nil && cluster.Spec.SchedulingPolicy != nil {
 		return cluster.Spec.SchedulingPolicy.Tolerations
 	}
 	return nil

a3037c52b41acce27530d9bcd8fe54d9f5c2f3a9
wangyelei <yelei.wang@apecloud.com>
2024-11-14 17:57:53 +0800

    chore: the pod of the custom ops is always pending (#8455)
    (cherry picked from commit bc7dd2f19d86aa762957d517eaadd84ca286923c)

--- pkg/operations/custom/utils.go
+++ pkg/operations/custom/utils.go
@@ -36,6 +36,7 @@ import (
 	"github.com/apecloud/kubeblocks/pkg/common"
 	"github.com/apecloud/kubeblocks/pkg/constant"
 	"github.com/apecloud/kubeblocks/pkg/controller/component"
+	"github.com/apecloud/kubeblocks/pkg/controller/scheduling"
 	intctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
 )
 
@@ -407,4 +408,15 @@ func getNameFromObjectKey(objectKey string) string {
 		return strs[1]
 	}
 	return objectKey
+}
+
+func getTolerations(cluster *appsv1.Cluster, compSpec *appsv1.ClusterComponentSpec) ([]corev1.Toleration, error) {
+	schedulePolicy, err := scheduling.BuildSchedulingPolicy(cluster, compSpec)
+	if err != nil {
+		return nil, err
+	}
+	if schedulePolicy == nil {
+		return nil, nil
+	}
+	return schedulePolicy.Tolerations, nil
 }

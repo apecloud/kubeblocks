a494c79c0ea4235f3dc8bf6344afebfdef50fc45
Leon <leon@apecloud.com>
2024-11-27 10:50:49 +0800

    fix: start the workload before scaling out (#8531)
    (cherry picked from commit 08cc8082fee9f9a3860f14aa51b061e5f599f68f)

--- controllers/apps/transformer_component_workload.go
+++ controllers/apps/transformer_component_workload.go
@@ -21,6 +21,7 @@ package apps
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
@@ -49,6 +50,11 @@ import (
 	"github.com/apecloud/kubeblocks/pkg/controller/graph"
 	"github.com/apecloud/kubeblocks/pkg/controller/model"
 	intctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
+)
+
+const (
+	// TODO: use replicas status
+	stopReplicasSnapshotKey = "apps.kubeblocks.io/stop-replicas-snapshot"
 )
 
 // componentWorkloadTransformer handles component workload generation
@@ -159,11 +165,6 @@ func (t *componentWorkloadTransformer) reconcileWorkload(ctx context.Context, cl
 	// build configuration template annotations to workload
 	configuration.BuildConfigTemplateAnnotations(protoITS, synthesizedComp)
 
-	// mark proto its as stopped if the component is stopped
-	if isCompStopped(synthesizedComp) {
-		t.stopWorkload(protoITS)
-	}
-
 	return nil
 }
 
@@ -220,24 +221,16 @@ func hasMemberJoinNDataActionDefined(lifecycleActions *appsv1.ComponentLifecycle
 		hasActionDefined([]*appsv1.Action{lifecycleActions.DataDump, lifecycleActions.DataLoad})
 }
 
-func isCompStopped(synthesizedComp *component.SynthesizedComponent) bool {
-	return synthesizedComp.Stop != nil && *synthesizedComp.Stop
-}
-
-func (t *componentWorkloadTransformer) stopWorkload(protoITS *workloads.InstanceSet) {
-	zero := func() *int32 { r := int32(0); return &r }()
-	// since its doesn't support stop, we achieve it by setting replicas to 0.
-	protoITS.Spec.Replicas = zero
-	for i := range protoITS.Spec.Instances {
-		protoITS.Spec.Instances[i].Replicas = zero
-	}
-}
-
 func (t *componentWorkloadTransformer) handleUpdate(reqCtx intctrlutil.RequestCtx, cli model.GraphClient, dag *graph.DAG,
-	cluster *appsv1.Cluster, synthesizeComp *component.SynthesizedComponent, comp *appsv1.Component, runningITS, protoITS *workloads.InstanceSet) error {
-	if !isCompStopped(synthesizeComp) {
+	cluster *appsv1.Cluster, synthesizedComp *component.SynthesizedComponent, comp *appsv1.Component, runningITS, protoITS *workloads.InstanceSet) error {
+	start, stop, err := t.handleWorkloadStartNStop(synthesizedComp, runningITS, &protoITS)
+	if err != nil {
+		return err
+	}
+
+	if !(start || stop) {
 		// postpone the update of the workload until the component is back to running.
-		if err := t.handleWorkloadUpdate(reqCtx, dag, cluster, synthesizeComp, comp, runningITS, protoITS); err != nil {
+		if err := t.handleWorkloadUpdate(reqCtx, dag, cluster, synthesizedComp, comp, runningITS, protoITS); err != nil {
 			return err
 		}
 	}
@@ -248,11 +241,107 @@ func (t *componentWorkloadTransformer) handleUpdate(reqCtx intctrlutil.RequestCt
 		// make sure the workload is updated after the env CM
 		cli.DependOn(dag, &corev1.ConfigMap{
 			ObjectMeta: metav1.ObjectMeta{
-				Namespace: synthesizeComp.Namespace,
-				Name:      constant.GenerateClusterComponentEnvPattern(synthesizeComp.ClusterName, synthesizeComp.Name),
+				Namespace: synthesizedComp.Namespace,
+				Name:      constant.GenerateClusterComponentEnvPattern(synthesizedComp.ClusterName, synthesizedComp.Name),
 			},
 		})
 	}
+
+	// if start {
+	//	return intctrlutil.NewDelayedRequeueError(time.Second, "workload is starting")
+	// }
+	return nil
+}
+
+func (t *componentWorkloadTransformer) handleWorkloadStartNStop(
+	synthesizedComp *component.SynthesizedComponent, runningITS *workloads.InstanceSet, protoITS **workloads.InstanceSet) (bool, bool, error) {
+	var (
+		stop  = isCompStopped(synthesizedComp)
+		start = !stop && isWorkloadStopped(runningITS)
+	)
+	if start || stop {
+		*protoITS = runningITS.DeepCopy() // don't modify the runningITS except for the replicas
+	}
+	if stop {
+		return start, stop, t.stopWorkload(synthesizedComp, runningITS, *protoITS)
+	}
+	if start {
+		return start, stop, t.startWorkload(synthesizedComp, runningITS, *protoITS)
+	}
+	return start, stop, nil
+}
+
+func isCompStopped(synthesizedComp *component.SynthesizedComponent) bool {
+	return synthesizedComp.Stop != nil && *synthesizedComp.Stop
+}
+
+func isWorkloadStopped(runningITS *workloads.InstanceSet) bool {
+	_, ok := runningITS.Annotations[stopReplicasSnapshotKey]
+	return ok
+}
+
+func (t *componentWorkloadTransformer) stopWorkload(
+	synthesizedComp *component.SynthesizedComponent, runningITS, protoITS *workloads.InstanceSet) error {
+	// since its doesn't support stop, we achieve it by setting replicas to 0.
+	protoITS.Spec.Replicas = ptr.To(int32(0))
+	for i := range protoITS.Spec.Instances {
+		protoITS.Spec.Instances[i].Replicas = ptr.To(int32(0))
+	}
+
+	// backup the replicas of runningITS
+	snapshot, ok := runningITS.Annotations[stopReplicasSnapshotKey]
+	if !ok {
+		replicas := map[string]int32{}
+		if runningITS.Spec.Replicas != nil {
+			replicas[""] = *runningITS.Spec.Replicas
+		}
+		for i := range runningITS.Spec.Instances {
+			if runningITS.Spec.Instances[i].Replicas != nil {
+				replicas[protoITS.Spec.Instances[i].Name] = *runningITS.Spec.Instances[i].Replicas
+			}
+		}
+		out, err := json.Marshal(replicas)
+		if err != nil {
+			return err
+		}
+		snapshot = string(out)
+
+		protoITS.Annotations[constant.KubeBlocksGenerationKey] = synthesizedComp.Generation
+	}
+	protoITS.Annotations[stopReplicasSnapshotKey] = snapshot
+	return nil
+}
+
+func (t *componentWorkloadTransformer) startWorkload(
+	synthesizedComp *component.SynthesizedComponent, runningITS, protoITS *workloads.InstanceSet) error {
+	snapshot := runningITS.Annotations[stopReplicasSnapshotKey]
+	replicas := map[string]int32{}
+	if err := json.Unmarshal([]byte(snapshot), &replicas); err != nil {
+		return err
+	}
+
+	restore := func(p **int32, key string) {
+		val, ok := replicas[key]
+		if ok {
+			*p = ptr.To(val)
+		} else {
+			*p = nil
+		}
+	}
+
+	// restore the replicas of runningITS
+	restore(&protoITS.Spec.Replicas, "")
+	for i := range runningITS.Spec.Instances {
+		for j := range protoITS.Spec.Instances {
+			if runningITS.Spec.Instances[i].Name == protoITS.Spec.Instances[j].Name {
+				restore(&protoITS.Spec.Instances[j].Replicas, runningITS.Spec.Instances[i].Name)
+				break
+			}
+		}
+	}
+
+	delete(protoITS.Annotations, stopReplicasSnapshotKey)
+	delete(runningITS.Annotations, stopReplicasSnapshotKey)
 
 	return nil
 }
@@ -455,27 +544,6 @@ func checkNRollbackProtoImages(itsObj, itsProto *workloads.InstanceSet) {
 	}
 }
 
-func (r *componentWorkloadOps) expandVolumeClaimTemplates(runningVCTs []corev1.PersistentVolumeClaim, protoVCTs []corev1.PersistentVolumeClaimTemplate, insTPLName string) error {
-	for _, vct := range runningVCTs {
-		var proto *corev1.PersistentVolumeClaimTemplate
-		for i, v := range protoVCTs {
-			if v.Name == vct.Name {
-				proto = &protoVCTs[i]
-				break
-			}
-		}
-		// REVIEW: seems we can remove a volume claim from templates at runtime, without any changes and warning messages?
-		if proto == nil {
-			continue
-		}
-
-		if err := r.expandVolumes(insTPLName, vct.Name, proto); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // expandVolume handles workload expand volume
 func (r *componentWorkloadOps) expandVolume() error {
 	// 1. expand the volumes without instance template name.
@@ -511,6 +579,27 @@ func (r *componentWorkloadOps) expandVolume() error {
 			}
 		}
 		if err := r.expandVolumeClaimTemplates(runningInsSpec.VolumeClaimTemplates, protoVCTs, runningInsTPL.Name); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *componentWorkloadOps) expandVolumeClaimTemplates(runningVCTs []corev1.PersistentVolumeClaim, protoVCTs []corev1.PersistentVolumeClaimTemplate, insTPLName string) error {
+	for _, vct := range runningVCTs {
+		var proto *corev1.PersistentVolumeClaimTemplate
+		for i, v := range protoVCTs {
+			if v.Name == vct.Name {
+				proto = &protoVCTs[i]
+				break
+			}
+		}
+		// REVIEW: seems we can remove a volume claim from templates at runtime, without any changes and warning messages?
+		if proto == nil {
+			continue
+		}
+
+		if err := r.expandVolumes(insTPLName, vct.Name, proto); err != nil {
 			return err
 		}
 	}

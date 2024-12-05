6e20cf66ca560a818d7e38e1ca77c3ddcab3db65
sophon <sophon@apecloud.com>
2024-12-05 16:58:34 +0800

    chore: removed deprecated rerender-related api and tools

--- apis/apps/v1/componentdefinition_types.go
+++ apis/apps/v1/componentdefinition_types.go
@@ -1074,21 +1074,6 @@ type ComponentConfigSpec struct {
 	// +optional
 	Keys []string `json:"keys,omitempty"`
 
-	// Specifies the secondary rendered config spec for pod-specific customization.
-	//
-	// The template is rendered inside the pod (by the "config-manager" sidecar container) and merged with the main
-	// template's render result to generate the final configuration file.
-	//
-	// This field is intended to handle scenarios where different pods within the same Component have
-	// varying configurations. It allows for pod-specific customization of the configuration.
-	//
-	// Note: This field will be deprecated in future versions, and the functionality will be moved to
-	// `cluster.spec.componentSpecs[*].instances[*]`.
-	//
-	// +kubebuilder:deprecatedversion:warning="This field has been deprecated since 0.9.0 and will be removed in 0.10.0"
-	// +optional
-	LegacyRenderedConfigSpec *LegacyRenderedTemplateSpec `json:"legacyRenderedConfigSpec,omitempty"`
-
 	// Specifies the name of the referenced configuration constraints object.
 	//
 	// +kubebuilder:validation:MaxLength=63
@@ -1144,13 +1129,6 @@ type ComponentConfigSpec struct {
 	//
 	// +optional
 	AsSecret *bool `json:"asSecret,omitempty"`
-}
-
-// LegacyRenderedTemplateSpec describes the configuration extension for the lazy rendered template.
-// Deprecated: LegacyRenderedTemplateSpec has been deprecated since 0.9.0 and will be removed in 0.10.0
-type LegacyRenderedTemplateSpec struct {
-	// Extends the configuration template.
-	ConfigTemplateExtension `json:",inline"`
 }
 
 type ConfigTemplateExtension struct {

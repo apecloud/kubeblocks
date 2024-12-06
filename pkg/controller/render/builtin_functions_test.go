8e7f8a7633082693b7aa5e42092b5b80ebcbaf1f
sophon <sophon@apecloud.com>
2024-12-06 11:33:01 +0800

    chore: fmt imports

--- pkg/controller/render/builtin_functions_test.go
+++ pkg/controller/render/builtin_functions_test.go
@@ -25,8 +25,8 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/spf13/viper"
 
+	"github.com/spf13/viper"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
--- pkg/controller/render/template_render_test.go
+++ pkg/controller/render/template_render_test.go
@@ -24,9 +24,9 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/types"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	appsv1 "github.com/apecloud/kubeblocks/apis/apps/v1"

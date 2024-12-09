3d128564766d2f8d5f2918a30c6bc416341bed5c
xuriwuyun <xuriwuyun@gmail.com>
2024-12-09 15:13:44 +0800

    fix: Check if the SQL is valid when creating a MySQL account.

--- pkg/lorry/engines/mysql/user.go
+++ pkg/lorry/engines/mysql/user.go
@@ -114,7 +114,11 @@ func (mgr *Manager) DescribeUser(ctx context.Context, userName string) (*models.
 	return user, nil
 }
 
-func (mgr *Manager) CreateUser(ctx context.Context, userName, password, _ string) error {
+func (mgr *Manager) CreateUser(ctx context.Context, userName, password, statement string) error {
+	if !strings.HasPrefix(statement, "CREATE ") {
+		return fmt.Errorf("the user %s is created with invalid sql %s, wait and try again", userName, statement)
+	}
+
 	sql := fmt.Sprintf(createUserSQL, userName, password)
 
 	_, err := mgr.Exec(ctx, sql)

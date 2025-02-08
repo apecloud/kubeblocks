fa1449e893c0ac8eaa6a61c13c29510fa87ec413
mengyipeng <413183498@qq.com>
2025-02-08 09:37:23 +0800

    consider *replicaChanger.ReplicaChanges is illegal

--- apis/operations/v1alpha1/opsrequest_validation.go
+++ apis/operations/v1alpha1/opsrequest_validation.go
@@ -470,13 +470,19 @@ func (r *OpsRequest) validateHorizontalScalingSpec(hScale HorizontalScaling, com
 		if replicaChanger.ReplicaChanges != nil && allReplicaChanges > *replicaChanger.ReplicaChanges {
 			return fmt.Errorf(`%s "replicaChanges" can't be less than the sum of "replicaChanges" for specified instance templates`, msgPrefix)
 		}
-		if isScaleIn && int(compSpec.Replicas)-len(offlineOrOnlineInsNames) < minReplicasOrShards {
+
+		var replicaChange int32
+		if replicaChanger.ReplicaChanges != nil {
+			replicaChange = *replicaChanger.ReplicaChanges
+		} else {
+			replicaChange = allReplicaChanges
+		}
+		if isScaleIn && int(compSpec.Replicas)-int(replicaChange) < minReplicasOrShards {
 			return fmt.Errorf(`the number of replicas after scaling down violates the replica limit for component "%s"`, hScale.ComponentName)
 		}
-		if !isScaleIn && int(compSpec.Replicas)+len(offlineOrOnlineInsNames) > maxReplicasOrShards {
+		if !isScaleIn && int(compSpec.Replicas)+int(replicaChange) > maxReplicasOrShards {
 			return fmt.Errorf(`the number of replicas after scaling up violates the replica limit for component "%s"`, hScale.ComponentName)
 		}
-
 		return nil
 	}
 	if scaleIn != nil {

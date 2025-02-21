34aa92bf69136e64164c547716b988329d509130
gnolong <2391353625@qq.com>
2025-02-21 16:59:04 +0800

    ci

--- pkg/testutil/apps/cluster_util.go
+++ pkg/testutil/apps/cluster_util.go
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package apps
 
 import (
-	"context"
-
 	"github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"

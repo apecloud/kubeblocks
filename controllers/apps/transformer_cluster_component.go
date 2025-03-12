765a62e403c53a7032f5214eca83220b13257f98
Y-Rookie <yong.huang@apecloud.com>
2025-03-12 15:59:14 +0800

    update

--- controllers/apps/transformer_cluster_component.go
+++ controllers/apps/transformer_cluster_component.go
@@ -21,14 +21,14 @@ package apps
 
 import (
 	"fmt"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	"reflect"
 	"slices"
 	"strconv"
 	"strings"
 
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"

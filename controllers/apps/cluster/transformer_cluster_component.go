0671230dec8d85ae3c5f00e3365b2fd86ee1ba47
Y-Rookie <yong.huang@apecloud.com>
2025-03-12 15:34:01 +0800

    fix comment and add ut

--- controllers/apps/cluster/transformer_cluster_component.go
+++ controllers/apps/cluster/transformer_cluster_component.go
@@ -29,8 +29,8 @@ import (
 
 	"golang.org/x/exp/maps"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -186,11 +186,50 @@ func copyAndMergeComponent(oldCompObj, newCompObj *appsv1.Component) *appsv1.Com
 	compObjCopy := oldCompObj.DeepCopy()
 	compProto := newCompObj
 
-	// merge labels and annotations
+	normalizeQuantity := func(name corev1.ResourceName, q resource.Quantity) resource.Quantity {
+		if name == corev1.ResourceCPU {
+			return resource.MustParse(fmt.Sprintf("%dm", q.MilliValue()))
+		} else if name == corev1.ResourceMemory || name == corev1.ResourceStorage || name == corev1.ResourceEphemeralStorage {
+			return resource.MustParse(fmt.Sprintf("%dMi", q.Value()/1024/1024))
+		} else {
+			return q.DeepCopy()
+		}
+	}
+
+	normalizeResourceList := func(resources *corev1.ResourceList) {
+		if resources == nil {
+			return
+		}
+		for k, v := range *resources {
+			(*resources)[k] = normalizeQuantity(k, v)
+		}
+	}
+
+	normalize := func(spec appsv1.ComponentSpec) appsv1.ComponentSpec {
+		normalized := spec.DeepCopy()
+		if normalized.Resources.Limits != nil {
+			normalizeResourceList(&normalized.Resources.Limits)
+		}
+		if normalized.Resources.Requests != nil {
+			normalizeResourceList(&normalized.Resources.Requests)
+		}
+		for i := range normalized.VolumeClaimTemplates {
+			vct := &normalized.VolumeClaimTemplates[i]
+			if vct.Spec.Resources.Limits != nil {
+				normalizeResourceList(&vct.Spec.Resources.Limits)
+			}
+			if vct.Spec.Resources.Requests != nil {
+				normalizeResourceList(&vct.Spec.Resources.Requests)
+			}
+		}
+		return *normalized
+	}
+
+	// Merge metadata
 	ictrlutil.MergeMetadataMapInplace(compProto.Annotations, &compObjCopy.Annotations)
 	ictrlutil.MergeMetadataMapInplace(compProto.Labels, &compObjCopy.Labels)
 
-	// merge spec except resources
+	// Merge all spec fields
 	compObjCopy.Spec.TerminationPolicy = compProto.Spec.TerminationPolicy
 	compObjCopy.Spec.CompDef = compProto.Spec.CompDef
 	compObjCopy.Spec.ServiceVersion = compProto.Spec.ServiceVersion
@@ -217,14 +256,17 @@ func copyAndMergeComponent(oldCompObj, newCompObj *appsv1.Component) *appsv1.Com
 	compObjCopy.Spec.DisableExporter = compProto.Spec.DisableExporter
 	compObjCopy.Spec.Stop = compProto.Spec.Stop
 	compObjCopy.Spec.Sidecars = compProto.Spec.Sidecars
+	compObjCopy.Spec.Resources = compProto.Spec.Resources
 
-	if reflect.DeepEqual(oldCompObj.Annotations, compObjCopy.Annotations) &&
-		reflect.DeepEqual(oldCompObj.Labels, compObjCopy.Labels) &&
-		equality.Semantic.DeepEqual(oldCompObj.Spec.Resources, compProto.Spec.Resources) &&
-		reflect.DeepEqual(oldCompObj.Spec, compObjCopy.Spec) {
+	metadataChanged := !reflect.DeepEqual(oldCompObj.Annotations, compObjCopy.Annotations) ||
+		!reflect.DeepEqual(oldCompObj.Labels, compObjCopy.Labels)
+	specChanged := !reflect.DeepEqual(normalize(oldCompObj.Spec), normalize(compObjCopy.Spec))
+
+	// If nothing changed after normalization, return nil
+	if !metadataChanged && !specChanged {
 		return nil
 	}
-	compObjCopy.Spec.Resources = compProto.Spec.Resources
+
 	return compObjCopy
 }
 

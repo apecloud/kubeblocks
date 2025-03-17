06851be74b0822b979bbf55a19059c3ad77c3654
gnolong <2391353625@qq.com>
2025-03-17 16:43:44 +0800

    test

--- controllers/apps/util/utils_test.go
+++ controllers/apps/util/utils_test.go
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package util
 
 import (
+	"context"
 	"fmt"
 	"reflect"
 	"testing"
@@ -27,11 +28,17 @@ import (
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	kbappsv1 "github.com/apecloud/kubeblocks/apis/apps/v1"
+	dpv1alpha1 "github.com/apecloud/kubeblocks/apis/dataprotection/v1alpha1"
 	workloads "github.com/apecloud/kubeblocks/apis/workloads/v1"
+	"github.com/apecloud/kubeblocks/pkg/constant"
+	intctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
+	viper "github.com/apecloud/kubeblocks/pkg/viperx"
 )
 
 func TestReflect(t *testing.T) {
@@ -78,4 +85,91 @@ func TestIsOwnedByInstanceSet(t *testing.T) {
 		},
 	}
 	assert.False(t, IsOwnedByInstanceSet(its))
+}
+
+func TestGetRestoreSystemAccountPassword(t *testing.T) {
+	encryptor := intctrlutil.NewEncryptor(viper.GetString(constant.CfgKeyDPEncryptionKey))
+	encryptedPwd, _ := encryptor.Encrypt([]byte("test-password"))
+
+	tests := []struct {
+		name        string
+		annotations map[string]string
+		backup      *dpv1alpha1.Backup
+		component   string
+		account     string
+		want        string
+	}{
+		{
+			name: "normal case",
+			annotations: map[string]string{
+				constant.RestoreFromBackupAnnotationKey: `{"comp1":{"name":"backup1","namespace":"default"}}`,
+			},
+			backup: &dpv1alpha1.Backup{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "backup1",
+					Namespace: "default",
+					Annotations: map[string]string{
+						constant.EncryptedSystemAccountsAnnotationKey: fmt.Sprintf(`{"comp1":{"account1":"%s"}}`, string(encryptedPwd)),
+					},
+				},
+			},
+			component: "comp1",
+			account:   "account1",
+			want:      "test-password",
+		},
+		{
+			name: "empty restore annotation",
+			annotations: map[string]string{
+				constant.RestoreFromBackupAnnotationKey: "",
+			},
+			backup:    &dpv1alpha1.Backup{},
+			component: "comp1",
+			account:   "account1",
+			want:      "",
+		},
+		{
+			name: "invalid restore annotation json",
+			annotations: map[string]string{
+				constant.RestoreFromBackupAnnotationKey: "invalid-json",
+			},
+			backup:    &dpv1alpha1.Backup{},
+			component: "comp1",
+			account:   "account1",
+			want:      "",
+		},
+		{
+			name: "component not found in restore annotation",
+			annotations: map[string]string{
+				constant.RestoreFromBackupAnnotationKey: `{"other-comp":{"name":"backup1","namespace":"default"}}`,
+			},
+			backup:    &dpv1alpha1.Backup{},
+			component: "comp1",
+			account:   "account1",
+			want:      "",
+		},
+		{
+			name: "backup not found",
+			annotations: map[string]string{
+				constant.RestoreFromBackupAnnotationKey: `{"comp1":{"name":"non-existent","namespace":"default"}}`,
+			},
+			backup:    nil,
+			component: "comp1",
+			account:   "account1",
+			want:      "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			scheme := runtime.NewScheme()
+			_ = dpv1alpha1.AddToScheme(scheme)
+			cli := fake.NewClientBuilder().WithScheme(scheme).WithObjects()
+			if tt.backup != nil {
+				cli = cli.WithObjects(tt.backup)
+			}
+			got := GetRestoreSystemAccountPassword(context.Background(), cli.Build(),
+				tt.annotations, tt.component, tt.account)
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }

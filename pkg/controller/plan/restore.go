d4ae453b5771bccf4ba928ed8cdb42c241522056
wangyelei <yelei.wang@apecloud.com>
2025-03-18 13:38:00 +0800

    fix bug

--- pkg/controller/plan/restore.go
+++ pkg/controller/plan/restore.go
@@ -313,7 +313,6 @@ func (r *RestoreManager) GetRestoreObjectMeta(comp *component.SynthesizedCompone
 	}
 	if len(r.restoreLabels) == 0 {
 		r.restoreLabels = constant.GetKBWellKnownLabels(r.Cluster.Spec.ClusterDefRef, r.Cluster.Name, comp.Name)
-		intctrlutil.MergeMetadataMapInplace(comp.Labels, &r.restoreLabels)
 	}
 	return metav1.ObjectMeta{
 		Name:      name,

e6e71c3c2f01cb166bf03790287576de4d6b05e0
wangyelei <yelei.wang@apecloud.com>
2025-08-20 11:03:58 +0800

    chore: update get component name func

--- pkg/dataprotection/restore/utils.go
+++ pkg/dataprotection/restore/utils.go
@@ -501,7 +501,8 @@ func BackupFilePathEnv(filePath, targetName, targetPodName string) []corev1.EnvV
 
 // getComponentNameFromObj gets the component name from the k8s object.
 func getComponentNameFromObj(obj client.Object) string {
-	if shardingName, ok := obj.GetLabels()[constant.KBAppShardingNameLabelKey]; ok {
+	shardingName := obj.GetLabels()[constant.KBAppShardingNameLabelKey]
+	if shardingName != "" {
 		return shardingName
 	}
 	return obj.GetLabels()[constant.KBAppComponentLabelKey]

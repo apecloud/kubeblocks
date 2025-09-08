52d27dd3add99bf5db4b832ac9cbd88fd707daa1
zjx20 <x.zhou@apecloud.com>
2025-09-08 21:44:03 +0800

    fix: do not set runAsNonRoot to the init container of datasafed (#9727)
    (cherry picked from commit 0c3b7d48cab1c81e1b6f0bb193201f1001278b6f)

--- controllers/dataprotection/backuprepo_controller.go
+++ controllers/dataprotection/backuprepo_controller.go
@@ -822,7 +822,6 @@ func (r *BackupRepoReconciler) runPreCheckJobForMounting(reconCtx *reconcileCont
 						}},
 						SecurityContext: &corev1.SecurityContext{
 							AllowPrivilegeEscalation: boolptr.False(),
-							RunAsNonRoot:             boolptr.True(),
 						},
 					}},
 					Volumes: []corev1.Volume{{
@@ -834,6 +833,9 @@ func (r *BackupRepoReconciler) runPreCheckJobForMounting(reconCtx *reconcileCont
 						},
 					}},
 					ServiceAccountName: saName,
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: boolptr.True(),
+					},
 				},
 			},
 			BackoffLimit: pointer.Int32(2),
@@ -902,10 +904,12 @@ datasafed rm %s`, precheckFilePath, precheckFilePath, precheckFilePath),
 						},
 						SecurityContext: &corev1.SecurityContext{
 							AllowPrivilegeEscalation: boolptr.False(),
-							RunAsNonRoot:             boolptr.True(),
 						},
 					}},
 					ServiceAccountName: saName,
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: boolptr.True(),
+					},
 				},
 			},
 			BackoffLimit: pointer.Int32(2),

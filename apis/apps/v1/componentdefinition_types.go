fec3e132b70f48cddca1267876c652fb08ee4c3d
Leon <leon@apecloud.com>
2025-12-31 10:16:35 +0800

    init

--- apis/apps/v1/componentdefinition_types.go
+++ apis/apps/v1/componentdefinition_types.go
@@ -1804,7 +1804,6 @@ type Action struct {
 	//
 	// This field cannot be updated.
 	//
-	// +kubebuilder:validation:Maximum=60
 	// +kubebuilder:default=0
 	// +optional
 	TimeoutSeconds int32 `json:"timeoutSeconds,omitempty"`

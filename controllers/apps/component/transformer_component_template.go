025b56843e12c1f826b8f0ad0c06e22cd1007db6
leon-ape <leon@apecloud.com>
2026-04-20 17:16:10 +0800

    fix: multi-cluster external managed template sync (#10143)
    (cherry picked from commit 9ab9ddf2f0e06dc8361a83117dc60caf564fe64f)

--- controllers/apps/component/transformer_component_template.go
+++ controllers/apps/component/transformer_component_template.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/Masterminds/sprig/v3"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/utils/ptr"
@@ -67,11 +68,34 @@ func (t *componentFileTemplateTransformer) Transform(ctx graph.TransformContext,
 
 	t.handleTemplateObjectChanges(transCtx, dag, runningObjs, protoObjs, toCreate, toDelete, toUpdate)
 
-	for _, obj := range protoObjs {
-		component.AddInstanceAssistantObject(transCtx.SynthesizeComponent, obj)
+	for _, tpl := range transCtx.SynthesizeComponent.FileTemplates {
+		if obj := t.instanceAssistantObject(transCtx, tpl, protoObjs); obj != nil {
+			component.AddInstanceAssistantObject(transCtx.SynthesizeComponent, obj)
+		}
 	}
 
 	return t.buildPodVolumes(transCtx)
+}
+
+func (t *componentFileTemplateTransformer) instanceAssistantObject(transCtx *componentTransformContext,
+	tpl component.SynthesizedFileTemplate, protoObjs map[string]*corev1.ConfigMap) client.Object {
+	if isExternalManaged(tpl) {
+		if len(tpl.Template) == 0 {
+			return nil
+		}
+		namespace := tpl.Namespace
+		if len(namespace) == 0 {
+			namespace = transCtx.SynthesizeComponent.Namespace
+		}
+		return &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespace,
+				Name:      tpl.Template,
+			},
+		}
+	}
+	objName := fileTemplateObjectName(transCtx.SynthesizeComponent, tpl.Name)
+	return protoObjs[objName]
 }
 
 func (t *componentFileTemplateTransformer) precheck(transCtx *componentTransformContext) error {

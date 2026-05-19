8ce658bae962853ce89a75955fa634b9a18f24b3
leon-ape <leon@apecloud.com>
2026-05-19 12:51:18 +0800

    chore: fix its image registry drift (#10256)

--- pkg/controller/instance/in_place_update_utils.go
+++ pkg/controller/instance/in_place_update_utils.go
@@ -208,7 +208,7 @@ func equalField(old, new any) bool {
 			if index < 0 {
 				return false
 			}
-			if nc.Image != ocs[index].Image {
+			if !intctrlutil.EqualContainerImageInSpec(ocs[index].Image, nc.Image) {
 				return false
 			}
 		}

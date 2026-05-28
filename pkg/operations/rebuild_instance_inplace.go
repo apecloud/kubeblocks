b6cfcf24ac2a1e6eff2d87849048f54807b1cd90
Wei Cao <cyg.cao@gmail.com>
2026-05-28 11:15:19 +0800

    fix(operations): preserve pvc protection in no-backup rebuild

--- pkg/operations/rebuild_instance_inplace.go
+++ pkg/operations/rebuild_instance_inplace.go
@@ -452,8 +452,10 @@ func (inPlaceHelper *inplaceRebuildHelper) rebuildSourcePVCsByDynamicProvision(r
 			}
 			return err
 		}
-		if err := inPlaceHelper.deleteSourcePVCForRebuild(reqCtx, cli, sourcePVC); err != nil {
-			return err
+		if sourcePVC.DeletionTimestamp == nil {
+			if err := intctrlutil.BackgroundDeleteObject(cli, reqCtx.Ctx, sourcePVC); err != nil {
+				return err
+			}
 		}
 	}
 	if err := inPlaceHelper.deleteTargetPodForRebuild(reqCtx, cli, opsRequest); err != nil {

fb30697d6a7a778e14bb0130c732dbafdd193a1d
leon-ape <leon@apecloud.com>
2026-06-04 16:01:25 +0800

    chore: require KB labels for external-managed ConfigMaps (#10332)

--- controllers/apps/component/transformer_component_validation.go
+++ controllers/apps/component/transformer_component_validation.go
@@ -22,8 +22,16 @@ package component
 import (
 	"fmt"
 
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+
 	appsv1 "github.com/apecloud/kubeblocks/apis/apps/v1"
+	workloads "github.com/apecloud/kubeblocks/apis/workloads/v1"
 	appsutil "github.com/apecloud/kubeblocks/controllers/apps/util"
+	"github.com/apecloud/kubeblocks/pkg/constant"
+	"github.com/apecloud/kubeblocks/pkg/controller/component"
 	"github.com/apecloud/kubeblocks/pkg/controller/graph"
 	intctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
 )
@@ -52,6 +60,9 @@ func (t *componentValidationTransformer) Transform(ctx graph.TransformContext, d
 	if err = validateCompReplicas(comp, transCtx.CompDef); err != nil {
 		return intctrlutil.NewRequeueError(appsutil.RequeueDuration, err.Error())
 	}
+	if err = validateExternalManagedConfigSources(transCtx); err != nil {
+		return intctrlutil.NewRequeueError(appsutil.RequeueDuration, err.Error())
+	}
 	// if err = validateSidecarContainers(comp, transCtx.CompDef); err != nil {
 	// 	return newRequeueError(requeueDuration, err.Error())
 	// }
@@ -74,4 +85,104 @@ func validateCompReplicas(comp *appsv1.Component, compDef *appsv1.ComponentDefin
 
 func replicasOutOfLimitError(replicas int32, replicasLimit appsv1.ReplicasLimit) error {
 	return fmt.Errorf("replicas %d out-of-limit [%d, %d]", replicas, replicasLimit.MinReplicas, replicasLimit.MaxReplicas)
+}
+
+func validateExternalManagedConfigSources(transCtx *componentTransformContext) error {
+	comp := transCtx.Component
+	if len(comp.Spec.Configs) == 0 {
+		return nil
+	}
+
+	externalManagedTemplates := map[string]component.SynthesizedFileTemplate{}
+	for _, tpl := range transCtx.SynthesizeComponent.FileTemplates {
+		if tpl.Config && ptr.Deref(tpl.ExternalManaged, false) {
+			externalManagedTemplates[tpl.Name] = tpl
+		}
+	}
+
+	var externalManagedConfigs []appsv1.ClusterComponentConfig
+	externalManagedConfigTemplates := map[string]component.SynthesizedFileTemplate{}
+	for _, config := range comp.Spec.Configs {
+		if config.Name == nil || config.ConfigMap == nil || config.ConfigMap.Name == "" {
+			continue
+		}
+		tpl, externalManagedTemplate := externalManagedTemplates[*config.Name]
+		if !externalManagedTemplate && !ptr.Deref(config.ExternalManaged, false) {
+			continue
+		}
+		externalManagedConfigs = append(externalManagedConfigs, config)
+		if externalManagedTemplate {
+			externalManagedConfigTemplates[*config.Name] = tpl
+		}
+	}
+	if len(externalManagedConfigs) == 0 {
+		return nil
+	}
+
+	clusterName, err := component.GetClusterName(comp)
+	if err != nil {
+		return err
+	}
+	compName, err := component.ShortName(clusterName, comp.Name)
+	if err != nil {
+		return err
+	}
+
+	for _, config := range externalManagedConfigs {
+		tpl := externalManagedConfigTemplates[*config.Name]
+		if isExistingExternalManagedConfigSource(transCtx, config, tpl) {
+			continue
+		}
+		if err := validateManagedConfigMapLabels(transCtx, config.ConfigMap.Name, clusterName, compName); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func isExistingExternalManagedConfigSource(transCtx *componentTransformContext, config appsv1.ClusterComponentConfig, tpl component.SynthesizedFileTemplate) bool {
+	if transCtx.RunningWorkload == nil || config.ConfigMap == nil || tpl.VolumeName == "" {
+		return false
+	}
+	runningITS, ok := transCtx.RunningWorkload.(*workloads.InstanceSet)
+	if !ok || runningITS == nil {
+		return false
+	}
+	for _, volume := range runningITS.Spec.Template.Spec.Volumes {
+		if volume.Name == tpl.VolumeName && volume.ConfigMap != nil && volume.ConfigMap.Name == config.ConfigMap.Name {
+			return true
+		}
+	}
+	return false
+}
+
+func validateManagedConfigMapLabels(transCtx *componentTransformContext, cmName, clusterName, compName string) error {
+	cm := &corev1.ConfigMap{}
+	cmKey := types.NamespacedName{
+		Namespace: transCtx.Component.Namespace,
+		Name:      cmName,
+	}
+	if err := transCtx.Client.Get(transCtx.Context, cmKey, cm); err != nil {
+		if apierrors.IsNotFound(err) {
+			return fmt.Errorf("configMap %q for external-managed config is not found", cmName)
+		}
+		return err
+	}
+
+	// Treat these common component labels as the apps-level handoff contract for
+	// externally managed config sources. The component controller intentionally
+	// does not inspect parameters-specific names, annotations, finalizers, or
+	// owner references.
+	expectedLabels := map[string]string{
+		constant.AppManagedByLabelKey:   constant.AppName,
+		constant.AppInstanceLabelKey:    clusterName,
+		constant.KBAppComponentLabelKey: compName,
+	}
+	for key, expected := range expectedLabels {
+		if actual := cm.Labels[key]; actual != expected {
+			return fmt.Errorf("configMap %q for external-managed config must have label %q=%q, got %q",
+				cmName, key, expected, actual)
+		}
+	}
+	return nil
 }

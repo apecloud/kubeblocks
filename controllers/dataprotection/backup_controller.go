19bdb329ea8dde87f2caaec27edb75524546756c
Wei Cao <cyg.cao@gmail.com>
2026-06-07 09:39:09 +0800

    fix: grant namespace access for backup deletion guard (#10347)

--- controllers/dataprotection/backup_controller.go
+++ controllers/dataprotection/backup_controller.go
@@ -79,6 +79,7 @@ type BackupReconciler struct {
 // +kubebuilder:rbac:groups=snapshot.storage.k8s.io,resources=volumesnapshots/finalizers,verbs=update;patch
 // +kubebuilder:rbac:groups=snapshot.storage.k8s.io,resources=volumesnapshotclasses,verbs=get;list;watch
 
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
 

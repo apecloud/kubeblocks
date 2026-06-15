2535caa9d1217409465176e02ecebbb36cc0d9b5
leon-ape <leon@apecloud.com>
2026-06-15 15:08:08 +0800

    chore: keep topology template compDef resolution constrained (#10372)
    (cherry picked from commit 6960b1fcd5a7c2d41cc3ed6143743276e9c5b63a)

--- controllers/apps/cluster/transformer_cluster_normalization.go
+++ controllers/apps/cluster/transformer_cluster_normalization.go
@@ -433,7 +433,7 @@ func (t *clusterNormalizationTransformer) resolveDefinitions4ComponentWithObj(tr
 		if len(tpl.ServiceVersion) == 0 && len(tpl.CompDef) == 0 {
 			continue
 		}
-		compDef, serviceVersion, err = t.resolveCompDefinitionNServiceVersionWithTemplate(transCtx, comp, &tpl)
+		compDef, serviceVersion, err = t.resolveCompDefinitionNServiceVersionWithTemplate(transCtx, comp, &tpl, compSpec.ComponentDef)
 		if err != nil {
 			return nil, err
 		}
@@ -458,7 +458,7 @@ func (t *clusterNormalizationTransformer) resolveCompDefinitionNServiceVersionWi
 }
 
 func (t *clusterNormalizationTransformer) resolveCompDefinitionNServiceVersionWithTemplate(transCtx *clusterTransformContext,
-	comp *appsv1.Component, protoTpl *appsv1.InstanceTemplate) (*appsv1.ComponentDefinition, string, error) {
+	comp *appsv1.Component, protoTpl *appsv1.InstanceTemplate, defaultCompDefName string) (*appsv1.ComponentDefinition, string, error) {
 	var (
 		ctx        = transCtx.Context
 		cli        = transCtx.Client
@@ -474,6 +474,9 @@ func (t *clusterNormalizationTransformer) resolveCompDefinitionNServiceVersionWi
 	}
 	serviceVersion := protoTpl.ServiceVersion
 	compDefName := protoTpl.CompDef
+	if len(compDefName) == 0 {
+		compDefName = defaultCompDefName
+	}
 	if comp == nil || runningTpl == nil || t.checkTemplateUpgrade(serviceVersion, compDefName, runningTpl) {
 		return resolveCompDefinitionNServiceVersion(ctx, cli, compDefName, serviceVersion)
 	}

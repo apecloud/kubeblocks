84e21fd6f52917c62427c4c1c652437081f257c3
wangyelei <yelei.wang@apecloud.com>
2026-06-30 10:28:24 +0800

    fix(operations): validate upgrade service version and fail fast on invalid format (#10470)

--- apis/operations/v1alpha1/opsrequest_types_test.go
+++ apis/operations/v1alpha1/opsrequest_types_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
 	"testing"
 )
 
@@ -51,4 +52,151 @@ func TestSetStatusAndMessage(t *testing.T) {
 	if p.Status != SucceedProgressStatus && p.Message != message {
 		t.Error("set progressDetail status and message failed")
 	}
+}
+
+func TestValidateUpgradeServiceVersion(t *testing.T) {
+	tests := []struct {
+		name    string
+		upgrade *Upgrade
+		wantErr bool
+	}{
+		{
+			name:    "nil upgrade",
+			upgrade: nil,
+			wantErr: true,
+		},
+		{
+			name:    "empty components",
+			upgrade: &Upgrade{},
+			wantErr: true,
+		},
+		{
+			name: "valid service version with three parts",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("17.5.0"),
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid service version without patch",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("17.5"),
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "nil service version skips validation",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:            ComponentOps{ComponentName: "pg"},
+						ComponentDefinitionName: strPtr("new-compdef"),
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "empty service version string is valid",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr(""),
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "service version with prefix v is valid",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("v1.0.0"),
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid service version random text",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("not-a-version"),
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "valid version with pre-release",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("17.5.0-alpha.1"),
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid version with build metadata",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("17.5.0+build123"),
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "multiple components with mixed versions",
+			upgrade: &Upgrade{
+				Components: []UpgradeComponent{
+					{
+						ComponentOps:   ComponentOps{ComponentName: "pg"},
+						ServiceVersion: strPtr("17.5.0"),
+					},
+					{
+						ComponentOps:   ComponentOps{ComponentName: "redis"},
+						ServiceVersion: strPtr("7.2"),
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &OpsRequest{}
+			if tt.upgrade != nil {
+				r.Spec.Upgrade = tt.upgrade
+			}
+			err := r.validateUpgrade(context.Background(), nil, nil)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("validateUpgrade() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func strPtr(s string) *string {
+	return &s
 }

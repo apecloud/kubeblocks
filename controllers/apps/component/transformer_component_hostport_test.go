aa84f274ce554ffeb6471ed25e2714a9bb144689
Wei <wei@black-cat.local>
2026-07-02 22:27:46 +0800

    fix(component): restrict non-host-network hostPort mappings to runtime containers
    The hostPort transformer applied network.hostPorts mappings by port name
    across all pod containers. Injected containers reuse common port names:
    kbagent's HTTP port is literally named "http", so an Elasticsearch-style
    component whose runtime container also names its port "http" ended up
    with two containers bound to the same host port and the pod was rejected
    with 'spec.containers[3].ports[0].hostPort: Duplicate value'.
    
    The ComponentNetwork API already documents that non-host-network
    mappings are restricted to ports defined in
    cmpd.spec.runtime.containers.ports, so enforce exactly that: skip any
    container not present in the componentDef runtime, which covers kbagent
    as well as other injected sidecars.
    
    Fixes #10502. Supersedes #10503, which skipped only kbagent containers.

---
+++ controllers/apps/component/transformer_component_hostport_test.go
@@ -0,0 +1,100 @@
+/*
+Copyright (C) 2022-2026 ApeCloud Co., Ltd
+
+This file is part of KubeBlocks project
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package component
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+
+	appsv1 "github.com/apecloud/kubeblocks/apis/apps/v1"
+	pkgcomponent "github.com/apecloud/kubeblocks/pkg/controller/component"
+	"github.com/apecloud/kubeblocks/pkg/kbagent"
+)
+
+func TestComponentHostPortTransformerAppliesToRuntimeContainersOnly(t *testing.T) {
+	// Elasticsearch-style collision: the runtime container and the injected
+	// kbagent container both name a port "http". Only the runtime container may
+	// receive the hostPort mapping, otherwise the pod is rejected with a
+	// duplicate hostPort value.
+	transCtx := &componentTransformContext{
+		ComponentOrig: &appsv1.Component{},
+		CompDef: &appsv1.ComponentDefinition{
+			Spec: appsv1.ComponentDefinitionSpec{
+				Runtime: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name: "elasticsearch",
+						Ports: []corev1.ContainerPort{
+							{Name: "http", ContainerPort: 9200},
+						},
+					}},
+				},
+			},
+		},
+		SynthesizeComponent: &pkgcomponent.SynthesizedComponent{
+			Network: &appsv1.ComponentNetwork{
+				HostPorts: []appsv1.HostPort{
+					{Name: "http", Port: 56595},
+				},
+			},
+			PodSpec: &corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name: "elasticsearch",
+						Ports: []corev1.ContainerPort{
+							{Name: "http", ContainerPort: 9200},
+						},
+					},
+					{
+						Name: kbagent.ContainerName,
+						Ports: []corev1.ContainerPort{
+							{Name: kbagent.DefaultHTTPPortName, ContainerPort: kbagent.DefaultHTTPPort},
+							{Name: kbagent.DefaultStreamingPortName, ContainerPort: kbagent.DefaultStreamingPort},
+						},
+					},
+					{
+						// a non-kbagent injected sidecar reusing the port name
+						Name: "metrics-sidecar",
+						Ports: []corev1.ContainerPort{
+							{Name: "http", ContainerPort: 9114},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := (&componentHostPortTransformer{}).Transform(transCtx, nil); err != nil {
+		t.Fatalf("Transform() error = %v", err)
+	}
+
+	appPort := transCtx.SynthesizeComponent.PodSpec.Containers[0].Ports[0]
+	if appPort.HostPort != 56595 {
+		t.Fatalf("app http HostPort = %d, want 56595", appPort.HostPort)
+	}
+
+	for _, container := range transCtx.SynthesizeComponent.PodSpec.Containers[1:] {
+		for _, port := range container.Ports {
+			if port.HostPort != 0 {
+				t.Fatalf("injected container %q port %q HostPort = %d, want 0", container.Name, port.Name, port.HostPort)
+			}
+		}
+	}
+}

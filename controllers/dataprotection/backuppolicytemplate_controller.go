f0f3f2a377df163f8590dd3755ae41340fb18a88
Harold Cheng <niuchangcun@gmail.com>
2026-08-07 10:54:35 +0800

    fix test

--- controllers/dataprotection/backuppolicytemplate_controller.go
+++ controllers/dataprotection/backuppolicytemplate_controller.go
@@ -18,6 +18,7 @@ package dataprotection
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"reflect"
 	"slices"
@@ -63,27 +64,24 @@ func (r *BackupPolicyTemplateReconciler) Reconcile(ctx context.Context, req reco
 		return intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
 	}
 	oldBPT := backupPolicyTemplate.DeepCopy()
-	if err := r.setComponentDefStatus(reqCtx, oldBPT, backupPolicyTemplate); err != nil {
-		return intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
-	}
-	if err := r.validateAvailable(reqCtx, oldBPT, backupPolicyTemplate); err != nil {
-		return intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
-	}
+	err1 := r.setComponentDefStatus(reqCtx, backupPolicyTemplate)
+	err2 := r.validateAvailable(reqCtx, backupPolicyTemplate)
 	if !reflect.DeepEqual(oldBPT.Status, backupPolicyTemplate.Status) {
 		if err := r.Client.Status().Patch(reqCtx.Ctx, backupPolicyTemplate, client.MergeFrom(oldBPT)); err != nil {
 			return intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
 		}
 	}
+	err := errors.Join(err1, err2)
+	if err != nil {
+		return intctrlutil.CheckedRequeueWithError(err, reqCtx.Log, "")
+	}
 	return intctrlutil.Reconciled()
 }
 
-func (r *BackupPolicyTemplateReconciler) setComponentDefStatus(reqCtx intctrlutil.RequestCtx, oldBPT, bpt *dpv1alpha1.BackupPolicyTemplate) error {
+func (r *BackupPolicyTemplateReconciler) setComponentDefStatus(reqCtx intctrlutil.RequestCtx, bpt *dpv1alpha1.BackupPolicyTemplate) error {
 	compDefList := &appsv1.ComponentDefinitionList{}
 	if err := r.Client.List(reqCtx.Ctx, compDefList); err != nil {
 		return err
-	}
-	if bpt.Status.MatchedCompDefs == nil {
-		bpt.Status.MatchedCompDefs = []string{}
 	}
 	newMatchedCompDefs := []string{}
 	for _, item := range compDefList.Items {
@@ -100,7 +98,7 @@ func (r *BackupPolicyTemplateReconciler) setComponentDefStatus(reqCtx intctrluti
 	return nil
 }
 
-func (r *BackupPolicyTemplateReconciler) validateAvailable(reqCtx intctrlutil.RequestCtx, oldBPT, bpt *dpv1alpha1.BackupPolicyTemplate) error {
+func (r *BackupPolicyTemplateReconciler) validateAvailable(reqCtx intctrlutil.RequestCtx, bpt *dpv1alpha1.BackupPolicyTemplate) error {
 	message := ""
 	backupMethodMap := map[string]*dpv1alpha1.ActionSet{}
 	actionSetNotFound := false

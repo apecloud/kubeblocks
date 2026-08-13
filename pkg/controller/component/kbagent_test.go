eaecbf070d924496fd0231eae131227b24b59901
Leon <leon@apecloud.com>
2026-08-13 12:09:11 +0800

    refactor: simplify kbagent init contract names

--- pkg/controller/component/kbagent_test.go
+++ pkg/controller/component/kbagent_test.go
@@ -289,7 +289,7 @@ var _ = Describe("kb-agent", func() {
 
 			ic := kbAgentInitContainer()
 			Expect(ic).ShouldNot(BeNil())
-			Expect(ic.Command).Should(Equal(kbagent.CurrentInitCopyCommand()))
+			Expect(ic.Command).Should(Equal(kbagent.InitCommand()))
 
 			c := kbAgentContainer()
 			Expect(c).ShouldNot(BeNil())

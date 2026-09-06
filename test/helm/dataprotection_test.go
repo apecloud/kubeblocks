044d9648b3e6cc6d6d2e9b5015db406237b2a440
Wei Cao <cyg.cao@gmail.com>
2026-09-06 17:40:16 +0800

    chore: align chart test copyright header

--- test/helm/dataprotection_test.go
+++ test/helm/dataprotection_test.go
@@ -1,7 +1,5 @@
 /*
-Copyright (C) 2026 ApeCloud Co., Ltd.
-
-This file is part of KubeBlocks project.
+Copyright (C) 2022-2026 ApeCloud Co., Ltd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

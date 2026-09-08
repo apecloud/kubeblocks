e6974e7c38eac68d815194dcab98c3df5d07ff3b
leon-ape <leon@apecloud.com>
2026-09-08 15:30:02 +0800

    chore: validate sharding definitions when loading referenced resources (#10866)

--- controllers/apps/cluster/transformer_cluster_normalization.go
+++ controllers/apps/cluster/transformer_cluster_normalization.go
@@ -35,6 +35,7 @@ import (
 
 	appsv1 "github.com/apecloud/kubeblocks/apis/apps/v1"
 	appsv1alpha1 "github.com/apecloud/kubeblocks/apis/apps/v1alpha1"
+	appsutil "github.com/apecloud/kubeblocks/controllers/apps/util"
 	"github.com/apecloud/kubeblocks/pkg/constant"
 	"github.com/apecloud/kubeblocks/pkg/controller/component"
 	"github.com/apecloud/kubeblocks/pkg/controller/graph"
@@ -329,9 +330,9 @@ func (t *clusterNormalizationTransformer) resolveDefinitions4ShardTemplate(trans
 	var shardingDef *appsv1.ShardingDefinition
 	shardingDefName = t.shardingDefinitionName(shardingDefName, comp)
 	if len(shardingDefName) > 0 {
-		shardingDef, err = resolveShardingDefinition(transCtx.Context, transCtx.Client, shardingDefName)
+		shardingDef, err = getNCheckShardingDefinition(transCtx.Context, transCtx.Client, shardingDefName)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, controllerutil.NewRequeueError(appsutil.RequeueDuration, err.Error())
 		}
 		if len(spec.ComponentDef) == 0 {
 			spec.ComponentDef = shardingDef.Spec.Template.CompDef
@@ -659,6 +660,20 @@ func clusterTopologyCompMatched(comp appsv1.ClusterTopologyComponent, compName s
 		return strings.HasPrefix(compName, comp.Name)
 	}
 	return false
+}
+
+func getNCheckShardingDefinition(ctx context.Context, cli client.Reader, name string) (*appsv1.ShardingDefinition, error) {
+	shardingDef, err := resolveShardingDefinition(ctx, cli, name)
+	if err != nil {
+		return nil, err
+	}
+	if shardingDef.Generation != shardingDef.Status.ObservedGeneration {
+		return nil, fmt.Errorf("the referenced ShardingDefinition is not up to date: %s", shardingDef.Name)
+	}
+	if shardingDef.Status.Phase != appsv1.AvailablePhase {
+		return nil, fmt.Errorf("the referenced ShardingDefinition is unavailable: %s", shardingDef.Name)
+	}
+	return shardingDef, nil
 }
 
 // resolveShardingDefinition resolves and returns the specific sharding definition object supported.

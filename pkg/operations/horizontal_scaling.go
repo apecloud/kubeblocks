314aacd94662c180666cab4e6ca946f41ec2c427
Leon <leon@apecloud.com>
2026-09-24 14:54:22 +0800

    refactor(ops): keep scaling helpers with horizontal scaling

--- pkg/operations/horizontal_scaling.go
+++ pkg/operations/horizontal_scaling.go
@@ -34,11 +34,47 @@ import (
 	opsv1alpha1 "github.com/apecloud/kubeblocks/apis/operations/v1alpha1"
 	workloads "github.com/apecloud/kubeblocks/apis/workloads/v1"
 	"github.com/apecloud/kubeblocks/pkg/constant"
+	"github.com/apecloud/kubeblocks/pkg/controller/instanceset"
 	"github.com/apecloud/kubeblocks/pkg/controller/sharding"
 	intctrlutil "github.com/apecloud/kubeblocks/pkg/controllerutil"
 )
 
 const horizontalScalingFailedReason = "HorizontalScalingFailed"
+
+// Deprecated: should use instancetemplate.PodNameBuilder.
+func generateAllPodNamesToSet(
+	compReplicas int32,
+	instances []appsv1.InstanceTemplate,
+	offlineInstances []string,
+	clusterName,
+	fullCompName string) (map[string]string, error) {
+	compName := constant.GenerateClusterComponentName(clusterName, fullCompName)
+	instanceNames, err := generateAllPodNames(compReplicas, instances, offlineInstances, compName)
+	if err != nil {
+		return nil, err
+	}
+	instanceSet := map[string]string{}
+	for _, insName := range instanceNames {
+		instanceSet[insName] = appsv1.GetInstanceTemplateName(clusterName, fullCompName, insName)
+	}
+	return instanceSet, nil
+}
+
+func generateAllPodNames(
+	compReplicas int32,
+	instances []appsv1.InstanceTemplate,
+	offlineInstances []string,
+	fullCompName string) ([]string, error) {
+	var templates []instanceset.InstanceTemplate
+	for i := range instances {
+		templates = append(templates, &workloads.InstanceTemplate{
+			Name:     instances[i].Name,
+			Replicas: instances[i].Replicas,
+			Ordinals: instances[i].Ordinals,
+		})
+	}
+	return instanceset.GenerateAllInstanceNames(fullCompName, compReplicas, templates, offlineInstances, appsv1.Ordinals{})
+}
 
 type horizontalScalingOpsHandler struct{}
 
